521dab48a8c029f8ab565d0addb6904a329a6e2c
Peter Waher <peterwaher@hotmail.com>
2016-06-11 13:04:29 +0200

    fixes.

--- Runtime/Waher.Runtime.Cache/Cache.cs
+++ Runtime/Waher.Runtime.Cache/Cache.cs
@@ -218,7 +218,7 @@ namespace Waher.Runtime.Cache
 			DateTime TP = DateTime.Now;
 
 			while (this.keysByLastUsage.ContainsKey(TP))
-				TP.AddTicks(rnd.Next(1, 10));
+				TP = TP.AddTicks(rnd.Next(1, 10));
 
 			return TP;
 		}
@@ -301,7 +301,7 @@ namespace Waher.Runtime.Cache
 				DateTime TP = DateTime.Now;
 
 				while (this.keysByCreation.ContainsKey(TP) || this.keysByLastUsage.ContainsKey(TP))
-					TP.AddTicks(rnd.Next(1, 10));
+					TP = TP.AddTicks(rnd.Next(1, 10));
 
 				Item = new CacheItem<KeyType, ValueType>(Key, Value, TP);
 

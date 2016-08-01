93966e2fd0722d04c7fcaf0f6e4d1135b474c5e8
Peter Waher <peterwaher@hotmail.com>
2016-08-01 17:18:10 +0200

    Running the IoT Gateway as a Windows Service.

---
+++ Waher.IoTGateway.Svc/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waher.IoTGateway.Svc
+{
+	public partial class Service : ServiceBase
+	{
+		public Service()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			Gateway.Start();
+		}
+
+		protected override void OnStop()
+		{
+			Gateway.Stop();
+		}
+	}
+}

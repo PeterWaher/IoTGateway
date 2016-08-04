ed10351be15e4b4f418179b5711413d8eabb528a
Peter Waher <peterwaher@hotmail.com>
2016-08-04 20:09:38 +0200

    Wix installation script.

--- Waher.IoTGateway.Installers/CustomAction.cs
+++ Waher.IoTGateway.Installers/CustomAction.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using Microsoft.Deployment.WindowsInstaller;
+using Waher.Networking.Sniffers;
+using Waher.Networking.XMPP;
 
 namespace Waher.IoTGateway.Installers
 {
@@ -67,5 +70,166 @@ namespace Waher.IoTGateway.Installers
 
 			return ActionResult.Success;
 		}
+
+		[CustomAction]
+		public static ActionResult ValidateBroker(Session Session)
+		{
+			Session.Log("Validating XMPP broker.");
+			Session["Log"] = "Validating XMPP broker.";
+			try
+			{
+				string XmppBroker = Session["XMPPBROKER"];
+				Session.Log("XMPP broker to validate: " + XmppBroker);
+				Session["Log"] = "XMPP broker to validate: " + XmppBroker;
+
+				using (XmppClient Client = new XmppClient(XmppBroker, 5222, string.Empty, string.Empty, "en"))
+				{
+					ManualResetEvent Done = new ManualResetEvent(false);
+					ManualResetEvent Fail = new ManualResetEvent(false);
+					bool Connected = false;
+
+					Client.Add(new SessionSniffer(Session));
+
+					Client.OnStateChanged += (Sender, NewState) =>
+					{
+						Session["Log"] = "New state: " + NewState.ToString();
+
+						switch (NewState)
+						{
+							case XmppState.StreamNegotiation:
+								Connected = true;
+								break;
+
+							case XmppState.Authenticating:
+							case XmppState.StartingEncryption:
+								Done.Set();
+								break;
+
+							case XmppState.Error:
+								Fail.Set();
+								break;
+						}
+					};
+
+					if (WaitHandle.WaitAny(new WaitHandle[] { Done, Fail }, 5000) < 0 || !Connected)
+					{
+						Session["XmppBrokerOk"] = "0";
+						Session.Log("Broker not reached. Domain name not OK.");
+						Session["Log"] = "Broker not reached. Domain name not OK.";
+					}
+					else
+					{
+						Session["XmppBrokerOk"] = "1";
+
+						if (Done.WaitOne(0))
+						{
+							Session["XmppPortRequired"] = "0";
+							Session["XMPPPORT"] = "5222";
+							Session.Log("Broker reached on default port (5222).");
+							Session["Log"] = "Broker reached on default port (5222).";
+						}
+						else
+						{
+							Session["XmppPortRequired"] = "1";
+							Session.Log("Broker reached, but XMPP service not available on default port (5222).");
+							Session["Log"] = "Broker reached, but XMPP service not available on default port (5222).";
+						}
+					}
+				}
+
+				return ActionResult.Success;
+			}
+			catch (Exception ex)
+			{
+				Session.Log("Validation of XMPP broker failed. Error reported: " + ex.Message);
+				Session["Log"] = "Validation of XMPP broker failed. Error reported: " + ex.Message;
+				return ActionResult.Failure;
+			}
+		}
+
+		[CustomAction]
+		public static ActionResult ValidatePort(Session Session)
+		{
+			Session.Log("Validating XMPP broker.");
+			Session["Log"] = "Validating XMPP broker.";
+			try
+			{
+				string XmppBroker = Session["XMPPBROKER"];
+				int XmppPort;
+
+				if (!int.TryParse(Session["XMPPPORT"], out XmppPort) || XmppPort <= 0 || XmppPort > 65535)
+				{
+					Session["XmppPortOk"] = "0";
+					Session.Log("Invalid port number.");
+					Session["Log"] = "Invalid port number.";
+				}
+				else
+				{
+					Session.Log("XMPP broker to validate: " + XmppBroker + ":" + XmppPort.ToString());
+					Session["Log"] = "XMPP broker to validate: " + XmppBroker + ":" + XmppPort.ToString();
+
+					using (XmppClient Client = new XmppClient(XmppBroker, XmppPort, string.Empty, string.Empty, "en"))
+					{
+						ManualResetEvent Done = new ManualResetEvent(false);
+						ManualResetEvent Fail = new ManualResetEvent(false);
+						bool Connected = false;
+
+						Client.Add(new SessionSniffer(Session));
+
+						Client.OnStateChanged += (Sender, NewState) =>
+						{
+							Session["Log"] = "New state: " + NewState.ToString();
+
+							switch (NewState)
+							{
+								case XmppState.StreamNegotiation:
+									Connected = true;
+									break;
+
+								case XmppState.Authenticating:
+								case XmppState.StartingEncryption:
+									Done.Set();
+									break;
+
+								case XmppState.Error:
+									Fail.Set();
+									break;
+							}
+						};
+
+						if (WaitHandle.WaitAny(new WaitHandle[] { Done, Fail }, 5000) < 0 || !Connected)
+						{
+							Session["XmppPortOk"] = "0";
+							Session.Log("Broker not reached. Domain name not OK.");
+							Session["Log"] = "Broker not reached. Domain name not OK.";
+						}
+						else
+						{
+							if (Done.WaitOne(0))
+							{
+								Session["XmppPortOk"] = "1";
+								Session.Log("Broker reached.");
+								Session["Log"] = "Broker reached.";
+							}
+							else
+							{
+								Session["XmppPortOk"] = "0";
+								Session.Log("Broker reached, but XMPP service not available on port.");
+								Session["Log"] = "Broker reached, but XMPP service not available on port.";
+							}
+						}
+					}
+				}
+
+				return ActionResult.Success;
+			}
+			catch (Exception ex)
+			{
+				Session.Log("Validation of XMPP broker failed. Error reported: " + ex.Message);
+				Session["Log"] = "Validation of XMPP broker failed. Error reported: " + ex.Message;
+				return ActionResult.Failure;
+			}
+		}
+
 	}
 }
---
+++ Waher.IoTGateway.Installers/SessionSniffer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.WindowsInstaller;
+using Waher.Networking.Sniffers;
+
+namespace Waher.IoTGateway.Installers
+{
+	public class SessionSniffer : ISniffer
+	{
+		private Session session;
+
+		public SessionSniffer(Session Session)
+		{
+			this.session = Session;
+		}
+
+		public void Error(string Error)
+		{
+			this.session["Log"] = "ERROR: " + Error;
+		}
+
+		public void Exception(string Exception)
+		{
+			this.session["Log"] = "EXCEPTION: " + Exception;
+		}
+
+		public void Information(string Comment)
+		{
+			this.session["Log"] = Comment;
+		}
+
+		public void ReceiveBinary(byte[] Data)
+		{
+		}
+
+		public void ReceiveText(string Text)
+		{
+			this.session["Log"] = "Rx: " + Text;
+		}
+
+		public void TransmitBinary(byte[] Data)
+		{
+		}
+
+		public void TransmitText(string Text)
+		{
+			this.session["Log"] = "Tx: " + Text;
+		}
+
+		public void Warning(string Warning)
+		{
+			this.session["Log"] = "WARNING: " + Warning;
+		}
+	}
+}

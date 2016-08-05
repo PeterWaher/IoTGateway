03017840060349569163a2df31e411463969f442
Peter Waher <peterwaher@hotmail.com>
2016-08-05 20:16:46 +0200

    Setting correct working directory.

--- Waher.IoTGateway.Svc/Service.cs
+++ Waher.IoTGateway.Svc/Service.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -18,6 +19,7 @@ namespace Waher.IoTGateway.Svc
 
 		protected override void OnStart(string[] args)
 		{
+			Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 			Gateway.Start(false);
 		}
 

3733f86c2dfd6cc789f446a8af75c03cf85f058c
Peter Waher <peterwaher@hotmail.com>
2017-06-23 13:09:00 +0200

    Adding Waher.Events.MQTT.UWP

---
+++ Events/Waher.Events.MQTT.UWP/EventEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Waher.Networking.MQTT;
+
+namespace Waher.Events.MQTT
+{
+	/// <summary>
+	/// Delegate for <see cref="MqttEventReceptor.OnEvent"/> events.
+	/// </summary>
+	/// <param name="Sender">Sender of event.</param>
+	/// <param name="e">Event arguments.</param>
+	public delegate void EventEventHandler(object Sender, EventEventArgs e);
+
+	/// <summary>
+	/// Event arguments for <see cref="MqttEventReceptor.OnEvent"/> events.
+	/// </summary>
+	public class EventEventArgs : MqttContent
+	{
+		private Event ev;
+
+		internal EventEventArgs(MqttContent e, Event Event)
+			: base(e.Header, e.Topic, e.Data)
+		{
+			this.ev = Event;
+		}
+
+		/// <summary>
+		/// Event.
+		/// </summary>
+		public Event Event { get { return this.ev; } }
+	}
+}

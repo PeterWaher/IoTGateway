e2bdfa6b0daf6d27b606fb09eb839754303a0e0a
Peter Waher <peterwaher@hotmail.com>
2017-07-10 14:04:40 +0200

    fix

--- Persistence/Waher.Persistence.MongoDB.Test/Properties/AssemblyInfo.cs
+++ Persistence/Waher.Persistence.MongoDB.Test/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Waher.Persistence.MongoDB.Test")]
-[assembly: AssemblyDescription("NUnit test project for Waher.Persistence.MongoDB.")]
+[assembly: AssemblyDescription("Unit test project for Waher.Persistence.MongoDB.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Waher Data AB")]
 [assembly: AssemblyProduct("Waher.IoTGateway")]

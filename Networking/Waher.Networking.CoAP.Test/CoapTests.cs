8a2eb945536b76cc36b63f1943cdfde590ed882e
Peter Waher <peterwaher@hotmail.com>
2017-07-12 18:53:14 +0200

    Updated Waher.Networking.CoAP.Test to MSTest.

--- Networking/Waher.Networking.CoAP.Test/CoapTests.cs
+++ Networking/Waher.Networking.CoAP.Test/CoapTests.cs
@@ -3,35 +3,48 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
 using System.Xml;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Waher.Content;
 using Waher.Networking.Sniffers;
 using Waher.Networking.CoAP.CoRE;
 using Waher.Networking.CoAP.Options;
+using Waher.Runtime.Inventory;
 
 namespace Waher.Networking.CoAP.Test
 {
-	[TestFixture]
+	[TestClass]
 	public class CoapTests
 	{
 		private CoapClient client;
 
-		[SetUp]
-		public void Setup()
+		[AssemblyInitialize]
+		public static void AssemblyInitialize(TestContext Context)
+		{
+			Types.Initialize(
+				typeof(IContentDecoder).Assembly,
+				typeof(CoapClient).Assembly);
+		}
+
+		[TestInitialize]
+		public void TestInitialize()
 		{
 			this.client = new CoapClient(new ConsoleOutSniffer(BinaryPresentationMethod.Hexadecimal));
 		}
 
-		[TearDown]
-		public void TearDown()
+		[TestCleanup]
+		public void TestCleanup()
 		{
-			ulong[] Tokens = this.client.GetActiveTokens();
-			ushort[] MessageIDs = this.client.GetActiveMessageIDs();
+			if (this.client != null)
+			{
+				ulong[] Tokens = this.client.GetActiveTokens();
+				ushort[] MessageIDs = this.client.GetActiveMessageIDs();
 
-			this.client.Dispose();
-			this.client = null;
+				this.client.Dispose();
+				this.client = null;
 
-			Assert.AreEqual(0, Tokens.Length, "There are tokens that have not been unregistered properly.");
-			Assert.AreEqual(0, MessageIDs.Length, "There are message IDs that have not been unregistered properly.");
+				Assert.AreEqual(0, Tokens.Length, "There are tokens that have not been unregistered properly.");
+				Assert.AreEqual(0, MessageIDs.Length, "There are message IDs that have not been unregistered properly.");
+			}
 		}
 
 		private async Task<object> Get(string Uri, params CoapOption[] Options)
@@ -168,141 +181,146 @@ namespace Waher.Networking.CoAP.Test
 			Assert.AreEqual(0, WaitHandle.WaitAny(new WaitHandle[] { Done, Error }, 30000));
 		}
 
-		[Test]
-		public async Task Test_01_GET()
+		[TestMethod]
+		public async Task CoAP_Test_01_GET()
 		{
 			// Default test resource
 			await this.Get("coap://vs0.inf.ethz.ch/test");
 		}
 
-		[Test]
-		public async Task Test_02_Root()
+		[TestMethod]
+		public async Task CoAP_Test_02_Root()
 		{
 			await this.Get("coap://vs0.inf.ethz.ch/");
 		}
 
-		[Test]
-		public async Task Test_03_Discover()
+		[TestMethod]
+		public async Task CoAP_Test_03_Discover()
 		{
 			LinkDocument Doc = await this.Get("coap://vs0.inf.ethz.ch/.well-known/core") as LinkDocument;
 			Assert.IsNotNull(Doc);
 		}
 
-		[Test]
-		public async Task Test_04_Separate()
+		[TestMethod]
+		public async Task CoAP_Test_04_Separate()
 		{
 			// Resource which cannot be served immediately and which cannot be acknowledged in a piggy-backed way
 			await this.Get("coap://vs0.inf.ethz.ch/separate");
 		}
 
-		[Test]
-		public async Task Test_05_LongPath()
+		[TestMethod]
+		public async Task CoAP_Test_05_LongPath()
 		{
 			// Long path resource
 			await this.Get("coap://vs0.inf.ethz.ch/seg1");
 		}
 
-		[Test]
-		public async Task Test_06_LongPath()
+		[TestMethod]
+		public async Task CoAP_Test_06_LongPath()
 		{
 			// Long path resource
 			await this.Get("coap://vs0.inf.ethz.ch/seg1/seg2");
 		}
 
-		[Test]
-		public async Task Test_07_LongPath()
+		[TestMethod]
+		public async Task CoAP_Test_07_LongPath()
 		{
 			// Long path resource
 			await this.Get("coap://vs0.inf.ethz.ch/seg1/seg2/seg3");
 		}
 
-		[Test]
-		public async Task Test_08_Large()
+		[TestMethod]
+		public async Task CoAP_Test_08_Large()
 		{
 			// Large resource
 			await this.Get("coap://vs0.inf.ethz.ch/large");
 		}
 
-		[Test]
-		public async Task Test_09_LargeSeparate()
+		[TestMethod]
+		public async Task CoAP_Test_09_LargeSeparate()
 		{
 			// Large resource
 			await this.Get("coap://vs0.inf.ethz.ch/large-separate");
 		}
 
-		[Test]
-		[Ignore("Interop server has invalid XML.")]
-		public async Task Test_10_MultiFormat()
+		[TestMethod]
+		[Ignore]
+		public async Task CoAP_Test_10_MultiFormat()
 		{
 			// Resource that exists in different content formats (text/plain utf8 and application/xml)
 			string s = await this.Get("coap://vs0.inf.ethz.ch/multi-format", new CoapOptionAccept(0)) as string;
-			Assert.NotNull(s);
+			AssertNotNull(s);
 
 			XmlDocument Xml = await this.Get("coap://vs0.inf.ethz.ch/multi-format", new CoapOptionAccept(41)) as XmlDocument;
-			Assert.NotNull(Xml);
+			AssertNotNull(Xml);
 		}
 
-		[Test]
-		public async Task Test_11_Hierarchical()
+		private static void AssertNotNull(object Obj)
+		{
+			Assert.IsTrue(Obj != null);
+		}
+
+		[TestMethod]
+		public async Task CoAP_Test_11_Hierarchical()
 		{
 			// Hierarchical link description entry
-			Assert.NotNull(await this.Get("coap://vs0.inf.ethz.ch/path") as LinkDocument);
-			Assert.NotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub1") as string);
-			Assert.NotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub2") as string);
-			Assert.NotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub3") as string);
+			AssertNotNull(await this.Get("coap://vs0.inf.ethz.ch/path") as LinkDocument);
+			AssertNotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub1") as string);
+			AssertNotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub2") as string);
+			AssertNotNull(await this.Get("coap://vs0.inf.ethz.ch/path/sub3") as string);
 		}
 
-		[Test]
-		public async Task Test_12_Query()
+		[TestMethod]
+		public async Task CoAP_Test_12_Query()
 		{
 			// Hierarchical link description entry
-			Assert.NotNull(await this.Get("coap://vs0.inf.ethz.ch/query?A=1&B=2") as string);
+			AssertNotNull(await this.Get("coap://vs0.inf.ethz.ch/query?A=1&B=2") as string);
 		}
 
-		[Test]
-		public async Task Test_13_Observable()
+		[TestMethod]
+		public async Task CoAP_Test_13_Observable()
 		{
 			// Observable resource which changes every 5 seconds
-			Assert.NotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs") as string);
+			AssertNotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs") as string);
 		}
 
-		[Test]
-		public async Task Test_14_Observable_Large()
+		[TestMethod]
+		public async Task CoAP_Test_14_Observable_Large()
 		{
 			// Observable resource which changes every 5 seconds
-			Assert.NotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-large") as string);
+			AssertNotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-large") as string);
 		}
 
-		[Test]
-		public async Task Test_15_Observable_NON()
+		[TestMethod]
+		public async Task CoAP_Test_15_Observable_NON()
 		{
 			// Observable resource which changes every 5 seconds
-			Assert.NotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-non") as string);
+			AssertNotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-non") as string);
 		}
 
-		[Test]
-		public async Task Test_16_Observable_Pumping()
+		[TestMethod]
+		public async Task CoAP_Test_16_Observable_Pumping()
 		{
 			// Observable resource which changes every 5 seconds
-			Assert.NotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-pumping") as string);
+			AssertNotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-pumping") as string);
 		}
 
-		[Test]
-		public async Task Test_17_Observable_Pumping_NON()
+		[TestMethod]
+		public async Task CoAP_Test_17_Observable_Pumping_NON()
 		{
 			// Observable resource which changes every 5 seconds
-			Assert.NotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-pumping-non") as string);
+			AssertNotNull(await this.Observe("coap://vs0.inf.ethz.ch/obs-pumping-non") as string);
 		}
 
-		[Test]
-		public async Task Test_18_POST()
+		[TestMethod]
+		public async Task CoAP_Test_18_POST()
 		{
 			// Perform POST transaction with responses containing several Location-Query options (CON mode)
 			await this.Post("coap://vs0.inf.ethz.ch/location-query", Encoding.UTF8.GetBytes("Hello"), 64, new CoapOptionContentFormat(0));
 		}
 
-		[Test]
-		public async Task Test_19_POST_Large()
+		[TestMethod]
+		public async Task CoAP_Test_19_POST_Large()
 		{
 			// Large resource that can be created using POST method
 
@@ -314,8 +332,8 @@ namespace Waher.Networking.CoAP.Test
 			await this.Post("coap://vs0.inf.ethz.ch/large-create", Encoding.UTF8.GetBytes(s), 64, new CoapOptionContentFormat(0));
 		}
 
-		[Test]
-		public async Task Test_20_POST_Large_Response()
+		[TestMethod]
+		public async Task CoAP_Test_20_POST_Large_Response()
 		{
 			// Handle POST with two-way blockwise transfer
 
@@ -327,15 +345,15 @@ namespace Waher.Networking.CoAP.Test
 			await this.Post("coap://vs0.inf.ethz.ch/large-post", Encoding.UTF8.GetBytes(s), 64, new CoapOptionContentFormat(0));
 		}
 
-		[Test]
-		public async Task Test_21_PUT()
+		[TestMethod]
+		public async Task CoAP_Test_21_PUT()
 		{
 			// Large resource that can be updated using PUT method
 			await this.Put("coap://vs0.inf.ethz.ch/large-update", Encoding.UTF8.GetBytes("Hello"), 64, new CoapOptionContentFormat(0));
 		}
 
-		[Test]
-		public async Task Test_22_PUT_Large()
+		[TestMethod]
+		public async Task CoAP_Test_22_PUT_Large()
 		{
 			// Large resource that can be updated using PUT method
 
--- Networking/Waher.Networking.CoAP.Test/Properties/AssemblyInfo.cs
+++ Networking/Waher.Networking.CoAP.Test/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Waher.Networking.CoAP.Test")]
 [assembly: AssemblyDescription("Contains CoAP-based unit tests.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,22 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("IoT Gateway")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b63d622-de76-4be9-9da6-fda1cde94f9f")]
+[assembly: Guid("1f54cdb8-b9bb-4fd5-beb8-f2b81a59a0b9")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

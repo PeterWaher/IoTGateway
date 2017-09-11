080c18ea73da6e8f578c3e20b7a0c465837add01
Peter Waher <peterwaher@hotmail.com>
2017-09-11 10:59:45 +0200

    Renaming test classes

---
+++ Persistence/Waher.Persistence.Files.Test/DBFilesGuidTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Waher.Persistence.Files.Storage;
+
+#if !LW
+namespace Waher.Persistence.Files.Test
+#else
+using Waher.Persistence.Files;
+namespace Waher.Persistence.FilesLW.Test
+#endif
+{
+	[TestClass]
+	public class DBFilesGuidTests
+	{
+		private static SequentialGuidGenerator gen;
+
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext Context)
+		{
+			gen = new SequentialGuidGenerator();
+		}
+
+		[ClassCleanup]
+		public static void ClassCleanup()
+		{
+			if (gen != null)
+			{
+				gen.Dispose();
+				gen = null;
+			}
+		}
+
+		[TestMethod]
+		public void Test_01_Generate1()
+		{
+			Console.Out.WriteLine(gen.CreateGuid().ToString());
+		}
+
+		[TestMethod]
+		public void Test_02_Generate100()
+		{
+			Guid Prev, Next;
+			int i;
+
+			Prev = gen.CreateGuid();
+			Console.Out.WriteLine(Prev.ToString());
+
+			for (i = 1; i < 100; i++)
+			{
+				Next = gen.CreateGuid();
+				Console.Out.WriteLine(Next.ToString());
+
+				AssertEx.Less(Prev, Next);
+
+				Prev = Next;
+			}
+		}
+
+		[TestMethod]
+		public void Test_03_Generate1000000()
+		{
+			Guid Prev, Next;
+			int i;
+
+			Prev = gen.CreateGuid();
+
+			for (i = 1; i < 1000000; i++)
+			{
+				Next = gen.CreateGuid();
+				AssertEx.Less(Prev, Next);
+				AssertEx.Less(Prev.ToString(), Next.ToString());
+				Prev = Next;
+			}
+		}
+	}
+}
---
+++ Persistence/Waher.Persistence.Files.Test/DBFilesProviderTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Waher.Content;
+using Waher.Persistence.Files.Serialization;
+using Waher.Persistence.Files.Statistics;
+using Waher.Runtime.Inventory;
+
+#if !LW
+using Waher.Persistence.Files.Test.Classes;
+
+namespace Waher.Persistence.Files.Test
+#else
+using Waher.Persistence.Files;
+using Waher.Persistence.FilesLW.Test.Classes;
+
+namespace Waher.Persistence.FilesLW.Test
+#endif
+{
+	[TestClass]
+	public class DBFilesProviderTests
+	{
+		internal const int BlocksInCache = 10000;
+
+		protected FilesProvider provider;
+		private ObjectBTreeFile file;
+
+		[TestInitialize]
+		public async Task TestInitialize()
+		{
+			if (File.Exists(DBFilesBTreeTests.MasterFileName + ".bak"))
+				File.Delete(DBFilesBTreeTests.MasterFileName + ".bak");
+
+			if (File.Exists(DBFilesBTreeTests.MasterFileName))
+			{
+				File.Copy(DBFilesBTreeTests.MasterFileName, DBFilesBTreeTests.MasterFileName + ".bak");
+				File.Delete(DBFilesBTreeTests.MasterFileName);
+			}
+
+			if (File.Exists(DBFilesBTreeTests.FileName + ".bak"))
+				File.Delete(DBFilesBTreeTests.FileName + ".bak");
+
+			if (File.Exists(DBFilesBTreeTests.FileName))
+			{
+				File.Copy(DBFilesBTreeTests.FileName, DBFilesBTreeTests.FileName + ".bak");
+				File.Delete(DBFilesBTreeTests.FileName);
+			}
+
+			if (File.Exists(DBFilesBTreeTests.BlobFileName + ".bak"))
+				File.Delete(DBFilesBTreeTests.BlobFileName + ".bak");
+
+			if (File.Exists(DBFilesBTreeTests.BlobFileName))
+			{
+				File.Copy(DBFilesBTreeTests.BlobFileName, DBFilesBTreeTests.BlobFileName + ".bak");
+				File.Delete(DBFilesBTreeTests.BlobFileName);
+			}
+
+			this.provider = new FilesProvider("Data", DBFilesBTreeTests.CollectionName, 8192, BlocksInCache, 8192, Encoding.UTF8, 10000, true);
+			this.file = await this.provider.GetFile("Default");
+		}
+
+		[TestCleanup]
+		public void TestCleanup()
+		{
+			if (this.provider != null)
+			{
+				this.provider.Dispose();
+				this.provider = null;
+				this.file = null;
+			}
+		}
+
+		[TestMethod]
+		public async Task Test_01_ByReference()
+		{
+			ByReference Obj = new ByReference()
+			{
+				Default = DBFilesBTreeTests.CreateDefault(100),
+				Simple = DBFilesBTreeTests.CreateSimple(100)
+			};
+
+			await this.provider.Insert(Obj);
+
+			ObjectBTreeFile File = await this.provider.GetFile("Default");
+			await DBFilesBTreeTests.AssertConsistent(File, this.provider, 3, Obj, true);
+			Console.Out.WriteLine(await DBFilesBTreeTests.ExportXML(File, "Data\\BTree.xml"));
+
+			Assert.AreNotEqual(Guid.Empty, Obj.ObjectId);
+			Assert.AreNotEqual(Guid.Empty, Obj.Default.ObjectId);
+			Assert.AreNotEqual(Guid.Empty, Obj.Simple.ObjectId);
+
+			ByReference Obj2 = await this.provider.LoadObject<ByReference>(Obj.ObjectId);
+
+			DBFilesObjectSerializationTests.AssertEqual(Obj2.Default, Obj.Default);
+			DBFilesObjectSerializationTests.AssertEqual(Obj2.Simple, Obj.Simple);
+		}
+
+		// TODO: Solve deadlocks.
+		// TODO: Multi-threaded stress test (with multiple indices).
+		// TOOO: Test huge databases with more than uint.MaxValue objects.
+		// TODO: Startup: Scan file if not shut down correctly. Rebuild in case file is corrupt
+	}
+}

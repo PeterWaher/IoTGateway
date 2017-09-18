3734c95c4d53c630ceefe7e859a23edea570325e
Peter Waher <peterwaher@hotmail.com>
2017-09-18 18:53:49 +0200

    Removing references

--- Script/Waher.Script.Graphs/Graph2D.cs
+++ Script/Waher.Script.Graphs/Graph2D.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using SkiaSharp;
-using System.Text;
-using Waher.Script;
 using Waher.Script.Abstraction.Elements;
 using Waher.Script.Abstraction.Sets;
 using Waher.Script.Exceptions;

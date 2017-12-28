c8e53fe0bed7b0eb01c39c0383247e8bffb1f247
Peter Waher <peterwaher@hotmail.com>
2017-12-28 22:31:09 +0100

    Starting and stopping gateway

--- Waher.IoTGateway.App/App.xaml.cs
+++ Waher.IoTGateway.App/App.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,87 +17,157 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Waher.Events;
 
 namespace Waher.IoTGateway.App
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
-            Frame rootFrame = Window.Current.Content as Frame;
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+			Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+				rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Load state from previously suspended application
+				}
 
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
-        }
+			if (e.PrelaunchActivated == false)
+			{
+				if (rootFrame.Content == null)
+				{
+					// When the navigation stack isn't restored navigate to the first page,
+					// configuring the new page by passing required information as a navigation
+					// parameter
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				}
+				// Ensure the current window is active
+				Window.Current.Activate();
+				Task.Run((Action)this.Init);
+			}
+		}
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
+		private async void Init()
+		{
+			try
+			{
+				Log.RegisterExceptionToUnnest(typeof(System.Runtime.InteropServices.ExternalException));
+				Log.RegisterExceptionToUnnest(typeof(System.Security.Authentication.AuthenticationException));
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
-    }
+				AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+				{
+					if (e.IsTerminating)
+					{
+						using (StreamWriter w = File.CreateText(Path.Combine(Gateway.AppDataFolder, "UnhandledException.txt")))
+						{
+							w.Write("Type: ");
+
+							if (e.ExceptionObject != null)
+								w.WriteLine(e.ExceptionObject.GetType().FullName);
+							else
+								w.WriteLine("null");
+
+							w.Write("Time: ");
+							w.WriteLine(DateTime.Now.ToString());
+
+							w.WriteLine();
+							if (e.ExceptionObject is Exception ex)
+							{
+								w.WriteLine(ex.Message);
+								w.WriteLine();
+								w.WriteLine(ex.StackTrace);
+							}
+							else
+							{
+								if (e.ExceptionObject != null)
+									w.WriteLine(e.ExceptionObject.ToString());
+
+								w.WriteLine();
+								w.WriteLine(Environment.StackTrace);
+							}
+
+							w.Flush();
+						}
+					}
+
+					if (e.ExceptionObject is Exception ex2)
+						Log.Critical(ex2);
+					else if (e.ExceptionObject != null)
+						Log.Critical(e.ExceptionObject.ToString());
+					else
+						Log.Critical("Unexpected null exception thrown.");
+				};
+
+				if (!Gateway.Start(false, false))
+					throw new Exception("Gateway being started in another process.");
+			}
+			catch (Exception ex)
+			{
+				Log.Emergency(ex);
+
+				MessageDialog Dialog = new MessageDialog(ex.Message, "Error");
+				await MainPage.Instance.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+					async () => await Dialog.ShowAsync());
+			}
+		}
+
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+
+			Gateway.Stop();
+			Log.Terminate();
+
+			deferral.Complete();
+		}
+	}
 }
--- Waher.IoTGateway.App/MainPage.xaml.cs
+++ Waher.IoTGateway.App/MainPage.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Waher.Events;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,122 @@ namespace Waher.IoTGateway.App
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+		private static MainPage instance = null;
+		private Events events;
+
+		public MainPage()
         {
             this.InitializeComponent();
-        }
-    }
+
+			this.events = new MainPage.Events();
+			Log.Register(this.events);
+
+			if (instance == null)
+				instance = this;
+
+			Hyperlink Link = new Hyperlink();
+
+			Link.Inlines.Add(new Run()
+			{
+				Text = Windows.Storage.ApplicationData.Current.LocalFolder.Path
+			});
+
+			Link.Click += Link_Click;
+
+			ToolTip ToolTip = new ToolTip()
+			{
+				Content = "If provisioning is used, you will find the iotdisco URI in this folder. Use this URI to claim the device. " +
+					"Erase content of this folder when application is closed, and then restart, to reconfigure the application."
+			};
+
+			ToolTipService.SetToolTip(Link, ToolTip);
+
+			this.LocalFolder.Inlines.Add(new Run() { Text = " " });
+			this.LocalFolder.Inlines.Add(Link);
+		}
+
+		private async void Link_Click(Hyperlink sender, HyperlinkClickEventArgs args)
+		{
+			try
+			{
+				await Windows.System.Launcher.LaunchFolderAsync(Windows.Storage.ApplicationData.Current.LocalFolder);
+			}
+			catch (Exception ex)
+			{
+				MessageDialog Dialog = new MessageDialog(ex.Message, "Error");
+				await MainPage.Instance.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+					async () => await Dialog.ShowAsync());
+			}
+		}
+
+		private void Page_Unloaded(object sender, RoutedEventArgs e)
+		{
+			Log.Unregister(this.events);
+			this.events = null;
+
+			if (instance == this)
+				instance = null;
+		}
+
+		public static MainPage Instance
+		{
+			get { return instance; }
+		}
+
+		public async void AddLogMessage(string Message)
+		{
+			DateTime TP = DateTime.Now;
+
+			await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				this.EventsPanel.Children.Insert(0, new TextBlock() { Text = Message, TextWrapping = TextWrapping.Wrap });
+
+				while (this.EventsPanel.Children.Count > 1000)
+					this.EventsPanel.Children.RemoveAt(1000);
+			});
+		}
+
+		private class Events : EventSink
+		{
+			public Events() : base(string.Empty)
+			{
+			}
+
+			public override void Queue(Event Event)
+			{
+				StringBuilder sb = new StringBuilder(Event.Message);
+
+				if (!string.IsNullOrEmpty(Event.Object))
+				{
+					sb.Append(' ');
+					sb.Append(Event.Object);
+				}
+
+				if (!string.IsNullOrEmpty(Event.Actor))
+				{
+					sb.Append(' ');
+					sb.Append(Event.Actor);
+				}
+
+				foreach (KeyValuePair<string, object> Parameter in Event.Tags)
+				{
+					sb.Append(" [");
+					sb.Append(Parameter.Key);
+					sb.Append("=");
+					if (Parameter.Value != null)
+						sb.Append(Parameter.Value.ToString());
+					sb.Append("]");
+				}
+
+				if (Event.Type >= EventType.Critical && !string.IsNullOrEmpty(Event.StackTrace))
+				{
+					sb.Append("\r\n\r\n");
+					sb.Append(Event.StackTrace);
+				}
+
+				MainPage.Instance.AddLogMessage(sb.ToString());
+			}
+		}
+
+	}
 }

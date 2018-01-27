a4cad626ede5f5e693a51ae6a38340cedcbd42eb
Peter Waher <peterwaher@hotmail.com>
2018-01-27 17:43:03 +0100

    Adding chat bot

---
+++ Waher.IoTGateway.App/ChatModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Waher.Networking.XMPP.BitsOfBinary;
+using Waher.Networking.XMPP.Chat;
+using Waher.Networking.XMPP.Concentrator;
+using Waher.Networking.XMPP.Provisioning;
+using Waher.Runtime.Inventory;
+
+namespace Waher.IoTGateway.App
+{
+	public class ChatModule : IModule
+	{
+		private ChatServer chatServer = null;
+		private BobClient bobClient = null;
+
+		public ChatModule()
+		{
+		}
+
+		public WaitHandle Start()
+		{
+			ConcentratorServer ConcentratorServer = null;
+			ProvisioningClient ProvisioningClient = null;
+
+			if (Types.TryGetModuleParameter("Concentrator", out object Obj))
+				ConcentratorServer = Obj as ConcentratorServer;
+
+			if (Types.TryGetModuleParameter("Provisioning", out Obj))
+				ProvisioningClient = Obj as ProvisioningClient;
+
+			this.bobClient = new BobClient(Gateway.XmppClient, Path.Combine(Gateway.AppDataFolder, "BoB"));
+			this.chatServer = new ChatServer(Gateway.XmppClient, this.bobClient, ConcentratorServer, ProvisioningClient);
+
+			return null;
+		}
+
+		public void Stop()
+		{
+			this.chatServer?.Dispose();
+			this.chatServer = null;
+
+			this.bobClient?.Dispose();
+			this.bobClient = null;
+		}
+	}
+}

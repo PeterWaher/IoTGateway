56a3dd600bbbb3cb427a7ff7bff56733fab62628
Peter Waher <peterwaher@hotmail.com>
2018-01-27 16:31:39 +0100

    Calculated field value.

--- Things/Waher.Things.Arduino/AnalogInput.cs
+++ Things/Waher.Things.Arduino/AnalogInput.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Maker.RemoteWiring;
 using Waher.IoTGateway;
+using Waher.Content;
 using Waher.Persistence.Attributes;
 using Waher.Runtime.Language;
+using Waher.Script;
+using Waher.Script.Objects;
 using Waher.Things.Attributes;
 using Waher.Things.DisplayableParameters;
 using Waher.Things.SensorData;
@@ -21,6 +22,11 @@ namespace Waher.Things.Arduino
 	public class AnalogInput : AnalogPin, ISensor
 	{
 		private AnalogInputPinMode mode = AnalogInputPinMode.Input;
+		private string expression = "Raw";
+		private string fieldName = "Value";
+		private string unit = string.Empty;
+		private Expression exp = null;
+		private byte nrDecimals = 0;
 		private bool initialized = false;
 
 		public AnalogInput()
@@ -29,7 +35,7 @@ namespace Waher.Things.Arduino
 		}
 
 		[Page(5, "Arduino")]
-		[Header(9, "Mode:")]
+		[Header(9, "Mode:", 20)]
 		[ToolTip(10, "Select drive mode of pin.")]
 		[DefaultValue(AnalogInputPinMode.Input)]
 		[Option(AnalogInputPinMode.Input, 11, "Input")]
@@ -41,6 +47,54 @@ namespace Waher.Things.Arduino
 				this.mode = value;
 				this.Initialize();
 			}
+		}
+
+		[Page(5, "Arduino")]
+		[Header(24, "Field Name:", 30)]
+		[ToolTip(25, "Name of calculated field.")]
+		[DefaultValue("Value")]
+		public string FieldName
+		{
+			get { return this.fieldName; }
+			set { this.fieldName = value; }
+		}
+
+		[Page(5, "Arduino")]
+		[Header(20, "Value Expression:", 40)]
+		[ToolTip(21, "Enter expression to scale the raw value into a physical quantity. The raw value will be available in the variable 'Raw'. Any script construct can be used, including adding units. For more information, see: https://waher.se/Script.md")]
+		[DefaultValue("Raw")]
+		public string Expression
+		{
+			get { return this.expression; }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+					this.exp = null;
+				else
+					this.exp = new Expression(value);
+
+				this.expression = value;
+			}
+		}
+
+		[Page(5, "Arduino")]
+		[Header(27, "Unit:", 50)]
+		[ToolTip(28, "Unit to use, if one is not provided by the expression above.")]
+		[DefaultValueStringEmpty]
+		public string Unit
+		{
+			get { return this.unit; }
+			set { this.unit = value; }
+		}
+
+		[Page(5, "Arduino")]
+		[Header(22, "Number of decimals:", 60)]
+		[ToolTip(23, "Number of decimals to use when presenting calculated value.")]
+		[DefaultValue(0)]
+		public byte NrDecimals
+		{
+			get { return this.nrDecimals; }
+			set { this.nrDecimals = value; }
 		}
 
 		public override Task<string> GetTypeNameAsync(Language Language)
@@ -74,12 +128,18 @@ namespace Waher.Things.Arduino
 				RemoteDevice Device = this.Device;
 				if (Device == null)
 					throw new Exception("Device not ready.");
-					
+
 				List<Field> Fields = new List<Field>();
 				DateTime Now = DateTime.Now;
 
 				if (!this.initialized)
 					this.Initialize();
+
+				if (Request.IsIncluded(FieldType.Momentary))
+				{
+					ushort Raw = Device.analogRead(this.PinNrStr);
+					this.CalcMomentary(Fields, Now, Raw);
+				}
 
 				if (Request.IsIncluded(FieldType.Status))
 				{
@@ -87,12 +147,6 @@ namespace Waher.Things.Arduino
 						typeof(Module).Namespace, 15));
 				}
 
-				if (Request.IsIncluded(FieldType.Momentary))
-				{
-					Fields.Add(new Int32Field(this, Now, "Value", Device.analogRead(this.PinNrStr), FieldType.Momentary, FieldQoS.AutomaticReadout,
-						typeof(Module).Namespace, 13));
-				}
-				
 				if (Request.IsIncluded(FieldType.Identity))
 				{
 					Fields.Add(new Int32Field(this, Now, "Pin Number", this.PinNr, FieldType.Identity, FieldQoS.AutomaticReadout,
@@ -107,10 +161,74 @@ namespace Waher.Things.Arduino
 			}
 		}
 
+		private void CalcMomentary(List<Field> Fields, DateTime Now, ushort Raw)
+		{
+			Fields.Add(new Int32Field(this, Now, "Raw", Raw, FieldType.Momentary, FieldQoS.AutomaticReadout, typeof(Module).Namespace, 26));
+
+			if (this.exp == null && !string.IsNullOrEmpty(this.expression))
+				this.exp = new Expression(this.expression);
+
+			if (this.exp != null)
+			{
+				Variables v = new Variables()
+				{
+					{ "Raw", (double)Raw }
+				};
+
+				object Value = this.exp.Evaluate(v);
+				Field F;
+
+				if (Value is double dbl)
+					F = new QuantityField(this, Now, this.fieldName, dbl, this.nrDecimals, this.unit, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is PhysicalQuantity qty)
+					F = new QuantityField(this, Now, this.fieldName, qty.Magnitude, this.nrDecimals, qty.Unit.ToString(), FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is bool b)
+					F = new BooleanField(this, Now, this.fieldName, b, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is DateTime DT)
+					F = new DateTimeField(this, Now, this.fieldName, DT, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is Duration D)
+					F = new DurationField(this, Now, this.fieldName, D, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is Enum E)
+					F = new EnumField(this, Now, this.fieldName, E, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is int i32)
+				{
+					if (string.IsNullOrEmpty(this.unit))
+						F = new Int32Field(this, Now, this.fieldName, i32, FieldType.Momentary, FieldQoS.AutomaticReadout);
+					else
+						F = new QuantityField(this, Now, this.fieldName, i32, 0, this.unit, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				}
+				else if (Value is long i64)
+				{
+					if (string.IsNullOrEmpty(this.unit))
+						F = new Int64Field(this, Now, this.fieldName, i64, FieldType.Momentary, FieldQoS.AutomaticReadout);
+					else
+						F = new QuantityField(this, Now, this.fieldName, i64, 0, this.unit, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				}
+				else if (Value is string s)
+					F = new StringField(this, Now, this.fieldName, s, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else if (Value is TimeSpan TS)
+					F = new TimeField(this, Now, this.fieldName, TS, FieldType.Momentary, FieldQoS.AutomaticReadout);
+				else
+					F = new StringField(this, Now, this.fieldName, Value.ToString(), FieldType.Momentary, FieldQoS.AutomaticReadout);
+
+				if (this.fieldName == "Value")
+				{
+					F.Module = typeof(Module).Namespace;
+					F.StringIdSteps = new LocalizationStep[] { new LocalizationStep(13) };
+				}
+
+				Fields.Add(F);
+			}
+		}
+
 		public void Pin_ValueChanged(ushort Value)
 		{
-			Gateway.NewMomentaryValues(this, new Int32Field(this, DateTime.Now, "Value", Value, FieldType.Momentary, FieldQoS.AutomaticReadout,
-				typeof(Module).Namespace, 13));
+			List<Field> Fields = new List<Field>();
+
+			this.CalcMomentary(Fields, DateTime.Now, Value);
+
+			foreach (Field F in Fields)
+				Gateway.NewMomentaryValues(this, F);
 		}
 
 		public override async Task<IEnumerable<Parameter>> GetDisplayableParametersAsync(Language Language, RequestOrigin Caller)

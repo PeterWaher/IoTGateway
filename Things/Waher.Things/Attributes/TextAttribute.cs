2b3da4539c7fe04b08b39afe6f2fc151e46d9c3e
Peter Waher <peterwaher@hotmail.com>
2018-01-28 16:49:03 +0100

    Position of text content in property forms

--- Things/Waher.Things/Attributes/TextAttribute.cs
+++ Things/Waher.Things/Attributes/TextAttribute.cs
@@ -7,32 +7,60 @@ using System.Threading.Tasks;
 namespace Waher.Things.Attributes
 {
 	/// <summary>
+	/// Where the instructions are to be place.
+	/// </summary>
+	public enum TextPosition
+	{
+		/// <summary>
+		/// Before the field.
+		/// </summary>
+		BeforeField,
+
+		/// <summary>
+		/// After the field.
+		/// </summary>
+		AfterField
+	}
+
+	/// <summary>
 	/// Shows a text segment before the parameter.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
 	public class TextAttribute : Attribute
 	{
+		private TextPosition position;
 		private int stringId;
 		private string label;
 
 		/// <summary>
 		/// Shows a text segment before the parameter.
 		/// </summary>
+		/// <param name="Position">Position of text in relation to the field.</param>
 		/// <param name="Label">Label string</param>
-		public TextAttribute(string Label)
-			: this(0, Label)
+		public TextAttribute(TextPosition Position, string Label)
+			: this(Position, 0, Label)
 		{
 		}
 
 		/// <summary>
 		/// Shows a text segment before the parameter.
 		/// </summary>
+		/// <param name="Position">Position of text in relation to the field.</param>
 		/// <param name="StringId">String ID in the namespace of the current class, in the default language defined for the class.</param>
 		/// <param name="Label">Default label string, in the default language defined for the class.</param>
-		public TextAttribute(int StringId, string Label)
+		public TextAttribute(TextPosition Position, int StringId, string Label)
 		{
+			this.position = Position;
 			this.stringId = StringId;
 			this.label = Label;
+		}
+
+		/// <summary>
+		/// Where the text is to be placed in relation to the field.
+		/// </summary>
+		public TextPosition Position
+		{
+			get { return this.position; }
 		}
 
 		/// <summary>

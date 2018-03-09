0f9f41e6eff87a9aa4471569aba1a42e490dc4e8
Peter Waher <peterwaher@hotmail.com>
2018-03-09 20:53:08 +0100

    Result set management

---
+++ Networking/Waher.Networking.XMPP/ResultSetManagement/RestrictedQuery.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Waher.Content.Xml;
+
+namespace Waher.Networking.XMPP.ResultSetManagement
+{
+	/// <summary>
+	/// Contains information about a restricted query, as deinfed in XEP-0059: Result Set Management
+	/// </summary>
+    public class RestrictedQuery
+    {
+		/// <summary>
+		/// http://jabber.org/protocol/rsm
+		/// </summary>
+		public const string NamespaceResultSetManagement = "http://jabber.org/protocol/rsm";
+
+		private string after = null;
+		private string before = null;
+		private int? index = null;
+		private int? max = null;
+
+		/// <summary>
+		/// Contains information about a restricted query, as deinfed in XEP-0059: Result Set Management
+		/// </summary>
+		/// <param name="Rsm">XML definition</param>
+		public RestrictedQuery(XmlElement Rsm)
+		{
+			foreach (XmlNode N in Rsm.ChildNodes)
+			{
+				if (N is XmlElement E)
+				{
+					switch (E.LocalName)
+					{
+						case "after":
+							this.after = E.InnerText;
+							break;
+
+						case "before":
+							this.before = E.InnerText;
+							break;
+
+						case "index":
+							if (int.TryParse(E.InnerText, out int i))
+								this.index = i;
+							break;
+
+						case "max":
+							if (int.TryParse(E.InnerText, out i))
+								this.max = i;
+							break;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Return items after this key.
+		/// </summary>
+		public string After
+		{
+			get { return this.after; }
+		}
+
+		/// <summary>
+		/// Return items before this key.
+		/// </summary>
+		public string Before
+		{
+			get { return this.before; }
+		}
+
+		/// <summary>
+		/// Request results from this index.
+		/// </summary>
+		public int? Index
+		{
+			get { return this.index; }
+		}
+
+		/// <summary>
+		/// If result set should be limited in size.
+		/// </summary>
+		public int? Max
+		{
+			get { return this.max; }
+		}
+
+		/// <summary>
+		/// Searches for a restricted query, by traversing siblings.
+		/// </summary>
+		/// <param name="FirstSibling">First sibling.</param>
+		/// <returns>Restricted query, if found, null otherwise.</returns>
+		public static RestrictedQuery IsRestricted(XmlNode FirstSibling)
+		{
+			while (FirstSibling != null && (FirstSibling.LocalName != "set" || FirstSibling.NamespaceURI != NamespaceResultSetManagement))
+				FirstSibling = FirstSibling.NextSibling;
+
+			if (FirstSibling != null && FirstSibling is XmlElement E)
+				return new RestrictedQuery(E);
+			else
+				return null;
+		}
+
+		/// <summary>
+		/// Appends pagination information to an XML request.
+		/// </summary>
+		/// <param name="Xml">XML output.</param>
+		public void Append(StringBuilder Xml)
+		{
+			Append(Xml, this.after, this.before, this.index, this.max);
+		}
+
+		/// <summary>
+		/// Appends pagination information to an XML request.
+		/// </summary>
+		/// <param name="Xml">XML output.</param>
+		/// <param name="After">Fetch records after this record.</param>
+		/// <param name="Before">Fetch records before this record.</param>
+		/// <param name="Index">Fetch records starting at this index.</param>
+		/// <param name="MaxCount">Return at most this number of items.</param>
+		public static void Append(StringBuilder Xml, string After, string Before, int? Index, int? MaxCount)
+		{
+			Xml.Append("<set xmlns='");
+			Xml.Append(NamespaceResultSetManagement);
+			Xml.Append("'>");
+
+			if (After != null)
+			{
+				Xml.Append("<after>");
+				Xml.Append(XML.Encode(After));
+				Xml.Append("</after>");
+			}
+
+			if (Before != null)
+			{
+				Xml.Append("<before>");
+				Xml.Append(XML.Encode(Before));
+				Xml.Append("</before>");
+			}
+
+			if (Index.HasValue)
+			{
+				Xml.Append("<index>");
+				Xml.Append(Index.Value.ToString());
+				Xml.Append("</index>");
+			}
+
+			if (MaxCount.HasValue)
+			{
+				Xml.Append("<max>");
+				Xml.Append(MaxCount.Value.ToString());
+				Xml.Append("</max>");
+			}
+
+			Xml.Append("</set>");
+		}
+	}
+}
---
+++ Networking/Waher.Networking.XMPP/ResultSetManagement/ResultPage.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Waher.Content.Xml;
+
+namespace Waher.Networking.XMPP.ResultSetManagement
+{
+	/// <summary>
+	/// Contains information about a result page, as deinfed in XEP-0059: Result Set Management
+	/// </summary>
+	public class ResultPage
+	{
+		private string first = null;
+		private string last = null;
+		private int? count = null;
+		private int? firstIndex = null;
+
+		/// <summary>
+		/// Contains information about a restricted query, as deinfed in XEP-0059: Result Set Management
+		/// </summary>
+		/// <param name="Rsm">XML definition</param>
+		public ResultPage(XmlElement Rsm)
+		{
+			foreach (XmlNode N in Rsm.ChildNodes)
+			{
+				if (N is XmlElement E)
+				{
+					switch (E.LocalName)
+					{
+						case "count":
+							if (int.TryParse(E.InnerText, out int i))
+								this.count = i;
+							break;
+
+						case "first":
+							this.first = E.InnerText;
+							if (E.HasAttribute("index") && int.TryParse(E["index"].Value, out i))
+								this.firstIndex = i;
+							break;
+
+						case "last":
+							this.last = E.InnerText;
+							break;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// First item/key in response.
+		/// </summary>
+		public string First
+		{
+			get { return this.first; }
+		}
+
+		/// <summary>
+		/// Last item/key in response.
+		/// </summary>
+		public string Last
+		{
+			get { return this.last; }
+		}
+
+		/// <summary>
+		/// Total number of items in result set.
+		/// </summary>
+		public int? Count
+		{
+			get { return this.count; }
+		}
+
+		/// <summary>
+		/// Index of first response.
+		/// </summary>
+		public int? FirstIndex
+		{
+			get { return this.firstIndex; }
+		}
+
+		/// <summary>
+		/// Checks if the result is paginated.
+		/// </summary>
+		/// <param name="FirstSibling">First sibling.</param>
+		/// <returns>Pagination, if found, null otherwise.</returns>
+		public static ResultPage IsPaginated(XmlNode FirstSibling)
+		{
+			while (FirstSibling != null && (FirstSibling.LocalName != "set" || FirstSibling.NamespaceURI != RestrictedQuery.NamespaceResultSetManagement))
+				FirstSibling = FirstSibling.NextSibling;
+
+			if (FirstSibling != null && FirstSibling is XmlElement E)
+				return new ResultPage(E);
+			else
+				return null;
+		}
+
+		/// <summary>
+		/// Appends pagination information to an XML request.
+		/// </summary>
+		/// <param name="Xml">XML output.</param>
+		public void Append(StringBuilder Xml)
+		{
+			Append(Xml, this.first, this.firstIndex, this.last, this.count);
+		}
+
+		/// <summary>
+		/// Appends pagination information to an XML result response.
+		/// </summary>
+		/// <param name="Xml">XML output.</param>
+		/// <param name="First">First element.</param>
+		/// <param name="FirstIndex">Index of first element.</param>
+		/// <param name="Last">Last element.</param>
+		/// <param name="Count">Number of items in result set.</param>
+		public static void Append(StringBuilder Xml, string First, int? FirstIndex, string Last, int? Count)
+		{
+			Xml.Append("<set xmlns='");
+			Xml.Append(RestrictedQuery.NamespaceResultSetManagement);
+			Xml.Append("'>");
+
+			if (First != null)
+			{
+				Xml.Append("<first");
+
+				if (FirstIndex.HasValue)
+				{
+					Xml.Append(" index='");
+					Xml.Append(FirstIndex.Value.ToString());
+					Xml.Append('\'');
+				}
+
+				Xml.Append('>');
+				Xml.Append(XML.Encode(First));
+				Xml.Append("</first>");
+			}
+
+			if (Last != null)
+			{
+				Xml.Append("<last>");
+				Xml.Append(XML.Encode(Last));
+				Xml.Append("</last>");
+			}
+
+			if (Count.HasValue)
+			{
+				Xml.Append("<count>");
+				Xml.Append(Count.Value.ToString());
+				Xml.Append("</count>");
+			}
+
+			Xml.Append("</set>");
+		}
+	}
+}

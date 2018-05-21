cb01357c796e7ac4a87dbb5427672695b90b9585
Peter Waher <peterwaher@hotmail.com>
2018-05-21 12:44:33 +0200

    DER encoder

---
+++ Security/Waher.Security.ACME.Test/DerTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Waher.Security.ACME.Test
+{
+	[TestClass]
+	public class DerTests
+	{
+		private DerEncoder derOutput;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			this.derOutput = new DerEncoder();
+		}
+
+		private void AssertEqualTo(byte[] Expected)
+		{
+			byte[] Bin = this.derOutput.ToArray();
+			int i, c = Bin.Length;
+
+			Assert.AreEqual(Expected.Length, c, "Output length not of expected length.");
+
+			for (i = 0; i < c; i++)
+				Assert.AreEqual(Expected[i], Bin[i], "Byte " + i.ToString() + " contained " + Bin[i].ToString("x2"), " not " + Expected[i].ToString("x2") + ", as expected.");
+		}
+
+		[TestMethod]
+		public void DER_Test_01_BOOLEAN_1()
+		{
+			this.derOutput.BOOLEAN(false);
+			this.AssertEqualTo(new byte[] { 1, 1, 0 });
+		}
+
+		[TestMethod]
+		public void DER_Test_02_BOOLEAN_2()
+		{
+			this.derOutput.BOOLEAN(true);
+			this.AssertEqualTo(new byte[] { 1, 1, 255 });
+		}
+
+		[TestMethod]
+		public void DER_Test_03_INTEGER_1()
+		{
+			this.derOutput.INTEGER(0x8f);
+			this.AssertEqualTo(new byte[] { 2, 2, 0, 0x8f });
+		}
+
+		[TestMethod]
+		public void DER_Test_04_INTEGER_2()
+		{
+			this.derOutput.INTEGER(3);
+			this.AssertEqualTo(new byte[] { 2, 1, 3 });
+		}
+
+		[TestMethod]
+		public void DER_Test_05_INTEGER_3()
+		{
+			this.derOutput.INTEGER(-200);
+			this.AssertEqualTo(new byte[] { 2, 2, 0xff, 0x38 });
+		}
+
+		[TestMethod]
+		public void DER_Test_06_INTEGER_4()
+		{
+			this.derOutput.INTEGER(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+			this.AssertEqualTo(new byte[] { 2, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+		}
+
+		[TestMethod]
+		public void DER_Test_07_BITSTRING_1()
+		{
+			BitArray Bits = new BitArray(20);
+			int i;
+
+			for (i = 0; i < 20; i++)
+				Bits[i] = ((i & 1) == 0);
+
+			this.derOutput.BITSTRING(Bits);
+			this.AssertEqualTo(new byte[] { 3, 4, 4, 0xaa, 0xaa, 0xa0 });
+		}
+
+		[TestMethod]
+		public void DER_Test_08_OCTET_STRING_1()
+		{
+			this.derOutput.OCTET_STRING(new byte[] { 0x1e, 0x08, 0x00, 0x55, 0x00, 0x73, 0x00, 0x65, 0x00, 0x72 });
+			this.AssertEqualTo(new byte[] { 4, 10, 0x1e, 0x08, 0x00, 0x55, 0x00, 0x73, 0x00, 0x65, 0x00, 0x72 });
+		}
+
+		[TestMethod]
+		public void DER_Test_09_NULL_1()
+		{
+			this.derOutput.NULL();
+			this.AssertEqualTo(new byte[] { 5, 0 });
+		}
+
+		[TestMethod]
+		public void DER_Test_10_OBJECT_IDENTIFIER_1()
+		{
+			this.derOutput.OBJECT_IDENTIFIER("1.3.6.1.4.1.311.21.20");
+			this.AssertEqualTo(new byte[] { 6, 9, 0x2b, 0x06, 0x01, 0x04, 0x01, 0x82, 0x37, 0x15, 0x14 });
+		}
+
+		[TestMethod]
+		public void DER_Test_11_UNICODE_STRING_1()
+		{
+			this.derOutput.UNICODE_STRING("CertificateTemplate");
+			this.AssertEqualTo(new byte[] { 0x1e, 0x26, 00, 0x43, 0x00, 0x65, 0x00, 0x72, 0x00, 0x74, 0x00, 0x69, 0x00, 0x66, 0x00, 0x69, 0x00, 0x63, 0x00, 0x61, 0x00, 0x74, 0x00, 0x65, 0x00, 0x54, 0x00, 0x65, 0x00, 0x6d, 0x00, 0x70, 0x00, 0x6c, 0x00, 0x61, 0x00, 0x74, 0x00, 0x65 });
+		}
+
+		[TestMethod]
+		public void DER_Test_12_IA5_STRING_1()
+		{
+			this.derOutput.IA5_STRING("6.0.5361.2");
+			this.AssertEqualTo(new byte[] { 0x16, 0x0a, 0x36, 0x2e, 0x30, 0x2e, 0x35, 0x33, 0x36, 0x31, 0x2e, 0x32 });
+		}
+
+		[TestMethod]
+		public void DER_Test_13_PRINTABLE_STRING_1()
+		{
+			this.derOutput.PRINTABLE_STRING("TestCN");
+			this.AssertEqualTo(new byte[] { 0x13, 0x06, 0x54, 0x65, 0x73, 0x74, 0x43, 0x4e });
+		}
+
+		[TestMethod]
+		public void DER_Test_14_UTF8_STRING_1()
+		{
+			this.derOutput.UTF8_STRING("Hellö");
+			this.AssertEqualTo(new byte[] { 0x0c, 0x06, 72, 101, 108, 108, 195, 182 });
+		}
+
+		[TestMethod]
+		public void DER_Test_15_SEQUENCE_1()
+		{
+			this.derOutput.StartSEQUENCE();
+			this.derOutput.OBJECT_IDENTIFIER("1.2.840.113549.1.1.1");
+			this.derOutput.NULL();
+			this.derOutput.EndSEQUENCE();
+
+			this.AssertEqualTo(new byte[] { 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00 });
+		}
+
+		[TestMethod]
+		public void DER_Test_16_SET_1()
+		{
+			this.derOutput.StartSET();
+			this.derOutput.OBJECT_IDENTIFIER("1.2.840.113549.1.1.1");
+			this.derOutput.NULL();
+			this.derOutput.EndSET();
+
+			this.AssertEqualTo(new byte[] { 0x31, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00 });
+		}
+	}
+}
---
+++ Security/Waher.Security.ACME/DerEncoder.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Waher.Security.ACME
+{
+	/// <summary>
+	/// Encodes data using the Distinguished Encoding Rules (DER), as defined in X.690
+	/// </summary>
+	public class DerEncoder
+	{
+		private List<byte> output = new List<byte>();
+		private LinkedList<List<byte>> stack = null;
+
+		/// <summary>
+		/// Encodes data using the Distinguished Encoding Rules (DER), as defined in X.690
+		/// </summary>
+		public DerEncoder()
+		{
+		}
+
+		/// <summary>
+		/// Clears the output buffer.
+		/// </summary>
+		public void Clear()
+		{
+			this.output.Clear();
+		}
+
+		/// <summary>
+		/// Converts the generated output to a byte arary.
+		/// </summary>
+		/// <returns>Byte array.</returns>
+		public byte[] ToArray()
+		{
+			if (this.stack != null && this.stack.First != null)
+				throw new Exception("DER output not properly closed.");
+
+			return this.output.ToArray();
+		}
+
+		/// <summary>
+		/// Encodes an BOOLEAN value.
+		/// </summary>
+		/// <param name="Value">BOOLEAN value.</param>
+		public void BOOLEAN(bool Value)
+		{
+			this.output.Add(1);
+			this.output.Add(1);
+
+			if (Value)
+				this.output.Add(0xff);
+			else
+				this.output.Add(0x00);
+		}
+
+		/// <summary>
+		/// Encodes an INTEGER value.
+		/// </summary>
+		/// <param name="Value">INTEGER value.</param>
+		public void INTEGER(long Value)
+		{
+			this.output.Add(2);
+			this.EncodeInteger(Value);
+		}
+
+		private void EncodeInteger(long i)
+		{
+			int Pos = this.output.Count;
+			int c = 8;
+			byte b;
+
+			this.output.Add(0);
+
+			if (i < 0)
+			{
+				do
+				{
+					b = (byte)(i >> 56);
+					i <<= 8;
+					c--;
+				}
+				while (b == 0xff);
+
+				if (b < 0x80)
+					this.output.Add(0xff);
+
+				this.output.Add(b);
+
+				while (c-- > 0)
+				{
+					b = (byte)(i >> 56);
+					this.output.Add(b);
+					i <<= 8;
+				}
+			}
+			else if (i == 0)
+				this.output.Add(0);
+			else
+			{
+				do
+				{
+					b = (byte)(i >> 56);
+					i <<= 8;
+					c--;
+				}
+				while (b == 0);
+
+				if (b >= 0x80)
+					this.output.Add(0);
+
+				this.output.Add(b);
+
+				while (c-- > 0)
+				{
+					b = (byte)(i >> 56);
+					this.output.Add(b);
+					i <<= 8;
+				}
+			}
+
+			this.output[Pos] = (byte)(this.output.Count - Pos - 1);
+		}
+
+		/// <summary>
+		/// Encodes an INTEGER value.
+		/// </summary>
+		/// <param name="Value">INTEGER value.</param>
+		public void INTEGER(byte[] Value)
+		{
+			this.output.Add(2);
+			this.EncodeBinary(Value);
+		}
+
+		private void EncodeBinary(byte[] Bin)
+		{
+			int Len = Bin.Length;
+
+			if (Len <= 127)
+			{
+				this.output.Add((byte)Len);
+				this.output.AddRange(Bin);
+			}
+			else
+			{
+				int Pos = this.output.Count;
+				this.EncodeInteger(Len);
+				this.output[Pos] |= 0x80;
+			}
+		}
+
+		/// <summary>
+		/// Encodes an BITSTRING value.
+		/// </summary>
+		/// <param name="Bits">BITSTRING value.</param>
+		public void BITSTRING(BitArray Bits)
+		{
+			this.output.Add(3);
+
+			int NrBits = Bits.Length;
+			int Len = (NrBits + 7) / 8 + 1;
+			int NrUnusedBits = 8 - (NrBits & 7);
+			byte[] Bin = new byte[Len];
+			byte b = 0;
+			int i, j = 0;
+
+			if (NrUnusedBits == 8)
+				NrUnusedBits = 0;
+
+			Bin[j++] = (byte)NrUnusedBits;
+
+			for (i = 0; i < NrBits; i++)
+			{
+				b <<= 1;
+				if (Bits[i])
+					b |= 1;
+
+				if ((i & 7) == 7)
+					Bin[j++] = b;
+			}
+
+			if (NrUnusedBits > 0)
+			{
+				b <<= NrUnusedBits;
+				Bin[j++] = b;
+			}
+
+			this.EncodeBinary(Bin);
+		}
+
+		/// <summary>
+		/// Encodes an OCTET STRING value.
+		/// </summary>
+		/// <param name="Value">OCTET STRING value.</param>
+		public void OCTET_STRING(byte[] Value)
+		{
+			this.output.Add(4);
+			this.EncodeBinary(Value);
+		}
+
+		/// <summary>
+		/// Encodes an NULL value.
+		/// </summary>
+		public void NULL()
+		{
+			this.output.Add(5);
+			this.output.Add(0);
+		}
+
+		/// <summary>
+		/// Encodes an OBJECT IDENTIFIER value.
+		/// </summary>
+		/// <param name="OID">OBJECT IDENTIFIER value.</param>
+		public void OBJECT_IDENTIFIER(string OID)
+		{
+			string[] s = OID.Split('.');
+			int i, c = s.Length;
+			uint[] OID2 = new uint[c];
+
+			for (i = 0; i < c; i++)
+			{
+				if (!uint.TryParse(s[i], out OID2[i]))
+					throw new ArgumentException("Invalid object identifier.", nameof(OID));
+			}
+
+			this.OBJECT_IDENTIFIER(OID2);
+		}
+
+		/// <summary>
+		/// Encodes an OBJECT IDENTIFIER value.
+		/// </summary>
+		/// <param name="OID">OBJECT IDENTIFIER value.</param>
+		public void OBJECT_IDENTIFIER(uint[] OID)
+		{
+			int i, c = OID.Length;
+			uint j;
+
+			if (c < 2)
+				throw new ArgumentException("Invalid length of OID.", nameof(OID));
+
+			if (OID[1] >= 40)
+				throw new ArgumentException("Invalid second integer in OID.", nameof(OID));
+
+			j = OID[0] * 40;
+			if (j > 255 || j + OID[1] > 255)
+				throw new ArgumentException("Invalid first integer in OID.", nameof(OID));
+
+			j += OID[1];
+
+			List<byte> Bin = new List<byte>()
+			{
+				(byte)j
+			};
+
+			for (i = 2; i < c; i++)
+			{
+				j = OID[i];
+
+				if (j < 0x80)
+					Bin.Add((byte)j);
+				else if (j < 0x4000)
+				{
+					Bin.Add((byte)(128 + (j >> 7)));
+					Bin.Add((byte)(j & 127));
+				}
+				else if (j < 0x200000)
+				{
+					Bin.Add((byte)(128 + (j >> 14)));
+					Bin.Add((byte)(128 + ((j >> 7) & 127)));
+					Bin.Add((byte)(j & 127));
+				}
+				else if (j < 0x10000000)
+				{
+					Bin.Add((byte)(128 + (j >> 21)));
+					Bin.Add((byte)(128 + ((j >> 14) & 127)));
+					Bin.Add((byte)(128 + ((j >> 7) & 127)));
+					Bin.Add((byte)(j & 127));
+				}
+				else
+				{
+					Bin.Add((byte)(128 + (j >> 28)));
+					Bin.Add((byte)(128 + ((j >> 21) & 127)));
+					Bin.Add((byte)(128 + ((j >> 14) & 127)));
+					Bin.Add((byte)(128 + ((j >> 7) & 127)));
+					Bin.Add((byte)(j & 127));
+				}
+			}
+
+			this.output.Add(6);
+			this.EncodeBinary(Bin.ToArray());
+		}
+
+		/// <summary>
+		/// Encodes a UNICODE STRING (BMPString) value.
+		/// </summary>
+		/// <param name="Value">UNICODE STRING (BMPString) value.</param>
+		public void UNICODE_STRING(string Value)
+		{
+			this.output.Add(0x1e);
+			this.EncodeBinary(Encoding.BigEndianUnicode.GetBytes(Value));
+		}
+
+		/// <summary>
+		/// Encodes an IA5 STRING value.
+		/// </summary>
+		/// <param name="Value">IA5 STRING value.</param>
+		public void IA5_STRING(string Value)
+		{
+			this.output.Add(0x16);
+			this.EncodeBinary(Encoding.ASCII.GetBytes(Value));
+		}
+
+		/// <summary>
+		/// Encodes an PRINTABLE STRING value.
+		/// </summary>
+		/// <param name="Value">PRINTABLE STRING value.</param>
+		public void PRINTABLE_STRING(string Value)
+		{
+			if (!PrintableString.IsMatch(Value))
+				throw new ArgumentException("Not printable string.", nameof(Value));
+
+			this.output.Add(0x13);
+			this.EncodeBinary(Encoding.ASCII.GetBytes(Value));
+		}
+
+		private static readonly Regex PrintableString = new Regex("^[A-Za-z0-9 '()+,-./:=?]*$", RegexOptions.Singleline | RegexOptions.Compiled);
+
+		/// <summary>
+		/// Encodes an UTF-8 STRING value.
+		/// </summary>
+		/// <param name="Value">UTF-8 STRING value.</param>
+		public void UTF8_STRING(string Value)
+		{
+			this.output.Add(0x0c);
+			this.EncodeBinary(Encoding.UTF8.GetBytes(Value));
+		}
+
+		/// <summary>
+		/// Starts a SEQUENCE.
+		/// </summary>
+		public void StartSEQUENCE()
+		{
+			this.Start();
+		}
+
+		private void Start()
+		{
+			if (this.stack == null)
+				this.stack = new LinkedList<List<byte>>();
+
+			this.stack.AddLast(this.output);
+
+			this.output = new List<byte>();
+		}
+
+		/// <summary>
+		/// Starts the current SEQUENCE.
+		/// </summary>
+		public void EndSEQUENCE()
+		{
+			this.End(0x30);
+		}
+
+		private void End(byte Type)
+		{
+			if (this.stack == null || this.stack.First == null)
+				throw new Exception("Not properly started.");
+
+			byte[] Bin = this.output.ToArray();
+
+			this.output = this.stack.First.Value;
+			this.stack.RemoveFirst();
+
+			this.output.Add(Type);
+			this.EncodeBinary(Bin);
+		}
+
+		/// <summary>
+		/// Starts a SET.
+		/// </summary>
+		public void StartSET()
+		{
+			this.Start();
+		}
+
+		/// <summary>
+		/// Starts the current SET.
+		/// </summary>
+		public void EndSET()
+		{
+			this.End(0x31);
+		}
+
+	}
+}

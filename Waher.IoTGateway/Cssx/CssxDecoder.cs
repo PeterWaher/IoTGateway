026c78f51623d8f576b6a6cdd9081410da2be574
Peter Waher <peterwaher@hotmail.com>
2018-06-15 18:34:34 +0200

    CSSX support

---
+++ Waher.IoTGateway/Cssx/CssxDecoder.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Waher.Content;
+using Waher.Runtime.Inventory;
+
+namespace Waher.IoTGateway.Cssx
+{
+	/// <summary>
+	/// CSSX encoder/decoder.
+	/// </summary>
+	public class CssxDecoder : IContentDecoder
+	{
+		/// <summary>
+		/// CSSX encoder/decoder.
+		/// </summary>
+		public CssxDecoder()
+		{
+		}
+
+		/// <summary>
+		/// Plain text content types.
+		/// </summary>
+		public static readonly string[] CssxContentTypes = new string[] 
+		{
+ 			"text/x.cssx"
+		};
+
+		/// <summary>
+		/// Plain text file extensions.
+		/// </summary>
+		public static readonly string[] CssxFileExtensions = new string[] 
+		{ 
+			"cssx"
+		};
+
+		/// <summary>
+		/// Supported content types.
+		/// </summary>
+		public string[] ContentTypes
+		{
+			get { return CssxContentTypes; }
+		}
+
+		/// <summary>
+		/// Supported file extensions.
+		/// </summary>
+		public string[] FileExtensions
+		{
+			get { return CssxFileExtensions; }
+		}
+
+		/// <summary>
+		/// If the decoder decodes an object with a given content type.
+		/// </summary>
+		/// <param name="ContentType">Content type to decode.</param>
+		/// <param name="Grade">How well the decoder decodes the object.</param>
+		/// <returns>If the decoder can decode an object with the given type.</returns>
+		public bool Decodes(string ContentType, out Grade Grade)
+		{
+			if (ContentType == "text/x.cssx")
+			{
+				Grade = Grade.Excellent;
+				return true;
+			}
+			else
+			{
+				Grade = Grade.NotAtAll;
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Decodes an object.
+		/// </summary>
+		/// <param name="ContentType">Internet Content Type.</param>
+		/// <param name="Data">Encoded object.</param>
+		/// <param name="Encoding">Any encoding specified. Can be null if no encoding specified.</param>
+		/// <param name="Fields">Any content-type related fields and their corresponding values.</param>
+		///	<param name="BaseUri">Base URI, if any. If not available, value is null.</param>
+		/// <returns>Decoded object.</returns>
+		/// <exception cref="ArgumentException">If the object cannot be decoded.</exception>
+		public object Decode(string ContentType, byte[] Data, Encoding Encoding, KeyValuePair<string, string>[] Fields, Uri BaseUri)
+		{
+			if (Encoding == null)
+				return System.Text.Encoding.UTF8.GetString(Data);
+			else
+				return Encoding.GetString(Data);
+		}
+
+		/// <summary>
+		/// Tries to get the content type of an item, given its file extension.
+		/// </summary>
+		/// <param name="FileExtension">File extension.</param>
+		/// <param name="ContentType">Content type.</param>
+		/// <returns>If the extension was recognized.</returns>
+		public bool TryGetContentType(string FileExtension, out string ContentType)
+		{
+			switch (FileExtension.ToLower())
+			{
+				case "cssx":
+					ContentType = "text/x.cssx";
+					return true;
+
+				default:
+					ContentType = string.Empty;
+					return false;
+			}
+		}
+	}
+}
---
+++ Waher.IoTGateway/Cssx/CssxToCss.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SkiaSharp;
+using Waher.Content;
+using Waher.IoTGateway.Setup;
+using Waher.Runtime.Inventory;
+using Waher.Script;
+
+namespace Waher.IoTGateway.Cssx
+{
+	/// <summary>
+	/// Converts CSSX-files to CSS, by evaluating emebedded script and replacing it with results.
+	/// </summary>
+	public class CssxToCss : IContentConverter
+	{
+		/// <summary>
+		/// Converts CSSX-files to CSS, by evaluating emebedded script and replacing it with results.
+		/// </summary>
+		public CssxToCss()
+		{
+		}
+
+		/// <summary>
+		/// Converts content from these content types.
+		/// </summary>
+		public string[] FromContentTypes => new string[] { "text/x.cssx" };
+
+		/// <summary>
+		/// Converts content to these content types. 
+		/// </summary>
+		public string[] ToContentTypes => new string[] { "text/css" };
+
+		/// <summary>
+		/// How well the content is converted.
+		/// </summary>
+		public Grade ConversionGrade => Grade.Perfect;
+
+		/// <summary>
+		/// Performs the actual conversion.
+		/// </summary>
+		/// <param name="FromContentType">Content type of the content to convert from.</param>
+		/// <param name="From">Stream pointing to binary representation of content.</param>
+		/// <param name="FromFileName">If the content is coming from a file, this parameter contains the name of that file. 
+		/// Otherwise, the parameter is the empty string.</param>
+		/// <param name="LocalResourceName">Local resource name of file, if accessed from a web server.</param>
+		/// <param name="URL">URL of resource, if accessed from a web server.</param>
+		/// <param name="ToContentType">Content type of the content to convert to.</param>
+		/// <param name="To">Stream pointing to where binary representation of content is to be sent.</param>
+		/// <param name="Session">Session states.</param>
+		/// <returns>If the result is dynamic (true), or only depends on the source (false).</returns>
+		public bool Convert(string FromContentType, Stream From, string FromFileName, string LocalResourceName, string URL, string ToContentType,
+			Stream To, Variables Session)
+		{
+			string Cssx;
+
+			using (StreamReader rd = new StreamReader(From))
+			{
+				Cssx = rd.ReadToEnd();
+			}
+
+			Session.Push();
+			try
+			{
+				ThemeDefinition Def = Theme.CurrerntTheme;
+				if (Def != null)
+				{
+					Session["HeaderColor"] = Def.HeaderColor;
+					Session["HeaderTextColor"] = Def.HeaderTextColor;
+					Session["ButtonColor"] = Def.ButtonColor;
+					Session["ButtonTextColor"] = Def.ButtonTextColor;
+					Session["LinkColor"] = Def.LinkColor;
+					Session["EditColor"] = Def.EditColor;
+
+					foreach (KeyValuePair<string, string> P in Def.GetCustomProperties())
+						Session[P.Key] = P.Value;
+				}
+
+				StringBuilder Result = new StringBuilder();
+				Expression Exp;
+				int i = 0;
+				int c = Cssx.Length;
+				int j, k;
+				string Script;
+				object Value;
+
+				while (i < c)
+				{
+					j = Cssx.IndexOf('¤', i);
+					if (j < 0)
+						break;
+
+					if (j > i)
+						Result.Append(Cssx.Substring(i, j - i));
+
+					k = Cssx.IndexOf('¤', j + 1);
+					if (k < 0)
+						break;
+
+					Script = Cssx.Substring(j + 1, k - j - 1);
+					Exp = new Expression(Script);
+					Value = Exp.Evaluate(Session);
+
+					if (Value is SKColor Color)
+					{
+						if (Color.Alpha == 255)
+						{
+							Result.Append('#');
+							Result.Append(Color.Red.ToString("X2"));
+							Result.Append(Color.Green.ToString("X2"));
+							Result.Append(Color.Blue.ToString("X2"));
+						}
+						else
+						{
+							Result.Append("rgba(");
+							Result.Append(Color.Red.ToString());
+							Result.Append(',');
+							Result.Append(Color.Green.ToString());
+							Result.Append(',');
+							Result.Append(Color.Blue.ToString());
+							Result.Append(',');
+							Result.Append(Expression.ToString(Color.Alpha / 255.0));
+							Result.Append(')');
+						}
+					}
+					else
+						Result.Append(Expression.ToString(Value));
+
+					i = k + 1;
+				}
+
+				if (i < c)
+					Result.Append(Cssx.Substring(i));
+
+				byte[] Data = Utf8WithBOM.GetBytes(Result.ToString());
+				To.Write(Data, 0, Data.Length);
+			}
+			finally
+			{
+				Session.Pop();
+			}
+
+			return false;
+		}
+
+		internal static readonly Encoding Utf8WithBOM = new UTF8Encoding(true);
+
+	}
+}

c7091a7ddd6567691f03322d6f179743a15b74cd
Peter Waher <peterwaher@hotmail.com>
2018-07-30 12:43:38 +0200

    New project: Waher.Networking.XMPP.BOSH

---
+++ Networking/Waher.Networking.XMPP.BOSH.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Waher.Networking.XMPP.BOSH.UWP")]
+[assembly: AssemblyDescription("Class library adding the HTTP alternative binding mechanism (BOSH) to the XMPP client defined in Waher.Networking.XMPP.")]
+[assembly: AssemblyConfiguration("Waher Data AB")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Waher.IoTGateway")]
+[assembly: AssemblyCopyright("Copyright © Waher Data AB 2018. All rights reserved.")]
+[assembly: AssemblyTrademark("IoT Gateway")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: ComVisible(false)]

deecbbcf432ce385f544a020b9dd28be7ad5f4cc
Peter Waher <peterwaher@hotmail.com>
2018-11-27 10:55:54 +0100

    Adding project Waher.Networking.XMPP.Contracts

---
+++ Networking/Waher.Networking.XMPP.Contracts/ContractsClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Waher.Networking.XMPP;
+using Waher.Networking.XMPP.P2P;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Contracts
+{
+	/// <summary>
+	/// Adds support for legal identities, smart contracts and signatures to an XMPP client.
+	/// 
+	/// The interface is defined in the IEEE XMPP IoT extensions:
+	/// https://gitlab.com/IEEE-SA/XMPPI/IoT
+	/// </summary>
+	public class ContractsClient : XmppExtension
+	{
+		/// <summary>
+		/// urn:ieee:iot:leg:id:1.0
+		/// </summary>
+		public const string NamespaceLegalIdentities = "urn:ieee:iot:leg:id:1.0";
+
+		private EndpointSecurity localEndpoint;
+		private readonly string componentAddress;
+
+		/// <summary>
+		/// Adds support for legal identities, smart contracts and signatures to an XMPP client.
+		/// 
+		/// The interface is defined in the IEEE XMPP IoT extensions:
+		/// https://gitlab.com/IEEE-SA/XMPPI/IoT
+		/// </summary>
+		/// <param name="Client">XMPP Client to use.</param>
+		/// <param name="ComponentAddress">Address to the contracts component.</param>
+		public ContractsClient(XmppClient Client, string ComponentAddress)
+			: base(Client)
+		{
+			this.componentAddress = ComponentAddress;
+			this.localEndpoint = new EndpointSecurity(Client, 128);
+		}
+
+		/// <summary>
+		/// Disposes of the extension.
+		/// </summary>
+		public override void Dispose()
+		{
+			this.localEndpoint?.Dispose();
+			this.localEndpoint = null;
+
+			base.Dispose();
+		}
+
+		/// <summary>
+		/// Implemented extensions.
+		/// </summary>
+		public override string[] Extensions => new string[] { };
+
+		/// <summary>
+		/// Gets the server public key.
+		/// </summary>
+		/// <param name="Callback">Method to call when response is returned.</param>
+		/// <param name="State">State object to pass on to <paramref name="Callback"/>.</param>
+		public void GetServerPublicKey(PublicKeyEventHandler Callback, object State)
+		{
+			this.client.SendIqGet(this.componentAddress, "<getPublicKey xmlns='" + NamespaceLegalIdentities + "'/>", (sender, e) =>
+			{
+				E2eEndpoint ServerEndpoint = null;
+				XmlElement E;
+
+				if (e.Ok && (E = e.FirstElement) != null && E.LocalName == "publicKey" && E.NamespaceURI == NamespaceLegalIdentities)
+				{
+					foreach (XmlNode N in E.ChildNodes)
+					{
+						if (N is XmlElement E2)
+						{
+							ServerEndpoint = EndpointSecurity.ParseE2eKey(E2, this.localEndpoint);
+							if (ServerEndpoint != null)
+								break;
+						}
+					}
+
+					e.Ok = ServerEndpoint != null;
+				}
+				else
+					e.Ok = false;
+
+				Callback?.Invoke(this, new PublicKeyEventArgs(e, ServerEndpoint));
+			}, State);
+		}
+
+	}
+}
---
+++ Networking/Waher.Networking.XMPP.Contracts/PublicKeyEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Contracts
+{
+	/// <summary>
+	/// Delegate for public key callback methods.
+	/// </summary>
+	/// <param name="Sender">Sender</param>
+	/// <param name="e">Event arguments</param>
+	public delegate void PublicKeyEventHandler(object Sender, PublicKeyEventArgs e);
+
+	/// <summary>
+	/// Event arguments for public key responses
+	/// </summary>
+	public class PublicKeyEventArgs : IqResultEventArgs
+	{
+		private readonly E2eEndpoint serverEndpoint;
+
+		/// <summary>
+		/// Event arguments for public key responses
+		/// </summary>
+		/// <param name="e">IQ response event arguments.</param>
+		/// <param name="ServerEndpoint">Public key of server endpoint.</param>
+		public PublicKeyEventArgs(IqResultEventArgs e, E2eEndpoint ServerEndpoint)
+			: base(e)
+		{
+			this.serverEndpoint = ServerEndpoint;
+		}
+
+		/// <summary>
+		/// Public key of server endpoint.
+		/// </summary>
+		public E2eEndpoint ServerEndpoint => this.serverEndpoint;
+	}
+}

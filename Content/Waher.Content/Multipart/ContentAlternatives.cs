df366809df2e7aa00746db0972f5b42dd2839f39
Peter Waher <peterwaher@hotmail.com>
2018-12-31 20:56:38 +0100

    Support for multipart/alternative decoding added

---
+++ Content/Waher.Content/Multipart/ContentAlternatives.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Waher.Content.Multipart
+{
+	/// <summary>
+	/// Represents alternative versions of the same content, encoded with 
+	/// multipart/alternative
+	/// </summary>
+	public class ContentAlternatives
+	{
+		private readonly EmbeddedContent[] content;
+
+		/// <summary>
+		/// Represents mixed content, encoded with multipart/mixed
+		/// </summary>
+		/// <param name="Content">Embedded content.</param>
+		public ContentAlternatives(EmbeddedContent[] Content)
+		{
+			this.content = Content;
+		}
+
+		/// <summary>
+		/// Embedded content.
+		/// </summary>
+		public EmbeddedContent[] Content => this.content;
+	}
+}

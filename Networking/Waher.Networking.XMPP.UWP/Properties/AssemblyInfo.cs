c18e87047473e509556c020285ca3cae74fa8463
Peter Waher <peterwaher@hotmail.com>
2019-01-18 20:13:00 +0100

    New package version numbers

--- Networking/Waher.Networking.XMPP.UWP/Properties/AssemblyInfo.cs
+++ Networking/Waher.Networking.XMPP.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Waher Data AB")]
 [assembly: AssemblyProduct("Waher.IoTGateway")]
-[assembly: AssemblyCopyright("Copyright © Waher Data AB 2016-2018. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © Waher Data AB 2016-2019. All rights reserved.")]
 [assembly: AssemblyTrademark("IoT Gateway")]
 [assembly: AssemblyCulture("")]
 
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.37")]
+[assembly: AssemblyVersion("1.0.38")]
 [assembly: ComVisible(false)]

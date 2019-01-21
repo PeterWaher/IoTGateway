c1af3b7865969dd52b8b8fdd009b30d857ee5053
Peter Waher <peterwaher@hotmail.com>
2019-01-21 13:56:03 +0100

    Adding test for multipart/mixed

--- Content/Waher.Content.Test/DecodingTests.cs
+++ Content/Waher.Content.Test/DecodingTests.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Waher.Content;
 using Waher.Content.Multipart;
 using Waher.Content.Xml;
 using Waher.Content.Html;
@@ -20,7 +19,8 @@ namespace Waher.Content.Test
 			Runtime.Inventory.Types.Initialize(
 				typeof(CommonTypes).Assembly,
 				typeof(HtmlDocument).Assembly,
-				typeof(ImageCodec).Assembly);
+				typeof(ImageCodec).Assembly,
+				typeof(XML).Assembly);
 		}
 
 		private object Decode(string FileName)
@@ -32,7 +32,7 @@ namespace Waher.Content.Test
 		}
 
 		[TestMethod]
-		public void Test_01_Multipart()
+		public void Test_01_Multipart_RelatedContent()
 		{
 			object Decoded = this.Decode("6a00b78b-ebb7-4da7-a8cd-65dd09d3ac59");
 
@@ -54,6 +54,46 @@ namespace Waher.Content.Test
 			HtmlDocument Html = Alternatives.Content[1].Decoded as HtmlDocument;
 			Assert.IsNotNull(Html);
 			Console.Out.WriteLine(Html.HtmlText);
+			Console.Out.WriteLine();
+		}
+
+		[TestMethod]
+		public void Test_02_Multipart_MixedContent()
+		{
+			object Decoded = this.Decode("0b9e5696-c32a-4418-a246-babb188e5beb");
+
+			MixedContent MixedContent = Decoded as MixedContent;
+			Assert.IsNotNull(MixedContent);
+			Assert.AreEqual(3, MixedContent.Content.Length);
+
+			RelatedContent RelatedContent = MixedContent.Content[0].Decoded as RelatedContent;
+			Assert.IsNotNull(RelatedContent);
+			Assert.AreEqual(2, RelatedContent.Content.Length);
+
+			ContentAlternatives Alternatives = RelatedContent.Content[0].Decoded as ContentAlternatives;
+			Assert.IsNotNull(Alternatives);
+			Assert.IsTrue(RelatedContent.Content[1].Decoded is SkiaSharp.SKImage);
+			File.WriteAllBytes("Data\\" + RelatedContent.Content[1].FileName, RelatedContent.Content[1].TransferDecoded ?? RelatedContent.Content[1].Raw);
+
+			Assert.AreEqual(2, Alternatives.Content.Length);
+			string PlainText = Alternatives.Content[0].Decoded as string;
+			Assert.IsNotNull(PlainText);
+			Console.Out.WriteLine(PlainText);
+			Console.Out.WriteLine();
+
+			HtmlDocument Html = Alternatives.Content[1].Decoded as HtmlDocument;
+			Assert.IsNotNull(Html);
+			Console.Out.WriteLine(Html.HtmlText);
+			Console.Out.WriteLine();
+
+			string Attachment1 = MixedContent.Content[1].Decoded as string;
+			Assert.IsNotNull(Attachment1);
+			Console.Out.WriteLine(Attachment1);
+			Console.Out.WriteLine();
+
+			XmlDocument Attachment2 = MixedContent.Content[2].Decoded as XmlDocument;
+			Assert.IsNotNull(Attachment2);
+			Console.Out.WriteLine(Attachment2.OuterXml);
 			Console.Out.WriteLine();
 		}
 

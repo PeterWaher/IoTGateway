e648b67bcaed276259ab82bffcf3675da0e8f852
Peter Waher <peterwaher@hotmail.com>
2019-02-20 21:48:36 +0100

    Adding projects Waher.Networking.Cluster and Waher.Networking.Cluster.Test

---
+++ Networking/Waher.Networking.Cluster.Test/EndpointTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Waher.Networking.Cluster.Test
+{
+	[TestClass]
+	public class EndpointTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ Networking/Waher.Networking.Cluster/ClusterEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Waher.Networking.Cluster
+{
+	/// <summary>
+	/// Represents one endpoint (or participant) in the network cluster.
+	/// </summary>
+	public class ClusterEndpoint
+	{
+	}
+}

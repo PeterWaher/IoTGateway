d015c125a0d1d34094a33b8de8b9bf9f4539c4bc
Peter Waher <peterwaher@hotmail.com>
2019-02-21 23:25:25 +0100

    Reception

--- Networking/Waher.Networking.Cluster.Test/EndpointTests.cs
+++ Networking/Waher.Networking.Cluster.Test/EndpointTests.cs
@@ -1,13 +1,57 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Waher.Networking.Sniffers;
 
 namespace Waher.Networking.Cluster.Test
 {
 	[TestClass]
 	public class EndpointTests
 	{
-		[TestMethod]
-		public void TestMethod1()
+		private static readonly IPAddress clusterAddress = IPAddress.Parse("224.0.0.0");
+		private ClusterEndpoint endpoint1 = null;
+		private ClusterEndpoint endpoint2 = null;
+
+		[TestInitialize]
+		public void TestInitialize()
 		{
+			this.endpoint1 = new ClusterEndpoint(clusterAddress, 12345, "UnitTest",
+				new ConsoleOutSniffer(BinaryPresentationMethod.Hexadecimal, LineEnding.NewLine));
+
+			foreach (IPEndPoint Endpoint in this.endpoint1.Endpoints)
+			{
+				this.endpoint2 = new ClusterEndpoint(Endpoint.Address, Endpoint.Port, "UnitTest");
+				break;
+			}
 		}
+
+		[TestCleanup]
+		public void TestCleanup()
+		{
+			this.endpoint1?.Dispose();
+			this.endpoint1 = null;
+
+			this.endpoint2?.Dispose();
+			this.endpoint2 = null;
+		}
+
+		[TestMethod]
+		public void Test_01_Send()
+		{
+			ManualResetEvent Done = new ManualResetEvent(false);
+
+			this.endpoint1.OnDataReceived += (sender, e) =>
+			{
+				Done.Set();
+			};
+
+			this.endpoint2.Transmit(new byte[] { 1, 2, 3, 4, 5 });
+
+			Assert.IsTrue(Done.WaitOne(5000));
+		}
+
 	}
 }
--- Networking/Waher.Networking.Cluster/ClusterEndpoint.cs
+++ Networking/Waher.Networking.Cluster/ClusterEndpoint.cs
@@ -24,7 +24,6 @@ namespace Waher.Networking.Cluster
 		private readonly IPEndPoint destination;
 		private readonly Aes aes;
 		private readonly byte[] key;
-		private bool disposed = false;
 
 		/// <summary>
 		/// Represents one endpoint (or participant) in the network cluster.
@@ -45,23 +44,10 @@ namespace Waher.Networking.Cluster
 		/// <param name="MulticastAddress">UDP Multicast address used for cluster communication.</param>
 		/// <param name="Port">Port used in cluster communication</param>
 		/// <param name="SharedSecret">Shared secret. Used to encrypt and decrypt communication. Secret is hashed before being fed to AES.</param>
-		/// <param name="Sniffers">Optional set of sniffers to use.</param>
-		public ClusterEndpoint(IPAddress MulticastAddress, int Port, byte[] SharedSecret,
-			params ISniffer[] Sniffers)
-			: this(MulticastAddress, Port, SharedSecret, false, Sniffers)
-		{
-		}
-
-		/// <summary>
-		/// Represents one endpoint (or participant) in the network cluster.
-		/// </summary>
-		/// <param name="MulticastAddress">UDP Multicast address used for cluster communication.</param>
-		/// <param name="Port">Port used in cluster communication</param>
-		/// <param name="SharedSecret">Shared secret. Used to encrypt and decrypt communication. Secret is hashed before being fed to AES.</param>
 		/// <param name="AllowLoopback">If communication over the loopback interface should be permitted. (Default=false)</param>
 		/// <param name="Sniffers">Optional set of sniffers to use.</param>
 		public ClusterEndpoint(IPAddress MulticastAddress, int Port, byte[] SharedSecret,
-			bool AllowLoopback, params ISniffer[] Sniffers)
+			params ISniffer[] Sniffers)
 			: base(Sniffers)
 		{
 			UdpClient Client;
@@ -97,7 +83,6 @@ namespace Waher.Networking.Cluster
 
 					AddressFamily AddressFamily = Address.AddressFamily;
 					bool IsLoopback = IPAddress.IsLoopback(Address);
-					IPAddress MulticastAddress;
 #else
 			foreach (NetworkInterface Interface in NetworkInterface.GetAllNetworkInterfaces())
 			{
@@ -107,9 +92,7 @@ namespace Waher.Networking.Cluster
 				switch (Interface.NetworkInterfaceType)
 				{
 					case NetworkInterfaceType.Loopback:
-						if (!AllowLoopback)
-							continue;
-						break;
+						continue;
 				}
 
 				IPInterfaceProperties Properties = Interface.GetIPProperties();
@@ -123,15 +106,29 @@ namespace Waher.Networking.Cluster
 					if (Address.AddressFamily != MulticastAddress.AddressFamily)
 						continue;
 
-					if (!IsLoopback || AllowLoopback)
+					if (!IsLoopback)
 					{
 						try
 						{
 							Client = new UdpClient(AddressFamily)
 							{
 								DontFragment = true,
-								MulticastLoopback = AllowLoopback
+								ExclusiveAddressUse = true,
+								MulticastLoopback = false,
+								EnableBroadcast = true,
+								Ttl = 30
 							};
+
+							Client.Client.Bind(new IPEndPoint(Address, 0));
+
+							try
+							{
+								Client.JoinMulticastGroup(MulticastAddress);
+							}
+							catch (Exception ex)
+							{
+								Log.Critical(ex);
+							}
 						}
 						catch (NotSupportedException)
 						{
@@ -143,12 +140,7 @@ namespace Waher.Networking.Cluster
 							continue;
 						}
 
-						Client.EnableBroadcast = true;
-						Client.Ttl = 30;
-						Client.Client.Bind(new IPEndPoint(Address, Port));
-						Client.JoinMulticastGroup(MulticastAddress);
-
-						ClusterUdpClient ClusterUdpClient = new ClusterUdpClient(this, Client);
+						ClusterUdpClient ClusterUdpClient = new ClusterUdpClient(this, Client, Address);
 						ClusterUdpClient.BeginReceive();
 
 						this.clients.AddLast(ClusterUdpClient);
@@ -158,12 +150,22 @@ namespace Waher.Networking.Cluster
 		}
 
 		/// <summary>
+		/// IP endpoints listening on.
+		/// </summary>
+		public IEnumerable<IPEndPoint> Endpoints
+		{
+			get
+			{
+				foreach (ClusterUdpClient Client in this.clients)
+					yield return Client.EndPoint;
+			}
+		}
+
+		/// <summary>
 		/// <see cref="IDisposable.Dispose"/>
 		/// </summary>
 		public void Dispose()
 		{
-			this.disposed = true;
-
 			foreach (ClusterUdpClient Client in this.clients)
 			{
 				try
@@ -198,22 +200,28 @@ namespace Waher.Networking.Cluster
 		{
 			private readonly ClusterEndpoint endpoint;
 			private readonly LinkedList<byte[]> outputQueue = new LinkedList<byte[]>();
-			private readonly byte[] iv = new byte[16];
+			private readonly IPAddress localAddress;
+			private readonly byte[] ivTx = new byte[16];
+			private readonly byte[] ivRx = new byte[16];
 			private HMACSHA1 hmac;
 			private UdpClient client;
 			private bool isWriting = false;
 			private bool disposed = false;
 
-			public ClusterUdpClient(ClusterEndpoint Endpoint, UdpClient Client)
+			public ClusterUdpClient(ClusterEndpoint Endpoint, UdpClient Client, IPAddress LocalAddress)
 			{
 				this.endpoint = Endpoint;
 				this.client = Client;
+				this.localAddress = LocalAddress;
 
-				byte[] A = ((IPEndPoint)Client.Client.LocalEndPoint).Address.GetAddressBytes();
-				Array.Copy(A, 0, this.iv, 8, 4);
+				byte[] A = LocalAddress.GetAddressBytes();
+				Array.Copy(A, 0, this.ivTx, 8, 4);
 
 				this.hmac = new HMACSHA1(A);
 			}
+
+			public IPAddress Address => this.localAddress;
+			public IPEndPoint EndPoint => this.client.Client.LocalEndPoint as IPEndPoint;
 
 			public void Dispose()
 			{
@@ -238,7 +246,54 @@ namespace Waher.Networking.Cluster
 
 						try
 						{
-							this.endpoint.DataReceived(Data.Buffer, Data.RemoteEndPoint);
+							byte[] Datagram = Data.Buffer;
+							int i, c = Datagram.Length - 12;
+
+							if (c <= 0 || (c & 15) != 0)
+								continue;
+
+							Array.Copy(Datagram, 0, this.ivRx, 0, 8);
+							Array.Copy(Datagram, 8, this.ivRx, 12, 4);
+
+							byte[] A = Data.RemoteEndPoint.Address.GetAddressBytes();
+							Array.Copy(A, 0, this.ivRx, 8, 4);
+
+							int FragmentNr = this.ivRx[13];
+							bool LastFragment = (FragmentNr & 0x80) != 0;
+							FragmentNr &= 0x7f;
+							FragmentNr <<= 8;
+							FragmentNr |= this.ivRx[12];
+
+							int Padding = this.ivRx[14] >> 4;
+
+							// TODO: Check DateTime
+							// TODO: Fragments
+
+							using (ICryptoTransform Decryptor = this.endpoint.aes.CreateDecryptor(this.endpoint.key, this.ivRx))
+							{
+								byte[] Decrypted = Decryptor.TransformFinalBlock(Datagram, 12, Datagram.Length - 12);
+
+								using (HMACSHA1 HMAC = new HMACSHA1(A))
+								{
+									c = Decrypted.Length - 16 - Padding;
+
+									byte[] MAC = HMAC.ComputeHash(Decrypted, 20, c);
+
+									for (i = 0; i < 20; i++)
+									{
+										if (MAC[i] != Decrypted[i])
+											break;
+									}
+
+									if (i < 20)
+										continue;
+
+									byte[] Received = new byte[c];
+									Array.Copy(Decrypted, 20, Received, 0, c);
+
+									this.endpoint.DataReceived(Received, Data.RemoteEndPoint);
+								}
+							}
 						}
 						catch (Exception ex)
 						{
@@ -288,38 +343,38 @@ namespace Waher.Networking.Cluster
 							throw new ArgumentOutOfRangeException("Message too big.", nameof(Message));
 
 						byte[] TP = BitConverter.GetBytes(DateTime.UtcNow.Ticks);
-						Array.Copy(TP, 0, this.iv, 0, 8);
+						Array.Copy(TP, 0, this.ivTx, 0, 8);
 
 						for (FragmentNr = 0; FragmentNr < NrFragments; FragmentNr++, Pos += 32768)
 						{
 							int FragmentSize = Math.Min(32768, Len - (FragmentNr << 15));
-							int Padding = (16 - FragmentSize) & 15;
+							int Padding = (-FragmentSize) & 15;
 							byte[] Datagram = new byte[28 + FragmentSize + Padding];
 
-							this.iv[12] = (byte)FragmentNr;
-							this.iv[13] = (byte)(FragmentNr >> 8);
+							this.ivTx[12] = (byte)FragmentNr;
+							this.ivTx[13] = (byte)(FragmentNr >> 8);
 
 							if (FragmentNr == NrFragments - 1)
-								this.iv[13] |= 0x80;
+								this.ivTx[13] |= 0x80;
 
-							this.iv[14] = (byte)((this.iv[14] & 0x0f) | (Padding << 4));
+							this.ivTx[14] = (byte)((this.ivTx[14] & 0x0f) | (Padding << 4));
 
-							Array.Copy(this.iv, 0, Datagram, 0, 8);
-							Array.Copy(this.iv, 12, Datagram, 8, 4);
+							Array.Copy(this.ivTx, 0, Datagram, 0, 8);
+							Array.Copy(this.ivTx, 12, Datagram, 8, 4);
 
 							byte[] MAC = this.hmac.ComputeHash(Message, Pos, FragmentSize);
 
-							Array.Copy(MAC, 0, Datagram, 12, 16);
-							Array.Copy(Message, Pos, Datagram, 28, FragmentSize);
+							Array.Copy(MAC, 0, Datagram, 12, 20);
+							Array.Copy(Message, Pos, Datagram, 32, FragmentSize);
 
-							using (ICryptoTransform Encryptor = this.endpoint.aes.CreateEncryptor(this.endpoint.key, this.iv))
+							using (ICryptoTransform Encryptor = this.endpoint.aes.CreateEncryptor(this.endpoint.key, this.ivTx))
 							{
-								byte[] Encrypted = Encryptor.TransformFinalBlock(Datagram, 16, FragmentSize + 16);
-								Array.Copy(Encrypted, 0, Datagram, 16, Encrypted.Length);
+								byte[] Encrypted = Encryptor.TransformFinalBlock(Datagram, 12, Datagram.Length - 12);
+								Array.Copy(Encrypted, 0, Datagram, 12, Encrypted.Length);
 							}
 
-							if (++this.iv[15] == 0)
-								++this.iv[14];
+							if (++this.ivTx[15] == 0)
+								++this.ivTx[14];
 
 							await this.client.SendAsync(Datagram, Datagram.Length, this.endpoint.destination);
 
@@ -352,9 +407,13 @@ namespace Waher.Networking.Cluster
 		private void DataReceived(byte[] Data, IPEndPoint From)
 		{
 			this.ReceiveBinary(Data);
+
+			this.OnDataReceived?.Invoke(this, new EventArgs());
 		}
 
-		private void Transmit(byte[] Message)
+		public event EventHandler OnDataReceived = null;
+
+		public void Transmit(byte[] Message)
 		{
 			this.TransmitBinary(Message);
 

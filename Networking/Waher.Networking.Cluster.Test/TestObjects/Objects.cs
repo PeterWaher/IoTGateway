4cfdc9a8a49c63eb44f765bb73721547af6220da
Peter Waher <peterwaher@hotmail.com>
2019-02-26 21:12:01 +0100

    Object properties

---
+++ Networking/Waher.Networking.Cluster.Test/TestObjects/Objects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Waher.Networking.Cluster;
+
+namespace Waher.Networking.Cluster.Test.TestObjects
+{
+	public class Objects : IClusterMessage
+	{
+		public Objects()
+		{
+		}
+
+		public Parent O1
+		{
+			get;
+			set;
+		}
+
+		public Parent O2
+		{
+			get;
+			set;
+		}
+
+		public Parent O3
+		{
+			get;
+			set;
+		}
+	}
+}

88ebfaa307224ef37a98fab3dc83b4ed157b4fbe
Peter Waher <peterwaher@hotmail.com>
2019-05-07 23:40:11 +0200

    Adding unit tests

--- Security/Waher.Security.SHA3.Test/SHAKE128_Tests.cs
+++ Security/Waher.Security.SHA3.Test/SHAKE128_Tests.cs
@@ -24,10 +24,10 @@ namespace Waher.Security.SHA3.Test
             Assert.AreEqual(States0Bits.Length, i);
         }
 
-        /*[TestMethod]
+        [TestMethod]
         public void Test_02_1600_bits()
         {
-            SHA3_224 H = new SHA3_224();
+            SHAKE128 H = new SHAKE128(4096);
             int i = 0;
 
             H.NewState += (sender, e) =>
@@ -45,8 +45,8 @@ namespace Waher.Security.SHA3.Test
 
             byte[] Digest = H.ComputeVariable(Input);
             string s = Hashes.BinaryToString(Digest);
-            Assert.AreEqual("9376816aba503f72f96ce7eb65ac095deee3be4bf9bbc2a1cb7e11e0", s);
+            Assert.AreEqual("131ab8d2b594946b9c81333f9bb6e0ce75c3b93104fa3469d3917457385da037cf232ef7164a6d1eb448c8908186ad852d3f85a5cf28da1ab6fe3438171978467f1c05d58c7ef38c284c41f6c2221a76f12ab1c04082660250802294fb87180213fdef5b0ecb7df50ca1f8555be14d32e10f6edcde892c09424b29f597afc270c904556bfcb47a7d40778d390923642b3cbd0579e60908d5a000c1d08b98ef933f806445bf87f8b009ba9e94f7266122ed7ac24e5e266c42a82fa1bbefb7b8db0066e16a85e0493f07df4809aec084a593748ac3dde5a6d7aae1e8b6e5352b2d71efbb47d4caeed5e6d633805d2d323e6fd81b4684b93a2677d45e7421c2c6aea259b855a698fd7d13477a1fe53e5a4a6197dbec5ce95f505b520bcd9570c4a8265a7e01f89c0c002c59bfec6cd4a5c109258953ee5ee70cd577ee217af21fa70178f0946c9bf6ca8751793479f6b537737e40b6ed28511d8a2d7e73eb75f8daac912ff906e0ab955b083bac45a8e5e9b744c8506f37e9b4e749a184b30f43eb188d855f1b70d71ff3e50c537ac1b0f8974f0fe1a6ad295ba42f6aec74d123a7abedde6e2c0711cab36be5acb1a5a11a4b1db08ba6982efccd716929a7741cfc63aa4435e0b69a9063e880795c3dc5ef3272e11c497a91acf699fefee206227a44c9fb359fd56ac0a9a75a743cff6862f17d7259ab075216c0699511643b6439", s);
             Assert.AreEqual(States1600Bits.Length, i);
-        }*/
+        }
     }
 }

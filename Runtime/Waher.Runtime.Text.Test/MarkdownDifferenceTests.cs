ec4a351152bcf4ba55ea384cd950a4bcd768097a
Peter Waher <peterwaher@hotmail.com>
2020-03-12 22:05:25 +0100

    Fixes

--- Runtime/Waher.Runtime.Text.Test/MarkdownDifferenceTests.cs
+++ Runtime/Waher.Runtime.Text.Test/MarkdownDifferenceTests.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Waher.Content.Emoji.Emoji1;
-using Waher.Script;
 
 namespace Waher.Content.Markdown.Test
 {
@@ -12,13 +10,7 @@ namespace Waher.Content.Markdown.Test
 	{
 		private void Test(string FileName)
 		{
-			Emoji1LocalFiles Emoji1LocalFiles = new Emoji1LocalFiles(Emoji1SourceFileType.Svg, 24, 24, "/emoji1/%FILENAME%", Path.Combine("Graphics", "Emoji1.zip"), "Graphics");
-			MarkdownSettings Settings = new MarkdownSettings(Emoji1LocalFiles, true, new Variables())
-			{
-				HttpxProxy = "/HttpxProxy/%URL%"
-			};
-
-			Assert.IsTrue(Emoji1LocalFiles.WaitUntilInitialized(60000));
+			MarkdownSettings Settings = new MarkdownSettings();
 
 			string OldMarkdown = File.ReadAllText("Markdown/Diff/Old/" + FileName);
 			string NewMarkdown = File.ReadAllText("Markdown/Diff/New/" + FileName);

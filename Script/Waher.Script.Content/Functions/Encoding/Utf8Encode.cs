6499ddb65d07816cdd596823f869e438ce29c1f7
Peter Waher <peterwaher@hotmail.com>
2020-05-06 23:44:57 +0200

    Function extensions for UTF-8 encoding/decoding

---
+++ Script/Waher.Script.Content/Functions/Encoding/Utf8Encode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Waher.Content;
+using Waher.Content.Xml;
+using Waher.Script.Abstraction.Elements;
+using Waher.Script.Model;
+using Waher.Script.Objects;
+
+namespace Waher.Script.Content.Functions.Encoding
+{
+	/// <summary>
+	/// Utf8Encode(Data)
+	/// </summary>
+	public class Utf8Encode : FunctionOneScalarVariable
+	{
+		/// <summary>
+		/// Utf8Encode(Data)
+		/// </summary>
+		/// <param name="Data">Binary data</param>
+		/// <param name="Start">Start position in script expression.</param>
+		/// <param name="Length">Length of expression covered by node.</param>
+		/// <param name="Expression">Expression containing script.</param>
+		public Utf8Encode(ScriptNode Data, int Start, int Length, Expression Expression)
+			: base(Data, Start, Length, Expression)
+		{
+		}
+
+		/// <summary>
+		/// Name of the function
+		/// </summary>
+		public override string FunctionName
+		{
+			get { return "utf8encode"; }
+		}
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(string Argument, Variables Variables)
+		{
+			return new ObjectValue(System.Text.Encoding.UTF8.GetBytes(Argument));
+		}
+
+	}
+}

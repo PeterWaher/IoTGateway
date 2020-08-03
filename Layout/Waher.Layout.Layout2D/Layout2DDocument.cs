d2a4eb9b0a2cdf9a54eb177118fad5b4d364109c
Peter Waher <peterwaher@hotmail.com>
2020-08-03 20:32:37 +0200

    2D Layout library

---
+++ Layout/Waher.Layout.Layout2D/Layout2DDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+
+namespace Waher.Layout.Layout2D
+{
+	/// <summary>
+	/// Contains a 2D layout document.
+	/// </summary>
+	public class Layout2DDocument
+	{
+	}
+}

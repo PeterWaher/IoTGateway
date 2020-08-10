e09298c4ff75f77216d34cebe17f72e4fda7c6c1
Peter Waher <peterwaher@hotmail.com>
2020-08-10 02:03:46 +0200

    comment

--- Layout/Waher.Layout.Layout2D/Map.cs
+++ Layout/Waher.Layout.Layout2D/Map.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Waher.Layout.Layout2D
 {
+	/// <summary>
+	/// Contains information about an actionable area in a generated image.
+	/// </summary>
 	public class Map
 	{
 	}

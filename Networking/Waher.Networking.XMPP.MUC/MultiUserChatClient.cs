14bf20168e4d00d1819010568b339dee7200b82c
Peter Waher <peterwaher@hotmail.com>
2020-11-29 18:09:31 +0100

    Adding project

---
+++ Networking/Waher.Networking.XMPP.MUC/MultiUserChatClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Waher.Networking.XMPP.MUC
+{
+	/// <summary>
+	/// Client managing communication with a Multi-User-Chat service.
+	/// https://xmpp.org/extensions/xep-0045.html
+	/// </summary>
+	public class MultiUserChatClient : XmppExtension
+	{
+		/// <summary>
+		/// http://jabber.org/protocol/muc
+		/// </summary>
+		public const string Namespace = "http://jabber.org/protocol/muc";
+
+		private readonly string componentAddress;
+
+		/// <summary>
+		/// Client managing communication with a Multi-User-Chat service.
+		/// https://xmpp.org/extensions/xep-0045.html
+		/// </summary>
+		/// <param name="Client">XMPP Client to use.</param>
+		/// <param name="ComponentAddress">Address to the Publish/Subscribe component.</param>
+		public MultiUserChatClient(XmppClient Client, string ComponentAddress)
+			: base(Client)
+		{
+			this.componentAddress = ComponentAddress;
+		}
+
+		/// <summary>
+		/// <see cref="IDisposable.Dispose"/>
+		/// </summary>
+		public override void Dispose()
+		{
+			base.Dispose();
+		}
+
+		/// <summary>
+		/// Publish/Subscribe component address.
+		/// </summary>
+		public string ComponentAddress
+		{
+			get { return this.componentAddress; }
+		}
+
+		/// <summary>
+		/// Implemented extensions.
+		/// </summary>
+		public override string[] Extensions => new string[] { "XEP-0045" };
+
+		/// <summary>
+		/// Enter a chatroom.
+		/// </summary>
+		/// <param name="RoomName">Room name.</param>
+		/// <param name="Domain">Domain of service hosting the room.</param>
+		/// <param name="NickName">Nickname to use in the chat room.</param>
+		public void EnterRoom(string RoomName, string Domain, string NickName)
+		{
+			this.client.SendDirectedPresence(RoomName + "@" + Domain + "/" + NickName,
+				"<x xmlns='" + Namespace + "'/");
+		}
+	}
+}

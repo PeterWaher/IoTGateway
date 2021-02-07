65c7e8dc74f2188b52a86493b6dcaab9953a8f91
Peter Waher <peterwaher@hotmail.com>
2021-02-07 17:58:27 +0100

    Build Date & Time fields

--- Waher.IoTGateway/Setup/Registration.cs
+++ Waher.IoTGateway/Setup/Registration.cs
@@ -8,5 +8,15 @@ namespace Waher.IoTGateway.Setup
 		private readonly static Dictionary<string, KeyValuePair<string, string>> clp = new Dictionary<string, KeyValuePair<string, string>>(StringComparer.CurrentCultureIgnoreCase)
 		{
 		};
+
+		/// <summary>
+		/// Date when solution was built.
+		/// </summary>
+		public static readonly string BuildDate = "";
+
+		/// <summary>
+		/// Time when solution was built.
+		/// </summary>
+		public static readonly string BuildTime = "";
 	}
 }
--- Waher.IoTGateway/Setup/XmppConfiguration.cs
+++ Waher.IoTGateway/Setup/XmppConfiguration.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Security;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Xml;

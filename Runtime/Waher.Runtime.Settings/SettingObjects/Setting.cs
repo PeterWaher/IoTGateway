04d2eafb0e468230ee88b115ed1dff68a5d27ab5
Peter Waher <peterwaher@hotmail.com>
2021-02-19 12:20:00 +0100

    Minor edit

--- Runtime/Waher.Runtime.Settings/SettingObjects/Setting.cs
+++ Runtime/Waher.Runtime.Settings/SettingObjects/Setting.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Waher.Persistence;
 using Waher.Persistence.Attributes;
 
 namespace Waher.Runtime.Settings.SettingObjects

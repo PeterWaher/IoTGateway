a7ffe353944fe8b8282893225211785a1981e582
Peter Waher <peterwaher@hotmail.com>
2021-03-01 21:33:20 +0100

    QR matrix

---
+++ Content/Waher.Content.QR/Encoding/QrMatrix.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Waher.Content.QR.Encoding
+{
+	/// <summary>
+	/// Class used to compute a QR code matrix.
+	/// </summary>
+	public class QrMatrix
+	{
+		private const bool X = true;
+		private const bool _ = false;
+		private static readonly bool[,] finderMarker = new bool[,]
+		{
+			{  X, X, X, X, X, X, X },
+			{  X, _, _, _, _, _, X },
+			{  X, _, X, X, X, _, X },
+			{  X, _, X, X, X, _, X },
+			{  X, _, X, X, X, _, X },
+			{  X, _, _, _, _, _, X },
+			{  X, X, X, X, X, X, X }
+		};
+		private static readonly bool[,] alignmentMarker = new bool[,]
+		{
+			{  X, X, X, X, X },
+			{  X, _, _, _, X },
+			{  X, _, X, _, X },
+			{  X, _, _, _, X },
+			{  X, X, X, X, X }
+		};
+		private static readonly char[] halfBlocks = new char[] { ' ', '▀', '▄', '█' };
+		private static readonly char[] quarterBlocks = new char[] { ' ', '▘', '▝', '▀', '▖', '▌', '▞', '▛', '▗', '▚', '▐', '▜', '▄', '▙', '▟', '█' };
+
+		private readonly bool[,] defined;
+		private readonly bool[,] dots;
+		private readonly int size;
+
+		/// <summary>
+		/// Class used to compute a QR code matrix.
+		/// </summary>
+		/// <param name="Size">Size of matrix.</param>
+		public QrMatrix(int Size)
+		{
+			if (Size < 21)
+				throw new ArgumentException("Invalid size.", nameof(Size));
+
+			this.size = Size;
+			this.defined = new bool[Size, Size];
+			this.dots = new bool[Size, Size];
+		}
+
+		/// <summary>
+		/// Encoded dots.
+		/// </summary>
+		public bool[,] Dots => this.dots;
+
+		/// <summary>
+		/// What parts of the mask has been defined.
+		/// </summary>
+		public bool[,] Defined => this.defined;
+
+		/// <summary>
+		/// Draws a Finder marker pattern in the matrix.
+		/// </summary>
+		/// <param name="X">Left coordinate of the marker.</param>
+		/// <param name="Y">Right coordinate of the marker.</param>
+		public void DrawFinderMarker(int X, int Y)
+		{
+			int x, y;
+
+			for (y = 0; y < 7; y++)
+			{
+				for (x = 0; x < 7; x++)
+				{
+					this.defined[y + Y, x + X] = true;
+					this.dots[y + Y, x + X] = finderMarker[y, x];
+				}
+			}
+		}
+
+		/// <summary>
+		/// Draws a Alignment marker pattern in the matrix.
+		/// </summary>
+		/// <param name="X">Left coordinate of the marker.</param>
+		/// <param name="Y">Right coordinate of the marker.</param>
+		public void DrawAlignmentMarker(int X, int Y)
+		{
+			int x, y;
+
+			for (y = 0; y < 5; y++)
+			{
+				for (x = 0; x < 5; x++)
+				{
+					this.defined[y + Y, x + X] = true;
+					this.dots[y + Y, x + X] = alignmentMarker[y, x];
+				}
+			}
+		}
+
+		/// <summary>
+		/// Draws a horizontal line in the matrix.
+		/// </summary>
+		/// <param name="X1">Left coordinate.</param>
+		/// <param name="X2">Right coordinate.</param>
+		/// <param name="Y">Y coordinate.</param>
+		/// <param name="Dot">If pixels should be lit (true) or cleared (false).</param>
+		/// <param name="Toggle">If pixels should be set and cleared consecutively.</param>
+		public void HLine(int X1, int X2, int Y, bool Dot, bool Dotted)
+		{
+			while (X1 <= X2)
+			{
+				this.defined[Y, X1] = true;
+				this.dots[Y, X1] = Dot;
+				X1++;
+				Dot ^= Dotted;
+			}
+		}
+
+		/// <summary>
+		/// Draws a horizontal line in the matrix.
+		/// </summary>
+		/// <param name="X">X coordinate.</param>
+		/// <param name="Y1">Top coordinate.</param>
+		/// <param name="Y2">Bottom coordinate.</param>
+		/// <param name="Dot">If pixels should be lit (true) or cleared (false).</param>
+		/// <param name="Toggle">If pixels should be set and cleared consecutively.</param>
+		public void VLine(int X, int Y1, int Y2, bool Dot, bool Dotted)
+		{
+			while (Y1 <= Y2)
+			{
+				this.defined[Y1, X] = true;
+				this.dots[Y1, X] = Dot;
+				Y1++;
+				Dot ^= Dotted;
+			}
+		}
+
+		/// <summary>
+		/// Encodes data bits in free positions in the matrix.
+		/// </summary>
+		/// <param name="Data"></param>
+		/// <returns>If all bytes fit into the matrix.</returns>
+		public bool EncodeDataBits(byte[] Data)
+		{
+			int i = 0;
+			int c = Data.Length;
+			int State = 0;
+			int x = this.size - 1;
+			int y = this.size - 1;
+			int j;
+			byte b;
+
+			while (i < c)
+			{
+				b = Data[i++];
+
+				for (j = 0; j < 8; j++)
+				{
+					if (x < 0)
+						return false;
+
+					if (this.defined[y, x])
+						j--;
+					else
+					{
+						this.defined[y, x] = true;
+						this.dots[y, x] = (b & 0x80) != 0;
+						b <<= 1;
+					}
+
+					switch (State)
+					{
+						case 0:
+							x--;
+							State++;
+							break;
+
+						case 1:
+							if (y == 0)
+							{
+								x--;
+								if (x == 6)
+									x--;
+								State++;
+							}
+							else
+							{
+								y--;
+								x++;
+								State--;
+							}
+							break;
+
+						case 2:
+							x--;
+							State++;
+							break;
+
+						case 3:
+							if (y == this.size - 1)
+							{
+								x--;
+								State = 0;
+							}
+							else
+							{
+								y++;
+								x++;
+								State--;
+							}
+							break;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Generates a text string representing the QR code using
+		/// full block characters and spaces.
+		/// Must be displayed in a font with fixed character sizes,
+		/// preferrably where each character is twice as high as wide.
+		/// </summary>
+		/// <returns>QR Code.</returns>
+		public string ToFullBlockText()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			int x, y;
+
+			for (y = 0; y < this.size; y++)
+			{
+				for (x = 0; x < this.size; x++)
+				{
+					if (!this.defined[y, x])
+						sb.Append("░░");
+					else if (this.dots[y, x])
+						sb.Append("██");
+					else
+						sb.Append("  ");
+				}
+
+				sb.AppendLine();
+			}
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Generates a text string representing the QR code using
+		/// half block characters and spaces.
+		/// Must be displayed in a font with fixed character sizes,
+		/// preferrably where each character is twice as high as wide.
+		/// </summary>
+		/// <returns>QR Code.</returns>
+		public string ToHalfBlockText()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			int x, y, y2;
+			int i;
+
+			for (y = 0; y < this.size; y += 2)
+			{
+				y2 = y + 1;
+
+				for (x = 0; x < this.size; x++)
+				{
+					i = this.defined[y, x] && this.dots[y, x] ? 1 : 0;
+
+					if (y2 < this.size && this.defined[y2, x] && this.dots[y2, x])
+						i |= 2;
+
+					sb.Append(halfBlocks[i]);
+				}
+
+				sb.AppendLine();
+			}
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Generates a text string representing the QR code using
+		/// quarter block characters and spaces.
+		/// Must be displayed in a font with fixed character sizes,
+		/// preferrably where each character are square.
+		/// </summary>
+		/// <returns>QR Code.</returns>
+		public string ToQuarterBlockText()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			int x, y, x2, y2;
+			int i;
+
+			for (y = 0; y < this.size; y += 2)
+			{
+				y2 = y + 1;
+
+				for (x = 0; x < this.size; x++)
+				{
+					x2 = x + 1;
+
+					i = this.defined[y, x] && this.dots[y, x] ? 1 : 0;
+
+					if (x2 < this.size && this.defined[y, x2] && this.dots[y, x2])
+						i |= 2;
+
+					if (y2 < this.size && this.defined[y2, x] && this.dots[y2, x])
+						i |= 4;
+
+					if (x2 < this.size && y2 < this.size && this.defined[y2, x2] && this.dots[y2, x2])
+						i |= 8;
+
+					sb.Append(quarterBlocks[i]);
+				}
+
+				sb.AppendLine();
+			}
+
+			return sb.ToString();
+		}
+	}
+}
--- Content/Waher.Content.QR/QrEncoder.cs
+++ Content/Waher.Content.QR/QrEncoder.cs
@@ -9,6 +9,49 @@ namespace Waher.Content.QR
 	public class QrEncoder : IDisposable
 	{
 		private readonly static ReedSolomonEC[] ecCalc = new ReedSolomonEC[31];
+		private readonly static byte[][] alignmentMarkers = new byte[][]
+		{
+			new byte[] { 6 },
+			new byte[] { 6, 18 },
+			new byte[] { 6, 22 },
+			new byte[] { 6, 26 },
+			new byte[] { 6, 30 },
+			new byte[] { 6, 34 },
+			new byte[] { 6, 22, 38 },
+			new byte[] { 6, 24, 42 },
+			new byte[] { 6, 26, 46 },
+			new byte[] { 6, 28, 50 },
+			new byte[] { 6, 30, 54 },
+			new byte[] { 6, 32, 58 },
+			new byte[] { 6, 34, 62 },
+			new byte[] { 6, 26, 46, 66 },
+			new byte[] { 6, 26, 48, 70 },
+			new byte[] { 6, 26, 50, 74 },
+			new byte[] { 6, 30, 54, 78 },
+			new byte[] { 6, 30, 56, 82 },
+			new byte[] { 6, 30, 58, 86 },
+			new byte[] { 6, 34, 62, 90 },
+			new byte[] { 6, 28, 50, 72, 94 },
+			new byte[] { 6, 26, 50, 74, 98 },
+			new byte[] { 6, 30, 54, 78, 102 },
+			new byte[] { 6, 28, 54, 80, 106 },
+			new byte[] { 6, 32, 58, 84, 110 },
+			new byte[] { 6, 30, 58, 86, 114 },
+			new byte[] { 6, 34, 62, 90, 118 },
+			new byte[] { 6, 26, 50, 74, 98 , 122 },
+			new byte[] { 6, 30, 54, 78, 102, 126 },
+			new byte[] { 6, 26, 52, 78, 104, 130 },
+			new byte[] { 6, 30, 56, 82, 108, 134 },
+			new byte[] { 6, 34, 60, 86, 112, 138 },
+			new byte[] { 6, 30, 58, 86, 114, 142 },
+			new byte[] { 6, 34, 62, 90, 118, 146 },
+			new byte[] { 6, 30, 54, 78, 102, 126, 150 },
+			new byte[] { 6, 24, 50, 76, 102, 128, 154 },
+			new byte[] { 6, 28, 54, 80, 106, 132, 158 },
+			new byte[] { 6, 32, 58, 84, 110, 136, 162 },
+			new byte[] { 6, 26, 54, 82, 110, 138, 166 },
+			new byte[] { 6, 30, 58, 86, 114, 142, 170 }
+		};
 
 		/// <summary>
 		/// QR Code encoder.
@@ -77,34 +120,22 @@ namespace Waher.Content.QR
 		/// <returns>Encoded message.</returns>
 		public byte[] ApplyErrorCorrection(int Version, CorrectionLevel Level, byte[] Message)
 		{
+			return this.ApplyErrorCorrection(this.FindVersionInfo(Version, Level), Message);
+		}
+
+		private VersionInfo FindVersionInfo(int Version, CorrectionLevel Level)
+		{
 			if (Version <= 0 || Version > 40)
 				throw new ArgumentOutOfRangeException("Invalid version.");
 
-			VersionInfo Info;
-
 			switch (Level)
 			{
-				case CorrectionLevel.L:
-					Info = Versions.LowVersions[Version - 1];
-					break;
-
-				case CorrectionLevel.M:
-					Info = Versions.MediumVersions[Version - 1];
-					break;
-
-				case CorrectionLevel.Q:
-					Info = Versions.QuartileVersions[Version - 1];
-					break;
-
-				case CorrectionLevel.H:
-					Info = Versions.HighVersions[Version - 1];
-					break;
-
-				default:
-					throw new ArgumentException("Invalid Error Correction level.", nameof(Level));
+				case CorrectionLevel.L: return Versions.LowVersions[Version - 1];
+				case CorrectionLevel.M: return Versions.MediumVersions[Version - 1];
+				case CorrectionLevel.Q: return Versions.QuartileVersions[Version - 1];
+				case CorrectionLevel.H: return Versions.HighVersions[Version - 1];
+				default: throw new ArgumentException("Invalid Error Correction level.", nameof(Level));
 			}
-
-			return this.ApplyErrorCorrection(Info, Message);
 		}
 
 		/// <summary>
@@ -195,6 +226,131 @@ namespace Waher.Content.QR
 			return Result;
 		}
 
+		/// <summary>
+		/// Generates a QR Code matrix from a data message.
+		/// </summary>
+		/// <param name="Version">QR Code version to use.</param>
+		/// <param name="Level">Error Correction Level</param>
+		/// <param name="Message">Data message.</param>
+		/// <returns>QR Code matrix.</returns>
+		public QrMatrix GenerateMatrix(int Version, CorrectionLevel Level, byte[] Message)
+		{
+			return this.GenerateMatrix(this.FindVersionInfo(Version, Level), Message, true);
+		}
+
+		/// <summary>
+		/// Generates a QR Code matrix from a data message.
+		/// </summary>
+		/// <param name="Version">QR Code version to use.</param>
+		/// <param name="Level">Error Correction Level</param>
+		/// <param name="Message">Data message.</param>
+		/// <param name="ApplyErrorCorrection">If Error Correction should be applied to the message (true)
+		/// or if the message include error correction codes already (false).</param>
+		/// <returns>QR Code matrix.</returns>
+		public QrMatrix GenerateMatrix(int Version, CorrectionLevel Level, byte[] Message, bool ApplyErrorCorrection)
+		{
+			return this.GenerateMatrix(this.FindVersionInfo(Version, Level), Message, ApplyErrorCorrection);
+		}
+
+		/// <summary>
+		/// Generates a QR Code matrix from a data message.
+		/// </summary>
+		/// <param name="Version">QR Code version and error correction information.</param>
+		/// <param name="Message">Data message.</param>
+		/// <returns>QR Code matrix.</returns>
+		public QrMatrix GenerateMatrix(VersionInfo Version, byte[] Message)
+		{
+			return this.GenerateMatrix(Version, Message, true);
+		}
+
+		/// <summary>
+		/// Generates a QR Code matrix from a data message.
+		/// </summary>
+		/// <param name="Version">QR Code version and error correction information.</param>
+		/// <param name="Message">Data message.</param>
+		/// <param name="ApplyErrorCorrection">If Error Correction should be applied to the message (true)
+		/// or if the message include error correction codes already (false).</param>
+		/// <returns>QR Code matrix.</returns>
+		public QrMatrix GenerateMatrix(VersionInfo Version, byte[] Message, bool ApplyErrorCorrection)
+		{
+			int Size = ((Version.Version - 1) << 2) + 21;
+			QrMatrix M = new QrMatrix(Size);
+			int i, j, c;
+
+			// Finder patterns
+
+			M.DrawFinderMarker(0, 0);
+			M.DrawFinderMarker(Size - 7, 0);
+			M.DrawFinderMarker(0, Size - 7);
+
+			// Separators
+
+			M.HLine(0, 7, 7, false, false);
+			M.VLine(7, 0, 6, false, false);
+
+			M.HLine(0, 7, Size - 8, false, false);
+			M.VLine(7, Size - 7, Size - 1, false, false);
+
+			M.HLine(Size - 8, Size - 1, 7, false, false);
+			M.VLine(Size - 8, 0, 6, false, false);
+
+			// Alignment patterns
+			
+			if (Version.Version > 0 && Version.Version <= alignmentMarkers.Length)
+			{
+				byte[] Markers = alignmentMarkers[Version.Version - 1];
+				c = Markers.Length;
+				
+				for (i = 0; i < c; i++)
+				{
+					for (j = 0; j < c; j++)
+					{
+						if ((i == 0 && (j == 0 || j == c - 1)) || (j == 0 && i == c - 1))
+							continue;
+			
+						M.DrawAlignmentMarker(Markers[i] - 2, Markers[j] - 2);
+					}
+				}
+			}
+			
+			// Timing patterns
+			
+			M.HLine(8, Size - 9, 6, true, true);
+			M.VLine(6, 8, Size - 9, true, true);
+			
+			// Dark module
+			
+			M.HLine(8, 8, Size - 8, true, false);
+			
+			// Reserve format information area
+			
+			M.HLine(0, 5, 8, false, false);
+			M.HLine(7, 8, 8, false, false);
+			M.VLine(8, 0, 5, false, false);
+			M.VLine(8, 7, 7, false, false);
+			
+			M.HLine(Size - 8, Size - 1, 8, false, false);
+			
+			M.VLine(8, Size - 7, Size - 1, false, false);
+			
+			// Reserve version information area
+			
+			for (i = 0; i < 3; i++)
+			{
+				M.HLine(0, 5, Size - 9 - i, false, false);
+				M.VLine(Size - 9 - i, 0, 5, false, false);
+			}
+			
+			// Data bits
+			
+			byte[] FullMessage = ApplyErrorCorrection ? this.ApplyErrorCorrection(Version, Message) : Message;
+
+			M.EncodeDataBits(FullMessage);	// TODO: Check result.
+			//if (!M.EncodeDataBits(FullMessage))
+			//	throw new NotSupportedException("Message too long for selected QR version & error correction level.");
+
+			return M;
+		}
 
 	}
 }

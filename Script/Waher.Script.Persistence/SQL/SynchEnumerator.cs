3477edf73dd80611984de129285c49e8d2163493
Peter Waher <peterwaher@hotmail.com>
2021-06-02 12:37:36 +0200

    code doc

--- Script/Waher.Script.Persistence/SQL/SynchEnumerator.cs
+++ Script/Waher.Script.Persistence/SQL/SynchEnumerator.cs
@@ -1,31 +1,51 @@
 ﻿using System;
 using System.Collections;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Waher.Script.Persistence.SQL
 {
-	internal class SynchEnumerator : IResultSetEnumerator
+	/// <summary>
+	/// Synchronous enumerator
+	/// </summary>
+	public class SynchEnumerator : IResultSetEnumerator
 	{
 		private readonly IEnumerator e;
 
+		/// <summary>
+		/// Synchronous enumerator
+		/// </summary>
+		/// <param name="e">Underlying enumerator.</param>
 		public SynchEnumerator(IEnumerator e)
 		{
 			this.e = e;
 		}
 
+		/// <summary>
+		/// Current item.
+		/// </summary>
 		public object Current => this.e.Current;
 
+		/// <summary>
+		/// Tries to move to next item.
+		/// </summary>
+		/// <returns>If successful</returns>
 		public bool MoveNext()
 		{
 			return this.e.MoveNext();
 		}
 
+		/// <summary>
+		/// Tries to move to next item.
+		/// </summary>
+		/// <returns>If successful</returns>
 		public Task<bool> MoveNextAsync()
 		{
 			return Task.FromResult<bool>(this.e.MoveNext());
 		}
 
+		/// <summary>
+		/// Resets the enumerator
+		/// </summary>
 		public void Reset()
 		{
 			this.e.Reset();

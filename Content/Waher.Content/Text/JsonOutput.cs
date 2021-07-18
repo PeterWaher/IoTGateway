79fd3f4c7db790fc3c76069d457c4fb08b1ca353
Peter Waher <peterwaher@hotmail.com>
2021-07-18 10:07:33 -0300

    JSON script output

---
+++ Content/Waher.Content/Text/JsonOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Waher.Runtime.Inventory;
+using Waher.Script.Abstraction.Elements;
+using Waher.Script.Output;
+
+namespace Waher.Content.Text
+{
+	/// <summary>
+	/// Converts values of type Dictionary{string, object} to expression strings.
+	/// </summary>
+	public class JsonOutput : ICustomStringOutput
+	{
+		/// <summary>
+		/// If the interface understands objects such as <paramref name="Object"/>.
+		/// </summary>
+		/// <param name="Object">Object</param>
+		/// <returns>How well objects of this type are supported.</returns>
+		public Grade Supports(Type Object) => Object == typeof(Dictionary<string, object>) ? Grade.Ok : Grade.NotAtAll;
+
+		/// <summary>
+		/// Gets a string representing a value.
+		/// </summary>
+		/// <param name="Value">Value</param>
+		/// <returns>Expression string.</returns>
+		public string GetString(object Value)
+		{
+			return JSON.Encode((Dictionary<string, object>)Value, false);
+		}
+	}
+}

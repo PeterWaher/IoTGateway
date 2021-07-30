a15e151ed8c86c17c89c407bfdb81ca174667905
Peter Waher <peterwaher@hotmail.com>
2021-07-30 13:00:43 -0300

    Incrementing package version numbers

--- Networking/Waher.Networking.XMPP.UWP/Properties/AssemblyInfo.cs
+++ Networking/Waher.Networking.XMPP.UWP/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.15")]
+[assembly: AssemblyVersion("1.1.16")]
 [assembly: ComVisible(false)]

a82ddaf162a9f3aecabefc22dcbf44a63375b9ac
Peter Waher <peterwaher@hotmail.com>
2022-02-15 20:45:46 -0300

    Incrementing package version numbers

--- Security/Waher.Security.JWT.UWP/JwtFactory.cs
+++ Security/Waher.Security.JWT.UWP/JwtFactory.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Waher.Security.JWS;
 
 namespace Waher.Security.JWT
 {
+	/// <summary>
+	/// Reason a token is not valid.
+	/// </summary>
+	public enum Reason
+	{
+		/// <summary>
+		/// No reason
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// No algorithm defined.
+		/// </summary>
+		NoAlgorithm,
+
+		/// <summary>
+		/// Algorithm not supported
+		/// </summary>
+		UnsupportedAlgorithm,
+
+		/// <summary>
+		/// No signature
+		/// </summary>
+		NoSignature,
+
+		/// <summary>
+		/// Token expired
+		/// </summary>
+		Expired,
+
+		/// <summary>
+		/// Too early to use token
+		/// </summary>
+		TooEarly,
+
+		/// <summary>
+		/// Signature invalid
+		/// </summary>
+		InvalidSignature
+	}
+
 	/// <summary>
 	/// A factory that can create and validate JWT tokens.
 	/// </summary>
@@ -67,21 +106,58 @@ namespace Waher.Security.JWT
 		/// <returns>If the token is correctly signed and valid.</returns>
 		public bool IsValid(JwtToken Token)
 		{
-			if (Token.Algorithm is null || !(Token.Algorithm is HmacSha256) || Token.Signature is null)
+			return this.IsValid(Token, out _);
+		}
+
+		/// <summary>
+		/// Checks if a token is valid and signed by the factory.
+		/// </summary>
+		/// <param name="Token">JWT token.</param>
+		/// <param name="Reason">Reason token is not valid.</param>
+		/// <returns>If the token is correctly signed and valid.</returns>
+		public bool IsValid(JwtToken Token, out Reason Reason)
+		{
+			if (Token.Algorithm is null)
+			{
+				Reason = Reason.NoAlgorithm;
 				return false;
+			}
+			else if (!(Token.Algorithm is HmacSha256))
+			{
+				Reason = Reason.UnsupportedAlgorithm;
+				return false;
+			}
+			else if (Token.Signature is null)
+			{
+				Reason = Reason.NoSignature;
+				return false;
+			}
 
 			if (Token.Expiration.HasValue || Token.NotBefore.HasValue)
 			{
 				DateTime Now = DateTime.UtcNow;
 
 				if (Token.Expiration.HasValue && Now >= Token.Expiration.Value + this.timeMargin)
+				{
+					Reason = Reason.Expired;
 					return false;
+				}
 
 				if (Token.NotBefore.HasValue && Now < Token.NotBefore.Value - this.timeMargin)
+				{
+					Reason = Reason.TooEarly;
 					return false;
+				}
 			}
 
-			return this.algorithm.IsValid(Token.Header, Token.Payload, Token.Signature);
+			if (!this.algorithm.IsValid(Token.Header, Token.Payload, Token.Signature))
+			{
+				Reason = Reason.InvalidSignature;
+				return false;
+			}
+
+			Reason = Reason.None;
+			return true;
 		}
 
 		/// <summary>

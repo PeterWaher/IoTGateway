a1dc052df6dfee8ad97712dac030f543b173f30d
Peter Waher <peterwaher@hotmail.com>
2022-02-18 21:27:39 -0300

    Fix
    Protection against recursion when outputting varaiables containing Variables collections

---
+++ Script/Waher.Script/Output/VariablesOutput.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using Waher.Runtime.Inventory;
+
+namespace Waher.Script.Output
+{
+	/// <summary>
+	/// Converts values of type Variables to expression strings.
+	/// </summary>
+	public class VariablesOutput : ICustomStringOutput
+	{
+		private static readonly TypeInfo variablesTypeInfo = typeof(Variables).GetTypeInfo();
+		private const string recursionMarker = "  IsExporting  ";
+
+		/// <summary>
+		/// If the interface understands objects such as <paramref name="Object"/>.
+		/// </summary>
+		/// <param name="Object">Object</param>
+		/// <returns>How well objects of this type are supported.</returns>
+		public Grade Supports(Type Object) => variablesTypeInfo.IsAssignableFrom(Object.GetType().GetTypeInfo()) ? Grade.Ok : Grade.NotAtAll;
+
+		/// <summary>
+		/// Gets a string representing a value.
+		/// </summary>
+		/// <param name="Value">Value</param>
+		/// <returns>Expression string.</returns>
+		public string GetString(object Value)
+		{
+			Variables Variables = (Variables)Value;
+
+			if (Variables.ContainsVariable(recursionMarker))
+				return Value.GetType().FullName;
+
+			Variables[recursionMarker] = true;
+
+			StringBuilder sb = new StringBuilder();
+			bool First = true;
+
+			sb.Append('(');
+
+			foreach (Variable Variable in Variables)
+			{
+				if (First)
+					First = false;
+				else
+					sb.Append(';');
+
+				sb.Append(Variable.Name);
+				sb.Append(":=");
+				sb.Append(Expression.ToString(Variable.ValueObject));
+			}
+
+			sb.Append(')');
+
+			Variables.Remove(recursionMarker);
+		
+			return sb.ToString();
+		}
+	}
+}

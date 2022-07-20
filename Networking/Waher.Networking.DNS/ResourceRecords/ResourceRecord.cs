f4647d609d548322211c4fd9d572b4382be65dfb
Peter Waher <peterwaher@hotmail.com>
2022-07-20 12:20:22 -0400

    Minor edit

--- Networking/Waher.Networking.DNS/ResourceRecords/ResourceRecord.cs
+++ Networking/Waher.Networking.DNS/ResourceRecords/ResourceRecord.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
+﻿using System.IO;
 using Waher.Networking.DNS.Communication;
 using Waher.Networking.DNS.Enumerations;
 using Waher.Persistence.Attributes;

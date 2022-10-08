9c4621534031e590a9a3c18ff70838f28ff0e880
Peter Waher <peterwaher@hotmail.com>
2022-10-08 20:06:58 +0200

    Adding Modbus libraries

---
+++ Things/Waher.Things.Modbus/ModbusGatewayNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Waher.Networking.Sniffers;
+using Waher.Runtime.Language;
+using Waher.Things.Ip;
+
+namespace Waher.Things.Modbus
+{
+	public class ModbusGatewayNode : IpHostPort, ISniffable
+	{
+		private readonly Sniffable sniffers = new Sniffable();
+
+		public ModbusGatewayNode()
+			: base()
+		{
+			this.Port = 502;
+			this.Tls = false;
+		}
+
+		public override Task<string> GetTypeNameAsync(Language Language)
+		{
+			return Language.GetStringAsync(typeof(ModbusGatewayNode), 1, "Modbus Gateway");
+		}
+
+		public override Task<bool> AcceptsChildAsync(INode Child)
+		{
+			return Task.FromResult<bool>(Child is MqttUnitNode);
+		}
+
+		#region ISniffable
+
+		public ISniffer[] Sniffers => this.sniffers.Sniffers;
+		public bool HasSniffers => this.sniffers.HasSniffers;
+		public void Add(ISniffer Sniffer) => this.sniffers.Add(Sniffer);
+		public void AddRange(IEnumerable<ISniffer> Sniffers) => this.sniffers.AddRange(Sniffers);
+		public bool Remove(ISniffer Sniffer) => this.sniffers.Remove(Sniffer);
+		public IEnumerator<ISniffer> GetEnumerator() => this.sniffers.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => this.sniffers.GetEnumerator();
+
+		#endregion
+
+	}
+}
---
+++ Things/Waher.Things.Modbus/ModbusUnitNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Waher.Runtime.Language;
+using Waher.Things.Metering;
+
+namespace Waher.Things.Modbus
+{
+	public class ModbusUnitNode : ProvisionedMeteringNode
+	{
+		public ModbusUnitNode()
+			: base()
+		{
+		}
+
+		public override Task<string> GetTypeNameAsync(Language Language)
+		{
+			return Language.GetStringAsync(typeof(ModbusGatewayNode), 2, "Modbus Unit");
+		}
+
+		public override Task<bool> AcceptsParentAsync(INode Parent)
+		{
+			return Task.FromResult<bool>(Parent is ModbusGatewayNode);
+		}
+
+		public override Task<bool> AcceptsChildAsync(INode Child)
+		{
+			return Task.FromResult<bool>(false);
+		}
+
+	}
+}

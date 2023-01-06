c77a967a197b823ad0aa9cdd88a15babc1596942
Peter Waher <peterwaher@hotmail.com>
2023-01-05 21:56:23 -0300

    Moving method to Persistence library

--- Persistence/Waher.Persistence.FilesLW/ObjectBTreeFile.cs
+++ Persistence/Waher.Persistence.FilesLW/ObjectBTreeFile.cs
@@ -5545,7 +5545,7 @@ namespace Waher.Persistence.Files
 						else if (ChildFilter is FilterFieldLikeRegEx FilterFieldLikeRegEx)
 						{
 							Searching.FilterFieldLikeRegEx FilterFieldLikeRegEx2 = (Searching.FilterFieldLikeRegEx)this.ConvertFilter(ChildFilter);
-							string ConstantPrefix = this.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
+							string ConstantPrefix = FilterFieldLikeRegEx.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
 
 							if (RegExFields is null)
 								RegExFields = new LinkedList<KeyValuePair<Searching.FilterFieldLikeRegEx, string>>();
@@ -5971,7 +5971,7 @@ namespace Waher.Persistence.Files
 				Searching.FilterFieldLikeRegEx FilterFieldLikeRegEx2 = (Searching.FilterFieldLikeRegEx)this.ConvertFilter(Filter);
 				IndexBTreeFile Index = this.FindBestIndex(out int _, FilterFieldLikeRegEx.FieldName, SortOrder);
 
-				string ConstantPrefix = Index is null ? string.Empty : this.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
+				string ConstantPrefix = Index is null ? string.Empty : FilterFieldLikeRegEx.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
 
 				if (string.IsNullOrEmpty(ConstantPrefix))
 				{
@@ -6129,7 +6129,7 @@ namespace Waher.Persistence.Files
 						else if (ChildFilter is FilterFieldLikeRegEx FilterFieldLikeRegEx)
 						{
 							Searching.FilterFieldLikeRegEx FilterFieldLikeRegEx2 = (Searching.FilterFieldLikeRegEx)this.ConvertFilter(ChildFilter);
-							string ConstantPrefix = this.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
+							string ConstantPrefix = FilterFieldLikeRegEx.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
 
 							if (RegExFields is null)
 								RegExFields = new LinkedList<KeyValuePair<Searching.FilterFieldLikeRegEx, string>>();
@@ -6201,7 +6201,7 @@ namespace Waher.Persistence.Files
 				Searching.FilterFieldLikeRegEx FilterFieldLikeRegEx2 = (Searching.FilterFieldLikeRegEx)this.ConvertFilter(Filter);
 				IndexBTreeFile Index = this.FindBestIndex(FilterFieldLikeRegEx.FieldName);
 
-				string ConstantPrefix = Index is null ? string.Empty : this.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
+				string ConstantPrefix = Index is null ? string.Empty : FilterFieldLikeRegEx.GetRegExConstantPrefix(FilterFieldLikeRegEx.RegularExpression, FilterFieldLikeRegEx2.Regex);
 
 				if (string.IsNullOrEmpty(ConstantPrefix))
 					return true;
@@ -6212,173 +6212,6 @@ namespace Waher.Persistence.Files
 				return true;
 			else
 				throw this.UnknownFilterType(Filter);
-		}
-
-		private string GetRegExConstantPrefix(string RegularExpression, Regex Regex)
-		{
-			StringBuilder Result = new StringBuilder();
-			int[] GroupNumbers = null;
-			int i, j, k, l, c = RegularExpression.Length;
-			char ch;
-
-			for (i = 0; i < c; i++)
-			{
-				ch = RegularExpression[i];
-				if (ch == '\\')
-				{
-					i++;
-					if (i < c)
-					{
-						ch = RegularExpression[i];
-
-						switch (ch)
-						{
-							case 'a':
-								Result.Append('\a');
-								break;
-
-							case 'b':
-								Result.Append('\b');
-								break;
-
-							case 't':
-								Result.Append('\t');
-								break;
-
-							case 'r':
-								Result.Append('\r');
-								break;
-
-							case 'v':
-								Result.Append('\v');
-								break;
-
-							case 'f':
-								Result.Append('\f');
-								break;
-
-							case 'n':
-								Result.Append('\n');
-								break;
-
-							case '.':
-							case '$':
-							case '^':
-							case '{':
-							case '[':
-							case '(':
-							case '|':
-							case ')':
-							case '*':
-							case '+':
-							case '?':
-							case '\\':
-								Result.Append(ch);
-								break;
-
-							case 'e':
-								Result.Append('\u001B');
-								break;
-
-							case 'c':
-								i++;
-								if (i < c)
-								{
-									ch = RegularExpression[i++];
-
-									if (ch == '@')
-										Result.Append((char)ch);
-									else if (ch >= 'A' && ch <= 'Z')
-										Result.Append((char)(ch - 64));
-									else
-									{
-										switch (ch)
-										{
-											case '[': Result.Append((char)27); break;
-											case '\\': Result.Append((char)28); break;
-											case ']': Result.Append((char)29); break;
-											case '^': Result.Append((char)30); break;
-											case '_': Result.Append((char)31); break;
-											default: return Result.ToString();
-										}
-									}
-								}
-								else
-									return Result.ToString();
-
-								break;
-
-							case 'x':
-								i++;
-								if (i + 2 <= c && int.TryParse(RegularExpression.Substring(i, 2), NumberStyles.HexNumber, null, out j))
-								{
-									i += 2;
-									Result.Append((char)j);
-								}
-								else
-									return Result.ToString();
-								break;
-
-							case 'u':
-								i++;
-								if (i + 4 <= c && int.TryParse(RegularExpression.Substring(i, 4), NumberStyles.HexNumber, null, out j))
-								{
-									i += 4;
-									Result.Append((char)j);
-								}
-								else
-									return Result.ToString();
-								break;
-
-							case '0':
-							case '1':
-							case '2':
-							case '3':
-							case '4':
-							case '5':
-							case '6':
-							case '7':
-							case '8':
-							case '9':
-								j = i++;
-								while ((k = i - j) < 3 && i < c && (ch = RegularExpression[i]) >= '0' && ch <= '9')
-									i++;
-
-								if (k == 1)
-									return Result.ToString();
-
-								l = int.Parse(RegularExpression.Substring(j, k));
-
-								if (GroupNumbers is null)
-									GroupNumbers = Regex.GetGroupNumbers();
-
-								if (Array.IndexOf(GroupNumbers, l) >= 0)
-									return Result.ToString();
-
-								k = 0;
-								j = 0;
-								while (l > 0)
-								{
-									k += (l % 10) << j;
-									j += 3;
-									l /= 10;
-								}
-
-								Result.Append((char)k);
-								break;
-
-							default:
-								return Result.ToString();
-						}
-					}
-				}
-				else if (".$^{[(|)*+?".IndexOf(ch) >= 0)
-					return Result.ToString();
-				else
-					Result.Append(ch);
-			}
-
-			return Result.ToString();
 		}
 
 		private Searching.IApplicableFilter ConvertFilter(Filter Filter)
--- Persistence/Waher.Persistence/Filters/FilterFieldLikeRegEx.cs
+++ Persistence/Waher.Persistence/Filters/FilterFieldLikeRegEx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Waher.Persistence.Filters
@@ -86,6 +88,179 @@ namespace Waher.Persistence.Filters
 		}
 
 		private Regex regex = null;
+
+		/// <summary>
+		/// Gets any constant prefix from a regular expression. This prefix
+		/// </summary>
+		/// <param name="RegularExpression">String-representation of regular expression.</param>
+		/// <param name="Regex">Parsed version of regular expression.</param>
+		/// <returns>Contant prefix.</returns>
+		public static string GetRegExConstantPrefix(string RegularExpression, Regex Regex)
+		{
+			StringBuilder Result = new StringBuilder();
+			int[] GroupNumbers = null;
+			int i, j, k, l, c = RegularExpression.Length;
+			char ch;
+
+			for (i = 0; i < c; i++)
+			{
+				ch = RegularExpression[i];
+				if (ch == '\\')
+				{
+					i++;
+					if (i < c)
+					{
+						ch = RegularExpression[i];
+
+						switch (ch)
+						{
+							case 'a':
+								Result.Append('\a');
+								break;
+
+							case 'b':
+								Result.Append('\b');
+								break;
+
+							case 't':
+								Result.Append('\t');
+								break;
+
+							case 'r':
+								Result.Append('\r');
+								break;
+
+							case 'v':
+								Result.Append('\v');
+								break;
+
+							case 'f':
+								Result.Append('\f');
+								break;
+
+							case 'n':
+								Result.Append('\n');
+								break;
+
+							case '.':
+							case '$':
+							case '^':
+							case '{':
+							case '[':
+							case '(':
+							case '|':
+							case ')':
+							case '*':
+							case '+':
+							case '?':
+							case '\\':
+								Result.Append(ch);
+								break;
+
+							case 'e':
+								Result.Append('\u001B');
+								break;
+
+							case 'c':
+								i++;
+								if (i < c)
+								{
+									ch = RegularExpression[i++];
+
+									if (ch == '@')
+										Result.Append((char)ch);
+									else if (ch >= 'A' && ch <= 'Z')
+										Result.Append((char)(ch - 64));
+									else
+									{
+										switch (ch)
+										{
+											case '[': Result.Append((char)27); break;
+											case '\\': Result.Append((char)28); break;
+											case ']': Result.Append((char)29); break;
+											case '^': Result.Append((char)30); break;
+											case '_': Result.Append((char)31); break;
+											default: return Result.ToString();
+										}
+									}
+								}
+								else
+									return Result.ToString();
+
+								break;
+
+							case 'x':
+								i++;
+								if (i + 2 <= c && int.TryParse(RegularExpression.Substring(i, 2), NumberStyles.HexNumber, null, out j))
+								{
+									i += 2;
+									Result.Append((char)j);
+								}
+								else
+									return Result.ToString();
+								break;
+
+							case 'u':
+								i++;
+								if (i + 4 <= c && int.TryParse(RegularExpression.Substring(i, 4), NumberStyles.HexNumber, null, out j))
+								{
+									i += 4;
+									Result.Append((char)j);
+								}
+								else
+									return Result.ToString();
+								break;
+
+							case '0':
+							case '1':
+							case '2':
+							case '3':
+							case '4':
+							case '5':
+							case '6':
+							case '7':
+							case '8':
+							case '9':
+								j = i++;
+								while ((k = i - j) < 3 && i < c && (ch = RegularExpression[i]) >= '0' && ch <= '9')
+									i++;
+
+								if (k == 1)
+									return Result.ToString();
+
+								l = int.Parse(RegularExpression.Substring(j, k));
+
+								if (GroupNumbers is null)
+									GroupNumbers = Regex.GetGroupNumbers();
+
+								if (Array.IndexOf(GroupNumbers, l) >= 0)
+									return Result.ToString();
+
+								k = 0;
+								j = 0;
+								while (l > 0)
+								{
+									k += (l % 10) << j;
+									j += 3;
+									l /= 10;
+								}
+
+								Result.Append((char)k);
+								break;
+
+							default:
+								return Result.ToString();
+						}
+					}
+				}
+				else if (".$^{[(|)*+?".IndexOf(ch) >= 0)
+					return Result.ToString();
+				else
+					Result.Append(ch);
+			}
+
+			return Result.ToString();
+		}
 
 	}
 }

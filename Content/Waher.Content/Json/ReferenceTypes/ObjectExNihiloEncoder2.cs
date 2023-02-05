8d20c8af2e236c449dcbfab6371fffd08710bf5d
Peter Waher <peterwaher@hotmail.com>
2023-02-05 14:48:43 -0300

    Fix

---
+++ Content/Waher.Content/Json/ReferenceTypes/ObjectExNihiloEncoder2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Waher.Runtime.Inventory;
+using Waher.Script.Abstraction.Elements;
+
+namespace Waher.Content.Json.ReferenceTypes
+{
+	/// <summary>
+	/// Encodes <see cref="IEnumerable{KeyValuePair{string, IElement}}"/> values.
+	/// </summary>
+	public class ObjectExNihiloEncoder2 : IJsonEncoder
+	{
+		/// <summary>
+		/// Encodes <see cref="IEnumerable{KeyValuePair{string, IElement}}"/> values.
+		/// </summary>
+		public ObjectExNihiloEncoder2()
+		{
+		}
+
+		/// <summary>
+		/// Encodes the <paramref name="Object"/> to JSON.
+		/// </summary>
+		/// <param name="Object">Object to encode.</param>
+		/// <param name="Indent">Any indentation to apply.</param>
+		/// <param name="Json">JSON output.</param>
+		public void Encode(object Object, int? Indent, StringBuilder Json)
+		{
+			JSON.Encode((IEnumerable<KeyValuePair<string, IElement>>)Object, Indent, Json);
+		}
+
+		/// <summary>
+		/// How well the JSON encoder encodes objects of type <paramref name="ObjectType"/>.
+		/// </summary>
+		/// <param name="ObjectType">Type of object to encode.</param>
+		/// <returns>How well objects of the given type are encoded.</returns>
+		public Grade Supports(Type ObjectType)
+		{
+			return typeof(IEnumerable<KeyValuePair<string, IElement>>).GetTypeInfo().IsAssignableFrom(ObjectType.GetTypeInfo()) ? Grade.Ok : Grade.NotAtAll;
+		}
+	}
+}
---
+++ Content/Waher.Content/Json/ReferenceTypes/TypedDictionaryEncoder2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Waher.Runtime.Inventory;
+
+namespace Waher.Content.Json.ReferenceTypes
+{
+	/// <summary>
+	/// Encodes <see cref="IEnumerable{KeyValuePair{string, object}}"/> values.
+	/// </summary>
+	public class TypedDictionaryEncoder2 : IJsonEncoder
+	{
+		/// <summary>
+		/// Encodes <see cref="IEnumerable{KeyValuePair{string, object}}"/> values.
+		/// </summary>
+		public TypedDictionaryEncoder2()
+		{
+		}
+
+		/// <summary>
+		/// Encodes the <paramref name="Object"/> to JSON.
+		/// </summary>
+		/// <param name="Object">Object to encode.</param>
+		/// <param name="Indent">Any indentation to apply.</param>
+		/// <param name="Json">JSON output.</param>
+		public void Encode(object Object, int? Indent, StringBuilder Json)
+		{
+			JSON.Encode((IEnumerable<KeyValuePair<string, object>>)Object, Indent, Json, null);
+		}
+
+		/// <summary>
+		/// How well the JSON encoder encodes objects of type <paramref name="ObjectType"/>.
+		/// </summary>
+		/// <param name="ObjectType">Type of object to encode.</param>
+		/// <returns>How well objects of the given type are encoded.</returns>
+		public Grade Supports(Type ObjectType)
+		{
+			return typeof(IEnumerable<KeyValuePair<string, object>>).GetTypeInfo().IsAssignableFrom(ObjectType.GetTypeInfo()) ? Grade.Excellent : Grade.NotAtAll;
+		}
+	}
+}

2f12e7b9b7248acade4975ce9da988d1d11b02f1
Peter Waher <peterwaher@hotmail.com>
2023-06-08 13:48:57 -0400

    Matrix output

--- Content/Waher.Content.Semantic/InMemorySemanticModel.cs
+++ Content/Waher.Content.Semantic/InMemorySemanticModel.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Waher.Script;
+using Waher.Script.Abstraction.Elements;
+using Waher.Script.Objects.Matrices;
+using Waher.Script.Operators.Matrices;
+using Waher.Script.Operators.Vectors;
 
 namespace Waher.Content.Semantic
 {
 	/// <summary>
 	/// In-memory semantic model.
 	/// </summary>
-	public class InMemorySemanticModel : ISemanticModel
+	public class InMemorySemanticModel : ISemanticModel, IToMatrix, IToVector
 	{
 		/// <summary>
 		/// Triples in model.
@@ -54,6 +59,38 @@ namespace Waher.Content.Semantic
 		IEnumerator IEnumerable.GetEnumerator()
 		{
 			return this.triples.GetEnumerator();
+		}
+
+		/// <summary>
+		/// Converts the object to a matrix.
+		/// </summary>
+		/// <returns>Matrix.</returns>
+		public IMatrix ToMatrix()
+		{
+			LinkedList<IElement> Elements = new LinkedList<IElement>();
+			int Rows = 0;
+
+			foreach (ISemanticTriple T in this.triples)
+			{
+				Elements.AddLast(Expression.Encapsulate(T.Subject.ElementValue));
+				Elements.AddLast(Expression.Encapsulate(T.Predicate.ElementValue));
+				Elements.AddLast(Expression.Encapsulate(T.Object.ElementValue));
+				Rows++;
+			}
+
+			return new ObjectMatrix(Rows, 3, Elements)
+			{
+				ColumnNames = new string[] { "Subject", "Predicate", "Object" }
+			};
+		}
+
+		/// <summary>
+		/// Converts the object to a vector.
+		/// </summary>
+		/// <returns>Matrix.</returns>
+		public IVector ToVector()
+		{
+			return VectorDefinition.Encapsulate(this.triples, false, null);
 		}
 	}
 }

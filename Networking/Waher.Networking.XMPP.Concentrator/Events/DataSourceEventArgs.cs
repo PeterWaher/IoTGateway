1b73e93686494aa31d5b2c3c7e65d3cfbdb14a5c
Peter Waher <peterwaher@hotmail.com>
2023-07-13 13:36:44 -0400

    Data Source events when sources are registered/unregistered

---
+++ Networking/Waher.Networking.XMPP.Concentrator/Events/DataSourceEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Waher.Things;
+
+namespace Waher.Networking.XMPP.Concentrator
+{
+	/// <summary>
+	/// Delegate for data source callback methods.
+	/// </summary>
+	/// <param name="Sender">Sender of event.</param>
+	/// <param name="e">Event arguments.</param>
+	public delegate Task DataSourceEventHandler(object Sender, DataSourceEventArgs e);
+
+	/// <summary>
+	/// Event arguments for data source events.
+	/// </summary>
+	public class DataSourceEventArgs
+	{
+		private readonly IDataSource dataSource;
+
+		internal DataSourceEventArgs(IDataSource DataSource)
+		{
+			this.dataSource = DataSource;
+		}
+
+		/// <summary>
+		/// DataSource of the concentrator server.
+		/// </summary>
+		public IDataSource DataSource => this.dataSource;
+	}
+}

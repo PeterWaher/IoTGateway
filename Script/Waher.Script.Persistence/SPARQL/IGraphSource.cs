53fc8b7035047fbb4aa824a6f8da199ba0e4a24e
Peter Waher <peterwaher@hotmail.com>
2023-07-12 20:37:33 -0400

    Fix doc

--- Script/Waher.Script.Persistence/SPARQL/IGraphSource.cs
+++ Script/Waher.Script.Persistence/SPARQL/IGraphSource.cs
@@ -17,7 +17,7 @@ namespace Waher.Script.Persistence.SPARQL
 		/// <param name="Source">Source URI</param>
 		/// <param name="Node">Node performing the loading.</param>
 		/// <param name="NullIfNotFound">If null should be returned, if graph is not found.</param>
-		/// <returns>Graph, if found, null if processed.</returns>
+		/// <returns>Graph, if found, null if not found, and null can be returned.</returns>
 		Task<ISemanticCube> LoadGraph(Uri Source, ScriptNode Node, bool NullIfNotFound);
 	}
 }

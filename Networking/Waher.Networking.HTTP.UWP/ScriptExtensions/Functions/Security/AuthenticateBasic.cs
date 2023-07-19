96810ebd8bb838f758f90f9422758bb8855073f8
Peter Waher <peterwaher@hotmail.com>
2023-07-19 16:48:58 -0400

    Minor edits

--- Networking/Waher.Networking.HTTP.UWP/ScriptExtensions/Functions/Security/AuthenticateBasic.cs
+++ Networking/Waher.Networking.HTTP.UWP/ScriptExtensions/Functions/Security/AuthenticateBasic.cs
@@ -120,9 +120,8 @@ namespace Waher.Networking.HTTP.ScriptExtensions.Functions.Security
 				throw new UnauthorizedException("Unauthorized access prohibited.", new string[] { Mechanism.GetChallenge() });
 			else
 			{
-				IUser User = await Mechanism.IsAuthenticated(Request);
-				if (User is null)
-					throw new ForbiddenException("Invalid user name or password.");
+				IUser User = await Mechanism.IsAuthenticated(Request)
+					?? throw new ForbiddenException("Invalid user name or password.");
 
 				return new ObjectValue(User);
 			}

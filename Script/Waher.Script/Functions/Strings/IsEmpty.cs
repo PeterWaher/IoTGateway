db54530c6d032dcbf458c8c77f7d076ed17b5714
Peter Waher <peterwaher@hotmail.com>
2023-09-06 17:56:13 -0300

    Support for non-string data types

--- Script/Waher.Script/Functions/Strings/IsEmpty.cs
+++ Script/Waher.Script/Functions/Strings/IsEmpty.cs
@@ -1,39 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using System.Numerics;
+using System.Threading.Tasks;
 using Waher.Script.Abstraction.Elements;
 using Waher.Script.Model;
 using Waher.Script.Objects;
 
 namespace Waher.Script.Functions.Strings
 {
-    /// <summary>
-    /// IsEmpty(s)
-    /// </summary>
-    public class IsEmpty : FunctionOneScalarVariable
-    {
-        /// <summary>
-        /// IsEmpty(x)
-        /// </summary>
-        /// <param name="Argument">Argument.</param>
-        /// <param name="Start">Start position in script expression.</param>
-        /// <param name="Length">Length of expression covered by node.</param>
+	/// <summary>
+	/// IsEmpty(s)
+	/// </summary>
+	public class IsEmpty : FunctionOneScalarVariable
+	{
+		/// <summary>
+		/// IsEmpty(x)
+		/// </summary>
+		/// <param name="Argument">Argument.</param>
+		/// <param name="Start">Start position in script expression.</param>
+		/// <param name="Length">Length of expression covered by node.</param>
 		/// <param name="Expression">Expression containing script.</param>
-        public IsEmpty(ScriptNode Argument, int Start, int Length, Expression Expression)
-            : base(Argument, Start, Length, Expression)
-        {
-        }
+		public IsEmpty(ScriptNode Argument, int Start, int Length, Expression Expression)
+			: base(Argument, Start, Length, Expression)
+		{
+		}
 
-        /// <summary>
-        /// Name of the function
-        /// </summary>
-        public override string FunctionName => nameof(IsEmpty);
+		/// <summary>
+		/// Name of the function
+		/// </summary>
+		public override string FunctionName => nameof(IsEmpty);
 
-        /// <summary>
-        /// Optional aliases. If there are no aliases for the function, null is returned.
-        /// </summary>
-        public override string[] Aliases
-        {
-            get { return new string[] { "empty" }; }
-        }
+		/// <summary>
+		/// Optional aliases. If there are no aliases for the function, null is returned.
+		/// </summary>
+		public override string[] Aliases
+		{
+			get { return new string[] { "empty" }; }
+		}
 
 		/// <summary>
 		/// Evaluates the function on a scalar argument.
@@ -47,33 +48,68 @@ namespace Waher.Script.Functions.Strings
 
 			if (Value is null)
 				return BooleanValue.True;
-
-			return base.EvaluateScalar(Argument, Variables);
+			else if (Value is string s)
+				return this.EvaluateScalar(s, Variables);
+			else
+				return BooleanValue.False;
 		}
 
-        /// <summary>
-        /// Evaluates the function on a scalar argument.
-        /// </summary>
-        /// <param name="Argument">Function argument.</param>
-        /// <param name="Variables">Variables collection.</param>
-        /// <returns>Function result.</returns>
-        public override Task<IElement> EvaluateScalarAsync(IElement Argument, Variables Variables)
-        {
-            return Task.FromResult<IElement>(this.EvaluateScalar(Argument, Variables));
-        }
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override Task<IElement> EvaluateScalarAsync(IElement Argument, Variables Variables)
+		{
+			return Task.FromResult(this.EvaluateScalar(Argument, Variables));
+		}
 
-        /// <summary>
-        /// Evaluates the function on a scalar argument.
-        /// </summary>
-        /// <param name="Argument">Function argument.</param>
-        /// <param name="Variables">Variables collection.</param>
-        /// <returns>Function result.</returns>
-        public override IElement EvaluateScalar(string Argument, Variables Variables)
-        {
-            if (string.IsNullOrEmpty(Argument))
-                return BooleanValue.True;
-            else
-                return BooleanValue.False;
-        }
-    }
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(string Argument, Variables Variables)
+		{
+			if (string.IsNullOrEmpty(Argument))
+				return BooleanValue.True;
+			else
+				return BooleanValue.False;
+		}
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(bool Argument, Variables Variables)
+		{
+			return BooleanValue.False;
+		}
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(double Argument, Variables Variables)
+		{
+			return BooleanValue.False;
+		}
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(Complex Argument, Variables Variables)
+		{
+			return BooleanValue.False;
+		}
+	}
 }

e8a7d01952382954d76429e5a7b5e6779e5e0987
Peter Waher <peterwaher@hotmail.com>
2023-09-05 21:15:33 -0300

    Sensor-data script

--- Things/Waher.Things.Script/ScriptNode.cs
+++ Things/Waher.Things.Script/ScriptNode.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Waher.Runtime.Language;
-using Waher.Things.ControlParameters;
+using Waher.Script;
+using Waher.Things.Attributes;
 using Waher.Things.Metering;
 using Waher.Things.Metering.NodeTypes;
+using Waher.Things.SensorData;
 using Waher.Things.Virtual;
 
 namespace Waher.Things.Script
@@ -11,8 +16,11 @@ namespace Waher.Things.Script
 	/// <summary>
 	/// Node defined by script.
 	/// </summary>
-	public class ScriptNode : ProvisionedMeteringNode, ISensor, IActuator
+	public class ScriptNode : ProvisionedMeteringNode, ISensor
 	{
+		private string[] sensorScript;
+		private Expression parsedSensorScript;
+
 		/// <summary>
 		/// Node defined by script.
 		/// </summary>
@@ -24,7 +32,19 @@ namespace Waher.Things.Script
 		/// <summary>
 		/// Script for generating sensor-data.
 		/// </summary>
-		public string SensorScript { get; set; }
+		[Page(2, "Script", 100)]
+		[Header(3, "Sensor-data script:")]
+		[ToolTip(4, "Script that returns sensor-data fields.")]
+		[Text(TextPosition.AfterField, 5, "Script that returns sensor-data fields. Intermediate fields can be returned using the preview function. Script should not return previewed fields, as these have already been reported. Use the \"this\" variable to refer to this node.")]
+		public string[] SensorScript
+		{
+			get => this.sensorScript;
+			set
+			{
+				this.sensorScript = value;
+				this.parsedSensorScript = null;
+			}
+		}
 
 		/// <summary>
 		/// Gets the type name of the node.
@@ -60,19 +80,75 @@ namespace Waher.Things.Script
 		/// Starts the readout of the sensor.
 		/// </summary>
 		/// <param name="Request">Request object. All fields and errors should be reported to this interface.</param>
-		public Task StartReadout(ISensorReadout Request)
+		public async Task StartReadout(ISensorReadout Request)
 		{
-			Request.ReportFields(true);
-			return Task.CompletedTask;
+			try
+			{
+				Expression Exp = this.parsedSensorScript;
+
+				if (Exp is null)
+				{
+					StringBuilder sb = new StringBuilder();
+
+					foreach (string s in this.sensorScript)
+						sb.AppendLine(s);
+
+					this.parsedSensorScript = Exp = new Expression(sb.ToString());
+				}
+
+				Variables v = new Variables()
+				{
+					["this"] = this
+				};
+
+				v.OnPreview += (sender, e) =>
+				{
+					this.ReportFields(Request, e.Preview.AssociatedObjectValue, false);
+				};
+
+				object Obj = await Exp.EvaluateAsync(v);
+				this.ReportFields(Request, Obj, true);
+
+				await this.RemoveErrorAsync("ScriptError");
+			}
+			catch (Exception ex)
+			{
+				await this.LogErrorAsync("ScriptError", ex.Message);
+				Request.ReportErrors(true, new ThingError(this, ex.Message));
+			}
 		}
 
-		/// <summary>
-		/// Get control parameters for the actuator.
-		/// </summary>
-		/// <returns>Collection of control parameters for actuator.</returns>
-		public Task<ControlParameter[]> GetControlParameters()
+		private void ReportFields(ISensorReadout Request, object Obj, bool Done)
 		{
-			return Task.FromResult<ControlParameter[]>(new ControlParameter[0]);
+			if (Obj is Field Field)
+				Request.ReportFields(Done, Field);
+			else if (Obj is Field[] Fields)
+				Request.ReportFields(Done, Fields);
+			else if (Obj is IEnumerable<Field> Fields2)
+			{
+				List<Field> Fields3 = new List<Field>();
+
+				foreach (Field F in Fields2)
+					Fields3.Add(F);
+
+				Request.ReportFields(Done, Fields3);
+			}
+			else if (Obj is IEnumerable Enumerable)
+			{
+				List<Field> Fields3 = new List<Field>();
+
+				foreach (object Obj2 in Enumerable)
+				{
+					if (Obj2 is Field F)
+						Fields3.Add(F);
+					else
+						throw new Exception("Expected script to return sensor data fields. Received object of type: " + Obj2.GetType().FullName);
+				}
+
+				Request.ReportFields(Done, Fields3);
+			}
+			else if (!(Obj is null))
+				throw new Exception("Expected script to return sensor data fields. Received object of type: " + Obj.GetType().FullName);
 		}
 	}
 }

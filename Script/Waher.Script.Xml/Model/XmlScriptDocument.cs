c679ad37c1d4d0561954703ec72e03293a0adfed
Peter Waher <peterwaher@hotmail.com>
2023-09-16 12:22:33 -0300

    Minor edits

--- Script/Waher.Script.Xml/Model/XmlScriptDocument.cs
+++ Script/Waher.Script.Xml/Model/XmlScriptDocument.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml;
 using Waher.Script.Abstraction.Elements;
-using Waher.Script.Exceptions;
 using Waher.Script.Model;
 using Waher.Script.Objects;
 

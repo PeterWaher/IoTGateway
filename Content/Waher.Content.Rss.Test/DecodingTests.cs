6a525a0b87c277a11949851181962bda3afa900a
Peter Waher <peterwaher@hotmail.com>
2023-12-05 16:33:21 -0300

    Adding content library for rss

---
+++ Content/Waher.Content.Rss.Test/DecodingTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+	
+namespace Waher.Content.Rss.Test
+{
+	[TestClass]
+	public class DecodingTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}

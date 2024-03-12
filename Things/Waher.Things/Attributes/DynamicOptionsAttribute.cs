3b89e558a8954f63b0fb1d85697017db57963522
Peter Waher <peterwaher@hotmail.com>
2024-03-12 10:18:50 -0300

    Fix doc

--- Things/Waher.Things/Attributes/DynamicOptionsAttribute.cs
+++ Things/Waher.Things/Attributes/DynamicOptionsAttribute.cs
@@ -3,7 +3,7 @@
 namespace Waher.Things.Attributes
 {
 	/// <summary>
-	/// Defines an option to display when editing the parameter.
+	/// Defines a method that returns an array of options to display when editing the parameter.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
 	public class DynamicOptionsAttribute : Attribute
@@ -11,7 +11,7 @@ namespace Waher.Things.Attributes
 		private readonly string methodName;
 
 		/// <summary>
-		/// Defines a method for retrieving a dynamic set of options to display when editing the parameter.
+		/// Defines a method that returns an array of options to display when editing the parameter.
 		/// </summary>
 		/// <param name="MethodName">Name of method on object that can be used to retrieve a dynamic set of options.</param>
 		public DynamicOptionsAttribute(string MethodName)

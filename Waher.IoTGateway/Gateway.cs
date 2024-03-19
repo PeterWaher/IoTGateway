9f9afedaf103016acd2cf32714c77a7884209140
Peter Waher <peterwaher@hotmail.com>
2024-03-19 16:42:40 -0300

    Reverse proxy configuration

--- Waher.IoTGateway/Gateway.cs
+++ Waher.IoTGateway/Gateway.cs
@@ -965,6 +965,41 @@ namespace Waher.IoTGateway
 					}
 				}
 
+				XmlElement ReverseProxy = Config.DocumentElement["ReverseProxy"];
+				if (!(ReverseProxy is null))
+				{
+					foreach (XmlNode N in ReverseProxy.ChildNodes)
+					{
+						if (N is XmlElement E && E.LocalName == "ProxyResource")
+						{
+							string LocalResource = XML.Attribute(E, "localResource");
+							string RemoteDomain = XML.Attribute(E, "remoteDomain");
+							string RemoteFolder = XML.Attribute(E, "remoteFolder");
+							bool Encrypted = XML.Attribute(E, "encrypted", false);
+							int RemotePort = XML.Attribute(E, "remotePort", Encrypted ? HttpServer.DefaultHttpsPort : HttpServer.DefaultHttpPort);
+							bool UseSession = XML.Attribute(E, "useSession", false);
+							int TimeoutMs = XML.Attribute(E, "timeoutMs", 10000);
+
+							try
+							{
+								webServer.Register(new HttpReverseProxyResource(LocalResource, RemoteDomain, RemotePort, RemoteFolder, Encrypted, 
+									TimeSpan.FromMilliseconds(TimeoutMs), UseSession));
+							}
+							catch (Exception ex)
+							{
+								Log.Error("Unable to register reverse proxy: " + ex.Message,
+									new KeyValuePair<string, object>("LocalResource", LocalResource),
+									new KeyValuePair<string, object>("RemoteDomain", RemoteDomain),
+									new KeyValuePair<string, object>("Encrypted", Encrypted),
+									new KeyValuePair<string, object>("RemotePort", RemotePort),
+									new KeyValuePair<string, object>("RemoteFolder", RemoteFolder),
+									new KeyValuePair<string, object>("UseSession", UseSession),
+									new KeyValuePair<string, object>("TimeoutMs", TimeoutMs));
+							}
+						}
+					}
+				}
+
 				await LoadScriptResources();
 
 				httpxServer = new HttpxServer(xmppClient, webServer, MaxChunkSize);

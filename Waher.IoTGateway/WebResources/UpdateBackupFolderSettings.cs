bc0ec511b08f0c6cae385a65cf0063b2974724af
Peter Waher <peterwaher@hotmail.com>
2024-05-01 16:27:06 -0400

    Minor edit

--- Waher.IoTGateway/WebResources/UpdateBackupFolderSettings.cs
+++ Waher.IoTGateway/WebResources/UpdateBackupFolderSettings.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Waher.Content;
-using Waher.Content.Html.Elements;
 using Waher.Networking.HTTP;
 
 namespace Waher.IoTGateway.WebResources

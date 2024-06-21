fbf6df90aecdfeef039a6c431205b5d13fa63115
Peter Waher <peterwaher@hotmail.com>
2024-06-21 22:53:13 +0200

    Fix: JSON encoding of control characters

--- Content/Waher.Content/JSON.cs
+++ Content/Waher.Content/JSON.cs
@@ -490,8 +490,80 @@ namespace Waher.Content
 			return CommonTypes.Escape(s, jsonCharactersToEscape, jsonCharacterEscapes);
 		}
 
-		private static readonly char[] jsonCharactersToEscape = new char[] { '\\', '"', '\n', '\r', '\t', '\b', '\f', '\a', '\v' };
-		private static readonly string[] jsonCharacterEscapes = new string[] { "\\\\", "\\\"", "\\n", "\\r", "\\t", "\\b", "\\f", "\\a", "\\v" };
+		private static readonly char[] jsonCharactersToEscape = new char[] 
+		{
+			'\\', 
+			'"',
+			'\x00',
+			'\x01',
+			'\x02',
+			'\x03',
+			'\x04',
+			'\x05',
+			'\x06',
+			'\a',	// 7  - 0x07
+			'\b',	// 8  - 0x08
+			'\t',	// 9  - 0x09
+			'\n',	// 10 - 0x0a
+			'\v',	// 11 - 0x0b
+			'\f',	// 12 - 0x0c
+			'\r',	// 13 - 0x0d
+			'\x0e',
+			'\x0f',
+			'\x10',
+			'\x11',
+			'\x12',
+			'\x13',
+			'\x14',
+			'\x15',
+			'\x16',
+			'\x17',
+			'\x18',
+			'\x19',
+			'\x1a',
+			'\x1b',
+			'\x1c',
+			'\x1d',
+			'\x1e',
+			'\x1f'
+		};
+		private static readonly string[] jsonCharacterEscapes = new string[]
+		{ 
+			"\\\\", 
+			"\\\"",
+			"\\u0000",
+			"\\u0001",
+			"\\u0002",
+			"\\u0003",
+			"\\u0004",
+			"\\u0005",
+			"\\u0006",
+			"\\a",
+			"\\b",
+			"\\t",
+			"\\n",
+			"\\v",
+			"\\f",
+			"\\r",
+			"\\u000e",
+			"\\u000f",
+			"\\u0010",
+			"\\u0011",
+			"\\u0012",
+			"\\u0013",
+			"\\u0014",
+			"\\u0015",
+			"\\u0016",
+			"\\u0017",
+			"\\u0018",
+			"\\u0019",
+			"\\u001a",
+			"\\u001b",
+			"\\u001c",
+			"\\u001d",
+			"\\u001e",
+			"\\u001f"
+		};
 
 		/// <summary>
 		/// Encodes an object as JSON.

47adaa3e3d0c8b893f115a9e08643cba5bf0fcf4
Peter Waher <peterwaher@hotmail.com>
2024-07-24 11:56:56 -0400

    Incrementing package version numbers

--- Networking/Waher.Networking.MQTT.UWP/Properties/AssemblyInfo.cs
+++ Networking/Waher.Networking.MQTT.UWP/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyVersion("2.0.4")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("")]
 

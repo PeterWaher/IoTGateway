b123cfa6726be941445f5d9846a9d10a76db957f
Peter Waher <peterwaher@hotmail.com>
2024-09-24 12:41:40 -0300

    Reversing order of labels in vertical bar graph

--- Script/Waher.Script.Graphs/Functions/Plots/VerticalBars.cs
+++ Script/Waher.Script.Graphs/Functions/Plots/VerticalBars.cs
@@ -1,4 +1,5 @@
-﻿using Waher.Script.Abstraction.Elements;
+﻿using System.Collections.Generic;
+using Waher.Script.Abstraction.Elements;
 using Waher.Script.Exceptions;
 using Waher.Script.Model;
 
@@ -75,7 +76,12 @@ namespace Waher.Script.Graphs.Functions.Plots
 
 			IElement Color = Arguments.Length <= 2 ? null : Arguments[2];
 
-			return new Graph2D(Variables, Labels, Values, new VerticalBarsPainter(), false, true, this,
+			LinkedList<IElement> ReverseOrder = new LinkedList<IElement>();
+
+			foreach (IElement Label in Labels.VectorElements)
+				ReverseOrder.AddFirst(Label);
+
+			return new Graph2D(Variables, (IVector)Labels.Encapsulate(ReverseOrder, this), Values, new VerticalBarsPainter(), false, true, this,
 				Color?.AssociatedObjectValue ?? Graph.DefaultColor);
 		}
 	}
--- Script/Waher.Script.Graphs/Functions/Plots/VerticalBarsPainter.cs
+++ Script/Waher.Script.Graphs/Functions/Plots/VerticalBarsPainter.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using SkiaSharp;
+﻿using SkiaSharp;
 
 namespace Waher.Script.Graphs.Functions.Plots
 {

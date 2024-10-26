23b7c93a521a0e67c12cc24bc23c18859dc585a4
ViggoSkj <viggo@trustanchorgroup.com>
2024-10-26 15:48:32 +0200

    Final responsive fixes and fully working dropdown for mobile and desktop

--- Waher.IoTGateway.Resources/Root/Nav.js
+++ Waher.IoTGateway.Resources/Root/Nav.js
@@ -1,17 +1,45 @@
 function NativeHeader() {
     const header = document.getElementById("native-header");
     Array.from(header.children[0].children[1].getElementsByTagName("ul")).forEach(subMenu => {
-        const linkElement = subMenu.nextElementSibling || subMenu.previousElementSibling;
+        const sibling = subMenu.nextElementSibling || subMenu.previousElementSibling;
 
         // if link dose not go anywhere, expand on click
-        linkElement.addEventListener("click", event => {
-
+        sibling.addEventListener("click", event => {
             // expand menue on mobile view
             if (window.matchMedia("screen and (max-width: 900px)").matches) {
-                event.preventDefault();
+                event.preventDefault(); // if link, prevent opening
                 subMenu.toggleAttribute("expanded");
             }
+        })
 
+        subMenu.parentElement.addEventListener("mouseenter", () => {
+            // if on computer, make sure dropdowns expand beyond the screen
+
+            const rect = subMenu.getBoundingClientRect();
+            const rightX = rect.left + rect.width
+
+            // content is overflowing
+            if (rightX > window.innerWidth) {
+                // go to top submenu
+                let element = subMenu
+                while(element.parentElement.parentElement.parentElement.tagName.toLowerCase() == "li")
+                {
+                    element = element.parentElement.parentElement
+                }
+
+                let oldOffset = 0
+                if (element.style.transform)
+                    oldOffset += Number(element.style.transform.split("(")[1].split("px")[0])
+                element.style.transform = `translateX(${oldOffset + window.innerWidth - rightX}px)`
+
+                const topLevelLi = element.parentElement
+
+                const handler = () => {
+                    topLevelLi.removeEventListener("mouseenter", handler);
+                    element.style.transform = ""
+                  };
+                topLevelLi.addEventListener("mouseenter", handler);
+            }
         })
     })
 

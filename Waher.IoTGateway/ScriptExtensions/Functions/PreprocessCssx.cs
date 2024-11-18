73f34bd2ddcfe06497ba7229c90a7ec0da07a882
Peter Waher <peterwaher@hotmail.com>
2024-11-18 16:39:23 -0300

    Preprocessing included CSSX

---
+++ Waher.IoTGateway/ScriptExtensions/Functions/PreprocessCssx.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Waher.Script;
+using Waher.Script.Abstraction.Elements;
+using Waher.Script.Model;
+using Waher.Script.Objects;
+
+namespace Waher.IoTGateway.ScriptExtensions.Functions
+{
+	/// <summary>
+	/// PreprocessCssx(Cssx)
+	/// </summary>
+	public class PreprocessCssx : FunctionOneScalarStringVariable
+    {
+		/// <summary>
+		/// PreprocessCssx(Cssx)
+		/// </summary>
+		/// <param name="Cssx">Cssx.</param>
+		/// <param name="Start">Start position in script expression.</param>
+		/// <param name="Length">Length of expression covered by node.</param>
+		/// <param name="Expression">Expression containing script.</param>
+		public PreprocessCssx(ScriptNode Cssx, int Start, int Length, Expression Expression)
+            : base(Cssx, Start, Length, Expression)
+        {
+        }
+
+        /// <summary>
+        /// Name of the function
+        /// </summary>
+        public override string FunctionName => nameof(PreprocessCssx);
+
+		/// <summary>
+		/// If the node (or its decendants) include asynchronous evaluation. Asynchronous nodes should be evaluated using
+		/// <see cref="ScriptNode.EvaluateAsync(Variables)"/>.
+		/// </summary>
+		public override bool IsAsynchronous => true;
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override IElement EvaluateScalar(string Argument, Variables Variables)
+		{
+			return this.EvaluateScalarAsync(Argument, Variables).Result;
+		}
+
+		/// <summary>
+		/// Evaluates the function on a scalar argument.
+		/// </summary>
+		/// <param name="Argument">Function argument.</param>
+		/// <param name="Variables">Variables collection.</param>
+		/// <returns>Function result.</returns>
+		public override async Task<IElement> EvaluateScalarAsync(string Argument, Variables Variables)
+		{
+			Argument = await Expression.TransformAsync(Argument, "¤", "¤", Variables);
+			return new StringValue(Argument);
+        }
+
+    }
+}

10ab89a0cce58927513a0da637119f20ab6c86ca
Peter Waher <peterwaher@hotmail.com>
2024-11-28 12:31:31 -0300

    Adding h2c ALPN protocol ID, for reference

--- Networking/Waher.Networking.HTTP/HttpServer.cs
+++ Networking/Waher.Networking.HTTP/HttpServer.cs
@@ -1087,7 +1087,7 @@ namespace Waher.Networking.HTTP
 					}
 
 					await Client.UpgradeToTlsAsServer(this.serverCertificate, Crypto.SecureTls, 
-						ClientCertificates, null, TrustCertificates, "http/1.1", "h2");
+						ClientCertificates, null, TrustCertificates, "http/1.1", "h2", "h2c");
 
 					if (this.HasSniffers)
 					{

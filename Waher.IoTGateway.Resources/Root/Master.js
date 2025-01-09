b711906d999ac628386e28bc5b00e5b1776ca76f
ViggoSkj <viggo@trustanchorgroup.com>
2025-01-09 08:20:37 +0100

    Began trying new solution

--- Waher.IoTGateway.Resources/Root/Master.js
+++ Waher.IoTGateway.Resources/Root/Master.js
@@ -1,5 +1,6 @@
 function NativeHeader() {
     const header = document.getElementById("native-header");
+    let indentation = 0
     Array.from(header.children[0].children[1].getElementsByTagName("ul")).forEach(subMenu => {
         const sibling = subMenu.nextElementSibling || subMenu.previousElementSibling;
 
@@ -12,84 +13,34 @@ function NativeHeader() {
             }
         })
 
+        const transformed = []
+
+        // expand submenu
+        subMenu.parentElement.addEventListener("mouseenter", () => {
+            indentation += subMenu.parentElement.clientWidth
+            subMenu.setAttribute("expanded", "")
+
+            maxWidth = 0
+
+            for (let i = 0; i < subMenu.children.length; i++) {
+                const child = subMenu.children[i]
+                const textElement = Array.from(child.children).find(c => c.tagName.toLocaleLowerCase() === "a")
+                if (textElement) {
+                    maxWidth = Math.max(maxWidth, textElement.clientWidth)
+                }
+            }
+
+            console.log(maxWidth)
+
+            //subMenu.style.width = maxWidth + "px"
+        })
+
         // close submenu
         subMenu.parentElement.addEventListener("mouseleave", () => {
             subMenu.removeAttribute("expanded")
-
-            // remove sibling translation
-            let sibling = subMenu.parentElement.nextElementSibling
-            while(sibling != undefined)
-            {
-                sibling.style.transform = ""
-                sibling = sibling.nextElementSibling
-            }
-        })
-        
-        
-        // expand submenu
-        subMenu.parentElement.addEventListener("mouseenter", () => {
-            subMenu.setAttribute("expanded", "") // expand menu
-
-
-            // make every dropdowns item the same with
-            let maxWidth = 0
-            const listItems = Array.from(subMenu.children) 
-            listItems.forEach(listItem => {
-                const anchorElement = listItem.children[0]
-                maxWidth = Math.max(maxWidth, anchorElement.clientWidth)
+            transformed.forEach(element => {
+                element.style.transform = ""
             })
-            listItems.forEach(listItem => {
-                listItem.children[0].style.width = maxWidth + "px"
-            })
-
-
-
-
-          
-            // if on computer, make sure dropdowns does not expand beyond the screen
-            const rect = subMenu.getBoundingClientRect();
-            const rightX = rect.right
-            const bottomY = rect.bottom
-
-            // content is overflowing to the right of the screen
-            if (rightX > window.innerWidth) {
-                // go to top submenu
-                let element = subMenu
-                while(element.parentElement.parentElement.parentElement.tagName.toLowerCase() == "li")
-                {
-                    element = element.parentElement.parentElement
-                }
-
-                let oldOffset = element.offset || 0
-                element.offset = oldOffset + window.innerWidth - rightX
-                element.style.transform += ` translateX(${element.offset}px)`
-
-                const topLevelLi = element.parentElement
-
-                const handler = () => {
-                    topLevelLi.removeEventListener("mouseenter", handler);
-                    element.style.transform = ""
-                  };
-                topLevelLi.addEventListener("mouseenter", handler);
-            }
-
-            // overflowing the bottom of the screen
-            if (bottomY > window.innerHeight) {
-                subMenu.style.maxHeight = rect.height - bottomY + window.innerHeight + "px";
-            }
-
-
-
-
-            // translate siblings
-            const offset = subMenu.parentElement.getBoundingClientRect().height - subMenu.parentElement.children[0].getBoundingClientRect().height
-
-            let sibling = subMenu.parentElement.nextElementSibling
-            while(sibling != undefined)
-            {
-                sibling.style.transform += ` translateY(${-offset}px)`
-                sibling = sibling.nextElementSibling
-            }
         })
     })
 

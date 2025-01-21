ff73d53f2e7c472dc06d2cc4420ff4d53f2813f0
Peter Waher <peterwaher@hotmail.com>
2025-01-21 19:37:45 -0300

    Simplifying processing of personal information in IDs

---
+++ Networking/Waher.Networking.XMPP.Contracts/Gender.cs
@@ -0,0 +1,23 @@
+﻿namespace Waher.Networking.XMPP.Contracts
+{
+	/// <summary>
+	/// Gender classification in a legal ID.
+	/// </summary>
+	public enum Gender
+	{
+		/// <summary>
+		/// Male
+		/// </summary>
+		Male,
+
+		/// <summary>
+		/// Female
+		/// </summary>
+		Female,
+
+		/// <summary>
+		/// Other
+		/// </summary>
+		Other
+	}
+}

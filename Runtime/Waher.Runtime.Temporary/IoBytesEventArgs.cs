dc12f544bc08760f953e2945c36bfcfef479c3fa
Peter Waher <peterwaher@hotmail.com>
2025-02-18 16:06:47 -0300

    TemporaryProgressFile, for tracking download progress

---
+++ Runtime/Waher.Runtime.Temporary/IoBytesEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Waher.Runtime.Temporary
+{
+    /// <summary>
+    /// Event arguments for an I/O bytes event.
+    /// </summary>
+    public class IoBytesEventArgs : EventArgs
+    {
+		/// <summary>
+		/// Event arguments for an I/O bytes event.
+		/// </summary>
+		/// <param name="Nr">Number of bytes</param>
+		/// <param name="Total">Total number of bytes</param>
+		public IoBytesEventArgs(int Nr, long Total)
+        {
+            this.Nr = Nr;
+            this.Total = Total;
+        }
+
+		/// <summary>
+		/// Number of bytes
+		/// </summary>
+		public int Nr { get; }
+
+		/// <summary>
+		/// Total number of bytes
+		/// </summary>
+		public long Total { get; }
+	}
+}
---
+++ Runtime/Waher.Runtime.Temporary/TemporaryProgressFile.cs
@@ -0,0 +1,123 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Waher.Events;
+
+namespace Waher.Runtime.Temporary
+{
+	/// <summary>
+	/// Class managing the contents of a temporary file. When the class is disposed, the temporary file is deleted.
+	/// The class provides an even letting the caller receive events on progress.
+	/// </summary>
+	public class TemporaryProgressFile : TemporaryFile
+	{
+		private long nrReadTotal = 0;
+		private long nrWrittenTotal = 0;
+
+		/// <summary>
+		/// Class managing the contents of a temporary file. When the class is disposed, the temporary file is deleted.
+		/// </summary>
+		public TemporaryProgressFile()
+			: base()
+		{
+		}
+
+		/// <summary>
+		/// Class managing the contents of a temporary file. When the class is disposed, the temporary file is deleted.
+		/// </summary>
+		/// <param name="FileName">Name of temporary file. Call <see cref="Path.GetTempFileName()"/> to get a new temporary file name.</param>
+		public TemporaryProgressFile(string FileName)
+			: base(FileName)
+		{
+		}
+
+		/// <summary>
+		/// Class managing the contents of a temporary file. When the class is disposed, the temporary file is deleted.
+		/// </summary>
+		/// <param name="FileName">Name of temporary file. Call <see cref="Path.GetTempFileName()"/> to get a new temporary file name.</param>
+		/// <param name="BufferSize">Buffer size.</param>
+		public TemporaryProgressFile(string FileName, int BufferSize)
+			: base(FileName, BufferSize)
+		{
+		}
+
+		/// <summary>
+		/// Event raised when data has been read from the file.
+		/// </summary>
+		public event EventHandlerAsync<IoBytesEventArgs> OnRead;
+
+		/// <summary>
+		/// Event raised when data has been written to the file.
+		/// </summary>
+		public event EventHandlerAsync<IoBytesEventArgs> OnWrite;
+
+		/// <inheritdoc/>
+		public override void Write(byte[] array, int offset, int count)
+		{
+			base.Write(array, offset, count);
+
+			this.nrWrittenTotal += count;
+			this.OnWrite.Raise(this, new IoBytesEventArgs(count, this.nrWrittenTotal));
+		}
+
+		/// <inheritdoc/>
+		public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+		{
+			await base.WriteAsync(buffer, offset, count, cancellationToken);
+
+			this.nrWrittenTotal += count;
+			await this.OnWrite.Raise(this, new IoBytesEventArgs(count, this.nrWrittenTotal));
+		}
+
+		/// <inheritdoc/>
+		public override void WriteByte(byte value)
+		{
+			base.WriteByte(value);
+
+			this.nrWrittenTotal++;
+			this.OnWrite.Raise(this, new IoBytesEventArgs(1, this.nrWrittenTotal));
+		}
+
+		/// <inheritdoc/>
+		public override int Read(byte[] array, int offset, int count)
+		{
+			int i = base.Read(array, offset, count);
+
+			if (i > 0)
+			{
+				this.nrReadTotal += i;
+				this.OnRead.Raise(this, new IoBytesEventArgs(i, this.nrReadTotal));
+			}
+
+			return i;
+		}
+
+		/// <inheritdoc/>
+		public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+		{
+			int i = await base.ReadAsync(buffer, offset, count, cancellationToken);
+
+			if (i > 0)
+			{
+				this.nrReadTotal += i;
+				await this.OnRead.Raise(this, new IoBytesEventArgs(i, this.nrReadTotal));
+			}
+
+			return i;
+		}
+
+		/// <inheritdoc/>
+		public override int ReadByte()
+		{
+			int i = base.ReadByte();
+			if (i < 0)
+				return i;
+
+			this.nrReadTotal++;
+			this.OnRead.Raise(this, new IoBytesEventArgs(i, this.nrReadTotal));
+
+			return i;
+		}
+
+	}
+}

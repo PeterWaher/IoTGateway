ed3c187b13a5e3962bfb96e802333a8af7d06524
Peter Waher <peterwaher@hotmail.com>
2025-03-27 14:02:06 -0300

    ChunkedList

---
+++ Runtime/Waher.Runtime.Inventory.Test/ChunkedListTests.cs
@@ -0,0 +1,141 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Waher.Runtime.Inventory.Test
+{
+	[TestClass]
+	public class ChunkedListTests
+	{
+		[TestMethod]
+		public void Test_01_DefaultConstructor()
+		{
+			ChunkedList<int> List = [];
+			Assert.AreEqual(0, List.Count);
+			Assert.IsFalse(List.IsReadOnly);
+		}
+
+		[TestMethod]
+		public void Test_02_ConstructorWithInitialChunkSize()
+		{
+			ChunkedList<int> List = new(32);
+			Assert.AreEqual(0, List.Count);
+		}
+
+		[TestMethod]
+		public void Test_03_ConstructorWithInitialAndMaxChunkSize()
+		{
+			ChunkedList<int> List = new(32, 64);
+			Assert.AreEqual(0, List.Count);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Test_04_ConstructorWithInvalidInitialChunkSize()
+		{
+			ChunkedList<int> List = new(0);
+			Assert.AreEqual(0, List.Count);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Test_05_ConstructorWithMaxChunkSizeLessThanInitialChunkSize()
+		{
+			ChunkedList<int> List = new(64, 32);
+			Assert.AreEqual(0, List.Count);
+		}
+
+		[TestMethod]
+		public void Test_06_AddSingleItem()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			Assert.AreEqual(1, List.Count);
+			Assert.IsTrue(List.Contains(1));
+		}
+
+		[TestMethod]
+		public void Test_07_AddMultipleItemsWithinInitialChunkSize()
+		{
+			ChunkedList<int> List = new(4)
+			{
+				1,
+				2,
+				3,
+				4
+			};
+			Assert.AreEqual(4, List.Count);
+		}
+
+		[TestMethod]
+		public void Test_08_AddItemsExceedingInitialChunkSize()
+		{
+			ChunkedList<int> List = new(4, 8);
+			for (int i = 0; i < 5; i++)
+				List.Add(i);
+			Assert.AreEqual(5, List.Count);
+		}
+
+		[TestMethod]
+		public void Test_09_RemoveExistingItem()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			List.Add(2);
+			Assert.IsTrue(List.Remove(1));
+			Assert.AreEqual(1, List.Count);
+			Assert.IsFalse(List.Contains(1));
+		}
+
+		[TestMethod]
+		public void Test_10_RemoveNonExistingItem()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			Assert.IsFalse(List.Remove(2));
+		}
+
+		[TestMethod]
+		public void Test_11_ContainsItem()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			Assert.IsTrue(List.Contains(1));
+			Assert.IsFalse(List.Contains(2));
+		}
+
+		[TestMethod]
+		public void Test_12_Clear()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			List.Clear();
+			Assert.AreEqual(0, List.Count);
+			Assert.IsFalse(List.Contains(1));
+		}
+
+		[TestMethod]
+		public void Test_13_CopyTo()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			List.Add(2);
+			var array = new int[2];
+			List.CopyTo(array, 0);
+			Assert.AreEqual(1, array[0]);
+			Assert.AreEqual(2, array[1]);
+		}
+
+		[TestMethod]
+		public void Test_14_Enumerator()
+		{
+			ChunkedList<int> List = [];
+			List.Add(1);
+			List.Add(2);
+			int Count = 0;
+			foreach (var item in List)
+				Count++;
+			
+			Assert.AreEqual(2, Count);
+		}
+	}
+}
---
+++ Runtime/Waher.Runtime.Inventory/ChunkedList.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Waher.Runtime.Inventory
+{
+	/// <summary>
+	/// A chunked list is a linked list of chunks of objects of type <typeparamref name="T"/>.
+	/// </summary>
+	/// <typeparam name="T">Element type.</typeparam>
+	public class ChunkedList<T> : ICollection<T>
+	{
+		private const int initialChunkSize = 16;
+		private const int initialMaxChunkSize = 1024;
+
+		private readonly int maxChunkSize;
+		private Chunk current;
+		private Chunk firstChunk;
+		private Chunk lastChunk;
+		private int chunkSize;
+		private int count = 0;
+
+		/// <summary>
+		/// Number of elements in collection.
+		/// </summary>
+		public int Count => this.count;
+
+		/// <summary>
+		/// If collection is read-only (false).
+		/// </summary>
+		public bool IsReadOnly => false;
+
+		/// <summary>
+		/// A chunked list is a linked list of chunks of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		public ChunkedList()
+			: this(initialChunkSize, initialMaxChunkSize)
+		{
+		}
+
+		/// <summary>
+		/// A chunked list is a linked list of chunks of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="InitialChunkSize">Initial Chunk Size.</param>
+		public ChunkedList(int InitialChunkSize)
+			: this(InitialChunkSize, InitialChunkSize)
+		{
+		}
+
+		/// <summary>
+		/// A chunked list is a linked list of chunks of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="InitialChunkSize">Initial Chunk Size.</param>
+		/// <param name="MaxChunkSize">Maximum Chunk Size.</param>
+		public ChunkedList(int InitialChunkSize, int MaxChunkSize)
+		{
+			if (InitialChunkSize <= 0)
+				throw new ArgumentException("Chunk size must be positive.", nameof(InitialChunkSize));
+
+			if (MaxChunkSize < InitialChunkSize)
+				throw new ArgumentException("Max chunk size must be greater than or equal to initial chunk size.", nameof(MaxChunkSize));
+
+			this.chunkSize = InitialChunkSize;
+			this.maxChunkSize = MaxChunkSize;
+
+			this.current = this.firstChunk = this.lastChunk = new Chunk(InitialChunkSize);
+
+			this.chunkSize <<= 1;
+			if (this.chunkSize > this.maxChunkSize)
+				this.chunkSize = this.maxChunkSize;
+		}
+
+		private class Chunk
+		{
+			public T[] Elements;
+			public Chunk Next;
+			public Chunk Prev;
+			public int Size;
+			public int Count;
+
+			public Chunk(int Size)
+			{
+				this.Elements = new T[Size];
+				this.Size = Size;
+				this.Count = 0;
+				this.Next = null;
+				this.Prev = null;
+			}
+
+			public Chunk(int Size, Chunk Previous)
+				: this(Size)
+			{
+				this.Prev = Previous;
+				Previous.Next = this;
+			}
+		}
+
+		/// <summary>
+		/// Adds an item to the collection.
+		/// </summary>
+		/// <param name="Item">Item</param>
+		public void Add(T Item)
+		{
+			if (this.current.Count < this.current.Size)
+				this.current.Elements[this.current.Count++] = Item;
+			else
+			{
+				this.lastChunk = new Chunk(this.chunkSize, this.current);
+				this.current = this.lastChunk;
+
+				this.chunkSize <<= 1;
+				if (this.chunkSize > this.maxChunkSize)
+					this.chunkSize = this.maxChunkSize;
+
+				this.current.Elements[this.current.Count++] = Item;
+			}
+
+			this.count++;
+		}
+
+		/// <summary>
+		/// Clears the collection.
+		/// </summary>
+		public void Clear()
+		{
+			this.current = this.firstChunk = this.lastChunk = new Chunk(this.chunkSize);
+			this.count = 0;
+		}
+
+		/// <summary>
+		/// Checks if an item is a member of the collection.
+		/// </summary>
+		/// <param name="Item">Item to search for.</param>
+		/// <returns>If the item is found in the collection.</returns>
+		public bool Contains(T Item)
+		{
+			Chunk Loop = this.firstChunk;
+			int i, c;
+
+			while (!(Loop is null))
+			{
+				for (i = 0, c = Loop.Count; i < c; i++)
+				{
+					if (Item.Equals(Loop.Elements[i]))
+						return true;
+				}
+
+				Loop = Loop.Next;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Copies the contents of the collection to an array.
+		/// </summary>
+		/// <param name="Desintation">Destination array.</param>
+		/// <param name="DesintationIndex">Start index into the array where elements are to be copied.</param>
+		public void CopyTo(T[] Desintation, int DesintationIndex)
+		{
+			Chunk Loop = this.firstChunk;
+
+			while (!(Loop is null))
+			{
+				if (Loop.Count > 0)
+				{
+					Array.Copy(Loop.Elements, 0, Desintation, DesintationIndex, Loop.Count);
+					DesintationIndex += Loop.Count;
+				}
+
+				Loop = Loop.Next;
+			}
+		}
+
+		/// <summary>
+		/// Removes an element from the collection.
+		/// </summary>
+		/// <param name="Item">Item to remove from the collection.</param>
+		/// <returns>If the element was found and removed.</returns>
+		public bool Remove(T Item)
+		{
+			Chunk Loop = this.firstChunk;
+			int i, c;
+
+			while (!(Loop is null))
+			{
+				for (i = 0, c = Loop.Count; i < c; i++)
+				{
+					if (Item.Equals(Loop.Elements[i]))
+					{
+						if (i < c - 1)
+							Array.Copy(Loop.Elements, i + 1, Loop.Elements, i, c - i - 1);
+
+						Loop.Count--;
+						this.count--;
+
+						return true;
+					}
+				}
+
+				Loop = Loop.Next;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Returns an enumerator for the collection.
+		/// </summary>
+		/// <returns>Enumerator</returns>
+		public IEnumerator<T> GetEnumerator()
+		{
+			return new ChunkedListEnumerator(this.firstChunk);
+		}
+
+		/// <summary>
+		/// Returns an enumerator for the collection.
+		/// </summary>
+		/// <returns>Enumerator</returns>
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
+		private class ChunkedListEnumerator : IEnumerator<T>
+		{
+			private Chunk first;
+			private Chunk current;
+			private int pos;
+
+			public ChunkedListEnumerator(Chunk FirstChunk)
+			{
+				this.first = FirstChunk;
+				this.current = null;
+				this.pos = 0;
+			}
+
+			public T Current
+			{
+				get
+				{
+					if (this.current is null)
+						throw new InvalidOperationException("Enumeration must be started or reset.");
+					else
+						return this.current.Elements[this.pos];
+				}
+			}
+
+			object IEnumerator.Current => this.Current;
+
+			public void Dispose()
+			{
+				this.first = null;
+				this.current = null;
+			}
+
+			public bool MoveNext()
+			{
+				if (this.current is null)
+				{
+					this.current = this.first;
+					this.pos = -1;
+				}
+
+				while (++this.pos >= this.current.Count)
+				{
+					this.current = this.current.Next;
+					if (this.current is null)
+						return false;
+
+					this.pos = -1;
+				}
+
+				return true;
+			}
+
+			public void Reset()
+			{
+				this.current = null;
+				this.pos = -1;
+			}
+		}
+	}
+}

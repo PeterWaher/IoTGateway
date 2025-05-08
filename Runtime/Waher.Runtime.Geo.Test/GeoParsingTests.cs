2c0d5077998c9769199fa98eeec817cb67175870
Peter Waher <peterwaher@hotmail.com>
2025-05-08 15:34:57 -0400

    Parsing GPS coordinate strings

--- Runtime/Waher.Runtime.Geo.Test/GeoParsingTests.cs
+++ Runtime/Waher.Runtime.Geo.Test/GeoParsingTests.cs
@@ -41,6 +41,8 @@ namespace Waher.Runtime.Geo.Test
 			Match M = GeoPosition.XmlGeoPositionPattern.Match(Value);
 
 			Assert.IsTrue(M.Success);
+			Assert.AreEqual(0, M.Index);
+			Assert.AreEqual(Value.Length, M.Length);
 
 			Assert.IsTrue(Expression.TryParse(M.Groups["Lat"].Value, out double Lat));
 			Assert.AreEqual(Latitude, Lat);
@@ -93,5 +95,177 @@ namespace Waher.Runtime.Geo.Test
 				Assert.AreEqual(Altitude, Pos.Altitude.Value);
 			}
 		}
+
+		[DataTestMethod]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E 100.1")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E 100.1")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W 100.1")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E -100.1")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W -100.1")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W -100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E 100.1 ft")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E 100.1 ft")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W 100.1 ft")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E -100.1 ft")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W -100.1 ft")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E, Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E, Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W, Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E, Altitude: -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W, Altitude: -100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W Altitude: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Altitude: -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W Altitude: -100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Altitude 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E Altitude 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W Altitude 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Altitude -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W Altitude -100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Alt: 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E Alt: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W Alt: 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E Alt: -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W Alt: -100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E, 100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" E, 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" W, 100.1 m")]
+		[DataRow("10° 12' 18.0\" N 20° 18' 24.0\" E, -100.1 m")]
+		[DataRow("10° 12' 18.0\" S 20° 18' 24.0\" W, -100.1 m")]
+		[DataRow("10° 12' 18\" N 20° 18' 24\" E 100.1")]
+		[DataRow("10° 12' 18\" S 20° 18' 24\" E 100.1")]
+		[DataRow("10° 12' 18\" N 20° 18' 24\" W 100.1")]
+		[DataRow("10° 12' 18\" N 20° 18' 24\" E -100.1")]
+		[DataRow("10° 12' 18\" S 20° 18' 24\" W -100.1")]
+		[DataRow("10° 12' 18\" N 20° 18' 24\" E")]
+		[DataRow("10° 12' 18\" S 20° 18' 24\" E")]
+		[DataRow("10° 12' 18\" N 20° 18' 24\" W")]
+		[DataRow("10° 12' 18\" S 20° 18' 24\" W")]
+		[DataRow("10° 12' N 20° 18' E 100.1")]
+		[DataRow("10° 12' S 20° 18' E 100.1")]
+		[DataRow("10° 12' N 20° 18' W 100.1")]
+		[DataRow("10° 12' N 20° 18' E -100.1")]
+		[DataRow("10° 12' S 20° 18' W -100.1")]
+		[DataRow("10° 12' N 20° 18' E")]
+		[DataRow("10° 12' S 20° 18' E")]
+		[DataRow("10° 12' N 20° 18' W")]
+		[DataRow("10° 12' S 20° 18' W")]
+		[DataRow("10° N 20° E 100")]
+		[DataRow("10° S 20° E 100")]
+		[DataRow("10° N 20° W 100")]
+		[DataRow("10° N 20° E -100")]
+		[DataRow("10° S 20° W -100")]
+		[DataRow("10° N 20° E")]
+		[DataRow("10° S 20° E")]
+		[DataRow("10° N 20° W")]
+		[DataRow("10° N 20° E")]
+		[DataRow("10° S 20° W")]
+		public void Test_03_GPS_RegEx(string Value)
+		{
+			Match M = GeoPosition.GpsGeoPositionPattern.Match(Value);
+
+			Assert.IsTrue(M.Success);
+			Assert.AreEqual(0, M.Index);
+			Assert.AreEqual(Value.Length, M.Length);
+		}
+
+		[DataTestMethod]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E 100.1", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E 100.1", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W 100.1", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E -100.1", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W -100.1", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E", 10.205, 20.31, double.NaN)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E", -10.205, 20.31, double.NaN)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W", 10.205, -20.31, double.NaN)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W", -10.205, -20.31, double.NaN)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E 100.1 ft", 10.205, 20.31, 100.1 * 0.3048)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E 100.1 ft", -10.205, 20.31, 100.1 * 0.3048)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W 100.1 ft", 10.205, -20.31, 100.1 * 0.3048)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E -100.1 ft", 10.205, 20.31, -100.1 * 0.3048)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W -100.1 ft", -10.205, -20.31, -100.1 * 0.3048)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E, Altitude: 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E, Altitude: 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W, Altitude: 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E, Altitude: -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W, Altitude: -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Altitude: 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E Altitude: 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W Altitude: 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Altitude: -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W Altitude: -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Altitude 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E Altitude 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W Altitude 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Altitude -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W Altitude -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Alt: 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E Alt: 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W Alt: 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E Alt: -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W Alt: -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E, 100.1 m", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" E, 100.1 m", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" W, 100.1 m", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18.0\" N 20° 18' 36.0\" E, -100.1 m", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18.0\" S 20° 18' 36.0\" W, -100.1 m", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18\" N 20° 18' 36\" E 100.1", 10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18\" S 20° 18' 36\" E 100.1", -10.205, 20.31, 100.1)]
+		[DataRow("10° 12' 18\" N 20° 18' 36\" W 100.1", 10.205, -20.31, 100.1)]
+		[DataRow("10° 12' 18\" N 20° 18' 36\" E -100.1", 10.205, 20.31, -100.1)]
+		[DataRow("10° 12' 18\" S 20° 18' 36\" W -100.1", -10.205, -20.31, -100.1)]
+		[DataRow("10° 12' 18\" N 20° 18' 36\" E", 10.205, 20.31, double.NaN)]
+		[DataRow("10° 12' 18\" S 20° 18' 36\" E", -10.205, 20.31, double.NaN)]
+		[DataRow("10° 12' 18\" N 20° 18' 36\" W", 10.205, -20.31, double.NaN)]
+		[DataRow("10° 12' 18\" S 20° 18' 36\" W", -10.205, -20.31, double.NaN)]
+		[DataRow("10° 12' N 20° 18' E 100.1", 10.2, 20.3, 100.1)]
+		[DataRow("10° 12' S 20° 18' E 100.1", -10.2, 20.3, 100.1)]
+		[DataRow("10° 12' N 20° 18' W 100.1", 10.2, -20.3, 100.1)]
+		[DataRow("10° 12' N 20° 18' E -100.1", 10.2, 20.3, -100.1)]
+		[DataRow("10° 12' S 20° 18' W -100.1", -10.2, -20.3, -100.1)]
+		[DataRow("10° 12' N 20° 18' E", 10.2, 20.3, double.NaN)]
+		[DataRow("10° 12' S 20° 18' E", -10.2, 20.3, double.NaN)]
+		[DataRow("10° 12' N 20° 18' W", 10.2, -20.3, double.NaN)]
+		[DataRow("10° 12' S 20° 18' W", -10.2, -20.3, double.NaN)]
+		[DataRow("10° N 20° E 100", 10, 20, 100)]
+		[DataRow("10° S 20° E 100", -10, 20, 100)]
+		[DataRow("10° N 20° W 100", 10, -20, 100)]
+		[DataRow("10° N 20° E -100", 10, 20, -100)]
+		[DataRow("10° S 20° W -100", -10, -20, -100)]
+		[DataRow("10° N 20° E", 10, 20, double.NaN)]
+		[DataRow("10° S 20° E", -10, 20, double.NaN)]
+		[DataRow("10° N 20° W", 10, -20, double.NaN)]
+		[DataRow("10° N 20° E", 10, 20, double.NaN)]
+		[DataRow("10° S 20° W", -10, -20, double.NaN)]
+		public void Test_04_GPS_TryParse(string Value, double Latitude, double Longitude, double Altitude)
+		{
+			Assert.IsTrue(GeoPosition.TryParseGps(Value, out GeoPosition Pos));
+
+			Assert.AreEqual(Latitude, Pos.Latitude);
+			Assert.AreEqual(Longitude, Pos.Longitude);
+
+			if (double.IsNaN(Altitude))
+				Assert.IsFalse(Pos.Altitude.HasValue);
+			else
+			{
+				Assert.IsTrue(Pos.Altitude.HasValue);
+				Assert.AreEqual(Altitude, Pos.Altitude.Value);
+			}
+		}
+
 	}
 }
--- Runtime/Waher.Runtime.Geo/GeoPosition.cs
+++ Runtime/Waher.Runtime.Geo/GeoPosition.cs
@@ -17,6 +17,12 @@ namespace Waher.Runtime.Geo
 		/// </summary>
 		public static readonly Regex XmlGeoPositionPattern = new Regex(@"(?'Lat'-?\d+(\.\d+)?),(?'Lon'-?\d+(\.\d+)?)(,(?'Alt'-?\d+(\.\d+)?))?", RegexOptions.Singleline | RegexOptions.Compiled);
 
+		/// <summary>
+		/// Pattern for parsing a geo-position GPS strings. The names groups Lat, Lon and Alt 
+		/// can be used to extract the latitude, longitude and altitude values respectively.
+		/// </summary>
+		public static readonly Regex GpsGeoPositionPattern = new Regex(@"^\s*(?'LatDeg'\d+)\s*°?(\s*(?'LatMin'\d+)'(\s*(?'LatSec'\d+(\.\d+)?)"")?)?\s*(?'LatSign'[nNsS])\s*(?'LonDeg'\d+)\s*°?(\s*(?'LonMin'\d+)'(\s*(?'LonSec'\d+(\.\d+)?)"")?)?\s*(?'LonSign'[eEwW])(\s*,)?\s*((Alt|Altitude)\:?\s*)?((?'Alt'-?\d+(\.\d+)?)\s*(?'AltUnit'(m|ft))?)?\s*$", RegexOptions.Singleline | RegexOptions.Compiled);
+
 		private double latitude;
 		private double longitude;
 		private double? altitude;
@@ -86,7 +92,8 @@ namespace Waher.Runtime.Geo
 		}
 
 		/// <summary>
-		/// Tries to parse a string representation of a GeoPosition.
+		/// Tries to parse a string representation of a GeoPosition. If first attempts the
+		/// XML format. If not successful, it tries the GPS format.
 		/// </summary>
 		/// <param name="s">String value.</param>
 		/// <param name="Value">Parsed geo-position.</param>
@@ -96,11 +103,14 @@ namespace Waher.Runtime.Geo
 			if (TryParseXml(s, out Value))
 				return true;
 
+			if (TryParseGps(s, out Value))
+				return true;
+
 			return false;
 		}
 
 		/// <summary>
-		/// Tries to parse a string representation of a GeoPosition.
+		/// Tries to parse an XML string representation of a GeoPosition.
 		/// </summary>
 		/// <param name="s">String value.</param>
 		/// <param name="Value">Parsed geo-position.</param>
@@ -109,7 +119,7 @@ namespace Waher.Runtime.Geo
 		{
 			int i = s.IndexOf(',');
 			if (i < 0 ||
-				!double.TryParse(s.Substring(0, i).Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator), out double Latitude) ||
+				!TryParse(s.Substring(0, i), out double Latitude) ||
 				Latitude < -90 ||
 				Latitude > 90)
 			{
@@ -125,7 +135,7 @@ namespace Waher.Runtime.Geo
 
 			if (j < 0)
 			{
-				if (!double.TryParse(s.Substring(i).Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator), out Longitude) ||
+				if (!TryParse(s.Substring(i), out Longitude) ||
 					Longitude < -180 ||
 					Longitude > 180)
 				{
@@ -137,7 +147,7 @@ namespace Waher.Runtime.Geo
 			}
 			else
 			{
-				if (!double.TryParse(s.Substring(i, j - i).Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator), out Longitude) ||
+				if (!TryParse(s.Substring(i, j - i), out Longitude) ||
 					Longitude < -180 ||
 					Longitude > 180)
 				{
@@ -145,7 +155,7 @@ namespace Waher.Runtime.Geo
 					return false;
 				}
 
-				if (!double.TryParse(s.Substring(j + 1).Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator), out double d))
+				if (!TryParse(s.Substring(j + 1), out double d))
 				{
 					Value = null;
 					return false;
@@ -159,6 +169,117 @@ namespace Waher.Runtime.Geo
 			return true;
 		}
 
+		private static bool TryParse(string s, out double Value)
+		{
+			return double.TryParse(s.Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator), out Value);
+		}
+
+		private static string ToString(double Value)
+		{
+			return Value.ToString().Replace(System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator, ".");
+		}
+
+		/// <summary>
+		/// Tries to parse a GPS string representation of a GeoPosition.
+		/// </summary>
+		/// <param name="s">String value.</param>
+		/// <param name="Value">Parsed geo-position.</param>
+		/// <returns>If the string could be parsed.</returns>
+		public static bool TryParseGps(string s, out GeoPosition Value)
+		{
+			Value = null;
+
+			Match M = GpsGeoPositionPattern.Match(s);
+			if (!M.Success || M.Index > 0 || M.Length < s.Length)
+				return false;
+
+			if (!TryParse(M.Groups["LatDeg"], M.Groups["LatMin"], M.Groups["LatSec"],
+				M.Groups["LatSign"], -90, 90, out double Latitude))
+			{
+				return false;
+			}
+
+			if (!TryParse(M.Groups["LonDeg"], M.Groups["LonMin"], M.Groups["LonSec"],
+				M.Groups["LonSign"], -180, 180, out double Longitude))
+			{
+				return false;
+			}
+
+			string AltitudeStr = M.Groups["Alt"].Value;
+			if (string.IsNullOrEmpty(AltitudeStr))
+				Value = new GeoPosition(Latitude, Longitude);
+			else
+			{
+				if (!TryParse(AltitudeStr, out double Altitude))
+					return false;
+
+				string Unit = M.Groups["AltUnit"].Value;
+
+				if (!string.IsNullOrEmpty(Unit))
+				{
+					switch (Unit.ToLower())
+					{
+						case "m":
+							break;
+
+						case "ft":
+							Altitude *= 0.3048;
+							break;
+
+						default:
+							return false;
+					}
+				}
+
+				Value = new GeoPosition(Latitude, Longitude, Altitude);
+			}
+
+			return true;
+		}
+
+		private static bool TryParse(Group Deg, Group Minute, Group Second, Group Sign,
+			double Min, double Max, out double Value)
+		{
+			int i;
+
+			if (Second is null || string.IsNullOrEmpty(Second.Value))
+				Value = 0;
+			else if (!TryParse(Second.Value, out Value))
+				return false;
+
+			Value /= 60;
+
+			if (!string.IsNullOrEmpty(Minute?.Value))
+			{
+				if (!int.TryParse(Minute.Value, out i))
+					return false;
+
+				Value += i;
+			}
+
+			Value /= 60;
+
+			if (!int.TryParse(Deg.Value, out i))
+				return false;
+
+			Value += i;
+
+			if (Sign is null || string.IsNullOrEmpty(Sign.Value) || Sign.Value.Length != 1)
+				return false;
+
+			char ch = char.ToUpper(Sign.Value[0]);
+
+			if (ch == 'S' || ch == 'W')
+				Value = -Value;
+			else if (ch != 'N' && ch != 'E')
+				return false;
+
+			if (Value < Min || Value > Max)
+				return false;
+
+			return true;
+		}
+
 		/// <summary>
 		/// String-representation of the geo-position, for use in XML.
 		/// </summary>
@@ -168,14 +289,14 @@ namespace Waher.Runtime.Geo
 			{
 				StringBuilder sb = new StringBuilder();
 
-				sb.Append(this.latitude.ToString().Replace(System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator, "."));
+				sb.Append(ToString(this.latitude));
 				sb.Append(',');
-				sb.Append(this.longitude.ToString().Replace(System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator, "."));
+				sb.Append(ToString(this.longitude));
 
 				if (this.altitude.HasValue)
 				{
 					sb.Append(',');
-					sb.Append(this.altitude.Value.ToString().Replace(System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator, "."));
+					sb.Append(ToString(this.altitude.Value));
 				}
 
 				return sb.ToString();
@@ -194,6 +315,13 @@ namespace Waher.Runtime.Geo
 				Append(this.latitude, 'N', 'S', sb);
 				sb.Append(' ');
 				Append(this.longitude, 'E', 'W', sb);
+
+				if (this.altitude.HasValue)
+				{
+					sb.Append(", ");
+					sb.Append(this.altitude.Value.ToString().Replace(".", System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator));
+					sb.Append(" m");
+				}
 
 				return sb.ToString();
 			}
@@ -240,7 +368,7 @@ namespace Waher.Runtime.Geo
 		/// <inheritdoc/>
 		public override bool Equals(object obj)
 		{
-			return 
+			return
 				obj is GeoPosition P &&
 				this.latitude == P.latitude &&
 				this.longitude == P.longitude &&
@@ -272,7 +400,7 @@ namespace Waher.Runtime.Geo
 
 			if (A is null)
 				return true;
-			
+
 			return A.Equals(B);
 		}
 

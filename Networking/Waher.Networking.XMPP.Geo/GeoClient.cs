9480630fe29183fc497136b05425343101f44f02
Peter Waher <peterwaher@hotmail.com>
2025-05-23 18:23:07 -0400

    Geo-spatial subscription

--- Networking/Waher.Networking.XMPP.Geo/GeoClient.cs
+++ Networking/Waher.Networking.XMPP.Geo/GeoClient.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
+using Waher.Content;
+using Waher.Content.Xml;
+using Waher.Events;
 using Waher.Networking.XMPP.Events;
+using Waher.Runtime.Geo;
 
 namespace Waher.Networking.XMPP.Geo
 {
@@ -87,6 +92,310 @@ namespace Waher.Networking.XMPP.Geo
 		/// Component address.
 		/// </summary>
 		public string ComponentAddress => this.componentAddress;
+
+		#endregion
+
+		#region Subscribe
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Callback">Called when a response is returned.</param>
+		/// <param name="State">State object to pass on to callback method.</param>
+		public Task Subscribe(GeoPosition Min, GeoPosition Max,
+			EventHandlerAsync<SubscribedEventArgs> Callback, object State)
+		{
+			return this.Subscribe(string.Empty, Min, Max, null, Callback, State);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <param name="Callback">Called when a response is returned.</param>
+		/// <param name="State">State object to pass on to callback method.</param>
+		public Task Subscribe(GeoPosition Min, GeoPosition Max, int Ttl,
+			EventHandlerAsync<SubscribedEventArgs> Callback, object State)
+		{
+			return this.Subscribe(string.Empty, Min, Max, Ttl, Callback, State);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="SubscriptionId">Identifier of existing subscription, if updating it.</param>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <param name="Callback">Called when a response is returned.</param>
+		/// <param name="State">State object to pass on to callback method.</param>
+		public Task Subscribe(string SubscriptionId, GeoPosition Min, GeoPosition Max, int? Ttl, 
+			EventHandlerAsync<SubscribedEventArgs> Callback, object State)
+		{
+			return this.Subscribe(this.componentAddress, SubscriptionId, Min, Max, Ttl, Callback, State);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="ComponentAddress">Geo-spatial component address.</param>
+		/// <param name="SubscriptionId">Identifier of existing subscription, if updating it.</param>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <param name="Callback">Called when a response is returned.</param>
+		/// <param name="State">State object to pass on to callback method.</param>
+		public async Task Subscribe(string ComponentAddress, string SubscriptionId,
+			GeoPosition Min, GeoPosition Max, int? Ttl,
+			EventHandlerAsync<SubscribedEventArgs> Callback, object State)
+		{
+			if (Min is null)
+				throw new ArgumentNullException(nameof(Min));
+
+			if (Max is null)
+				throw new ArgumentNullException(nameof(Max));
+
+			StringBuilder Xml = new StringBuilder();
+
+			Xml.Append("<subscribe xmlns='");
+			Xml.Append(NamespaceGeoSpatialNeuroFoundationV1);
+
+			if (!string.IsNullOrEmpty(SubscriptionId))
+			{
+				Xml.Append("' id='");
+				Xml.Append(XML.Encode(ComponentAddress));
+			}
+
+			Xml.Append("' minLat='");
+			Xml.Append(CommonTypes.Encode(Min.Latitude));
+
+			Xml.Append("' minLon='");
+			Xml.Append(CommonTypes.Encode(Min.Longitude));
+
+			if (Min.Altitude.HasValue)
+			{
+				Xml.Append("' minAlt='");
+				Xml.Append(CommonTypes.Encode(Min.Altitude.Value));
+			}
+
+			Xml.Append("' maxLat='");
+			Xml.Append(CommonTypes.Encode(Max.Latitude));
+
+			Xml.Append("' maxLon='");
+			Xml.Append(CommonTypes.Encode(Max.Longitude));
+
+			if (Max.Altitude.HasValue)
+			{
+				Xml.Append("' maxAlt='");
+				Xml.Append(CommonTypes.Encode(Max.Altitude.Value));
+			}
+
+			if (Ttl.HasValue)
+			{
+				Xml.Append("' ttl='");
+				Xml.Append(Ttl.Value);
+			}
+
+			Xml.Append("'/>");
+
+			await this.client.SendIqSet(ComponentAddress, Xml.ToString(), async (sender, e) =>
+			{
+				await Callback.Raise(this, new SubscribedEventArgs(e));
+			}, State);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <returns>Subscription result.</returns>
+		/// <exception cref="Exception">In case an error is returned.</exception>
+		public Task<SubscribedEventArgs> SubscribeAsync(GeoPosition Min, GeoPosition Max)
+		{
+			return this.SubscribeAsync(string.Empty, Min, Max, null);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <returns>Subscription result.</returns>
+		/// <exception cref="Exception">In case an error is returned.</exception>
+		public Task<SubscribedEventArgs> SubscribeAsync(GeoPosition Min, GeoPosition Max, int Ttl)
+		{
+			return this.SubscribeAsync(string.Empty, Min, Max, Ttl);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="SubscriptionId">Identifier of existing subscription, if updating it.</param>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <returns>Subscription result.</returns>
+		/// <exception cref="Exception">In case an error is returned.</exception>
+		public Task<SubscribedEventArgs> SubscribeAsync(string SubscriptionId, GeoPosition Min, GeoPosition Max, int? Ttl)
+		{
+			return this.SubscribeAsync(this.componentAddress, SubscriptionId, Min, Max, Ttl);
+		}
+
+		/// <summary>
+		/// Subscribes to geo-spatial information, or updates an existing subscription.
+		/// 
+		/// The client subscribes to geo-spatial information using a bounding box.
+		/// The bounding box is defined using to coordinates using the Mercator projection, 
+		/// which is a cylindrical map projection. 
+		/// 
+		/// If the minimum longitude is greater than the maximum longitude value, the 
+		/// bounding box is assumed to wrap around the antimeridian, or the +- 180 degree 
+		/// longitude meridian.
+		/// </summary>
+		/// <param name="ComponentAddress">Geo-spatial component address.</param>
+		/// <param name="SubscriptionId">Identifier of existing subscription, if updating it.</param>
+		/// <param name="Min">Lower left-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Max">Upper right-hand corner of bounding box, using the Mercator projection.</param>
+		/// <param name="Ttl">Number of seconds the subscription should be alive, unless
+		/// the connection is dropped (for a local subscription) or subscription is updated.</param>
+		/// <returns>Subscription result.</returns>
+		/// <exception cref="Exception">In case an error is returned.</exception>
+		public async Task<SubscribedEventArgs> SubscribeAsync(string ComponentAddress, string SubscriptionId,
+			GeoPosition Min, GeoPosition Max, int? Ttl)
+		{
+			TaskCompletionSource<SubscribedEventArgs> Result = new TaskCompletionSource<SubscribedEventArgs>();
+
+			await this.Subscribe(ComponentAddress, SubscriptionId, Min, Max, Ttl, (sender, e) =>
+			{
+				if (e.Ok)
+					Result.TrySetResult(e);
+				else
+					Result.TrySetException(e.StanzaError ?? new Exception("Unable to perform subscription."));
+
+				return Task.CompletedTask;
+			}, null);
+
+			return await Result.Task;
+		}
+
+		#endregion
+
+		#region Unsubscribe 
+
+		public async Task Unsubscribe()
+		{
+			// TODO
+		}
+
+		public async Task UnsubscribeAsync()
+		{
+			// TODO
+		}
+
+		#endregion
+
+		#region Publications
+
+		public async Task Publish()
+		{
+			// TODO
+		}
+
+		public async Task PublishAsync()
+		{
+			// TODO
+		}
+
+		public async Task Delete()
+		{
+			// TODO
+		}
+
+		public async Task DeleteAsync()
+		{
+			// TODO
+		}
+
+		#endregion
+
+		#region Search
+
+		public async Task Search()
+		{
+			// TODO
+		}
+
+		public async Task SearchAsync()
+		{
+			// TODO
+		}
 
 		#endregion
 
---
+++ Networking/Waher.Networking.XMPP.Geo/SubscribedEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using Waher.Content.Xml;
+using Waher.Networking.XMPP.Events;
+
+namespace Waher.Networking.XMPP.Geo
+{
+	/// <summary>
+	/// Event arguments for the result of a subscription request.
+	/// </summary>
+	public class SubscribedEventArgs : IqResultEventArgs
+	{
+		/// <summary>
+		/// Event arguments for the result of a subscription request.
+		/// </summary>
+		/// <param name="e">Response</param>
+		public SubscribedEventArgs(IqResultEventArgs e)
+			: base(e)
+		{
+			if (e.Ok &&
+				!(e.Response is null) &&
+				e.Response.LocalName == "subscribed" &&
+				e.Response.NamespaceURI == GeoClient.NamespaceGeoSpatialNeuroFoundationV1)
+			{
+				this.SubscriptionId = XML.Attribute(e.Response, "id");
+				this.Ttl = XML.Attribute(e.Response, "ttl", 0);
+			}
+			else
+				e.Ok = false;
+		}
+
+		/// <summary>
+		/// Identifier of the subscription.
+		/// </summary>
+		public string SubscriptionId { get; }
+
+		/// <summary>
+		/// Number of seconds before the subscription gets automatically unsubscribed, unless updated.
+		/// </summary>
+		public int Ttl { get; }
+	}
+}

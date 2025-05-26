11d8cfa2ef76d29603b6143f237ce77c3f93fa03
Peter Waher <peterwaher@hotmail.com>
2025-05-26 13:28:57 -0400

    Adding static methods for simple access to encrypt & decrypt functions from code behind

--- Script/Waher.Script.Cryptography/Functions/Encryption/Aes256Encrypt.cs
+++ Script/Waher.Script.Cryptography/Functions/Encryption/Aes256Encrypt.cs
@@ -88,6 +88,29 @@ namespace Waher.Script.Cryptography.Functions.Encryption
 			CipherMode CipherMode = c <= 3 ? CipherMode.CBC : this.ToEnum<CipherMode>(Arguments[3]);
 			PaddingMode PaddingMode = c <= 4 ? PaddingMode.PKCS7 : this.ToEnum<PaddingMode>(Arguments[4]);
 
+			return new ObjectValue(Encrypt(Data, Key, IV, CipherMode, PaddingMode));
+		}
+
+		/// <summary>
+		/// Performs AES encryption of data.
+		/// </summary>
+		/// <param name="Data">Binary Data</param>
+		/// <param name="Key">Symmetric Key</param>
+		/// <param name="IV">Initialization Vector</param>
+		/// <param name="CipherMode">Cipher Mode</param>
+		/// <param name="PaddingMode">Padding Mode</param>
+		/// <returns>Encrypted data.</returns>
+		public static byte[] Encrypt(byte[] Data, byte[] Key, byte[] IV, CipherMode CipherMode = CipherMode.CBC, PaddingMode PaddingMode = PaddingMode.PKCS7)
+		{
+			if (Data is null)
+				throw new ArgumentNullException(nameof(Data));
+
+			if (Key is null)
+				throw new ArgumentNullException(nameof(Key));
+
+			if (IV is null)
+				throw new ArgumentNullException(nameof(IV));
+
 			using (Aes Aes = Aes.Create())
 			{
 				Aes.BlockSize = 128;
@@ -97,10 +120,10 @@ namespace Waher.Script.Cryptography.Functions.Encryption
 
 				using (ICryptoTransform Transform = Aes.CreateEncryptor(Key, IV))
 				{
-					byte[] Encrypted = Transform.TransformFinalBlock(Data, 0, Data.Length);
-					return new ObjectValue(Encrypted);
+					return Transform.TransformFinalBlock(Data, 0, Data.Length);
 				}
 			}
 		}
+
 	}
 }

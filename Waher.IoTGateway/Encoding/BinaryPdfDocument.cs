69b9a118313035644a1db572cfb0d98f6bb4d5d3
Peter Waher <peterwaher@hotmail.com>
2025-06-02 12:35:43 -0400

    Binary PDF Document Codec, allowing attachments of PDF documents.

---
+++ Waher.IoTGateway/Encoding/BinaryPdfDocument.cs
@@ -0,0 +1,22 @@
+﻿namespace Waher.IoTGateway.Encoding
+{
+	/// <summary>
+	/// A Binary PDF Document
+	/// </summary>
+	public class BinaryPdfDocument
+	{
+		/// <summary>
+		/// A Binary PDF Document
+		/// </summary>
+		/// <param name="Document">Document</param>
+		public BinaryPdfDocument(byte[] Document)
+		{
+			this.Document = Document;
+		}
+
+		/// <summary>
+		/// Binary representation of PDF document.
+		/// </summary>
+		public byte[] Document { get; }
+	}
+}

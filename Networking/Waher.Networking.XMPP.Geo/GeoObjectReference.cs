f4e0786a8dde3efe87f24ce7c3957e7953e6e46e
Peter Waher <peterwaher@hotmail.com>
2025-06-16 16:29:02 -0400

    Geo-spatial contract events

---
+++ Networking/Waher.Networking.XMPP.Geo/GeoObjectReference.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Xml;
+using Waher.Runtime.Geo;
+
+namespace Waher.Networking.XMPP.Geo
+{
+	/// <summary>
+	/// Represents a reference to a geo-spatial object.
+	/// </summary>
+	public class GeoObjectReference
+	{
+		/// <summary>
+		/// Represents a reference to a geo-spatial object.
+		/// </summary>
+		public GeoObjectReference()
+		{
+		}
+
+		/// <summary>
+		/// Geo-spatial object reference identifier.
+		/// </summary>
+		public string GeoId { get; set; }
+
+		/// <summary>
+		/// XML definition of object reference.
+		/// </summary>
+		public XmlElement Definition { get; set; }
+
+		/// <summary>
+		/// Bare JID of creator, if available.
+		/// Publications resulting from internal processing, such as geo-localization of 
+		/// contracts, or device registrations, will lack a creator registration.
+		/// </summary>
+		public string Creator { get; set; }
+
+		/// <summary>
+		/// Position of object
+		/// </summary>
+		public GeoPosition Position { get; set; }
+
+		/// <summary>
+		/// Number of seconds until the object expires, unless updated, if defined.
+		/// </summary>
+		public int? TimeToLive { get; set; }
+
+		/// <summary>
+		/// When information was created.
+		/// Timestamps should use UTC.
+		/// </summary>
+		public DateTime Created { get; set; }
+
+		/// <summary>
+		/// When information was last updated (if updated).
+		/// Timestamps should use UTC.
+		/// </summary>
+		public DateTime? Updated { get; set; }
+
+		/// <summary>
+		/// Timestamp indicating from what point in time publication is valid.
+		/// Can be used to publish a future location object reference.
+		/// Timestamps should use UTC.
+		/// </summary>
+		public DateTime? From { get; set; }
+
+		/// <summary>
+		/// Timestamp indicating to what point in time publication is valid.
+		/// Can be used to publish an expected expiration timestamp for a location object reference.
+		/// Timestamps should use UTC.
+		/// </summary>
+		public DateTime? To { get; set; }
+	}
+}
---
+++ Networking/Waher.Networking.XMPP.Geo/GeoObjectSubscriptionEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using Waher.Networking.XMPP.Events;
+
+namespace Waher.Networking.XMPP.Geo
+{
+	/// <summary>
+	/// Event arguments for events related to Geo-spatial objects.
+	/// </summary>
+	public class GeoObjectSubscriptionEventArgs : MessageEventArgs
+	{
+		/// <summary>
+		/// Event arguments for events related to Geo-spatial objects.
+		/// </summary>
+		/// <param name="e">Message event arguments.</param>
+		/// <param name="SubscriptionId">Geo-spatial subscription identifier.</param>
+		/// <param name="Reference">Reference to geo-spatial object.</param>
+		public GeoObjectSubscriptionEventArgs(MessageEventArgs e, string SubscriptionId, GeoObjectReference Reference)
+			: base(e)
+		{
+			this.SubscriptionId = SubscriptionId;
+			this.Reference = Reference;
+		}
+
+		/// <summary>
+		/// Geo-spatial subscription identifier.
+		/// </summary>
+		public string SubscriptionId { get; }
+
+		/// <summary>
+		/// Reference to geo-spatial object.
+		/// </summary>
+		public GeoObjectReference Reference { get; }
+	}
+}

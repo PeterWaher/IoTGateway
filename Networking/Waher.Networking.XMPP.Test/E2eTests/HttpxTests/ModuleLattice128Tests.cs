9541017e0773363a918505ba5a93ee13e24afa12
Peter Waher <peterwaher@hotmail.com>
2025-08-09 12:53:24 +0200

    Adding Module-Lattice unit E2EE tests

---
+++ Networking/Waher.Networking.XMPP.Test/E2eTests/HttpxTests/ModuleLattice128Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Test.E2eTests.HttpxTests
+{
+    [TestClass]
+    public class ModuleLattice128Tests : XmppHttpxTests
+	{
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext _)
+		{
+			SetupSnifferAndLog();
+		}
+
+		[ClassCleanup]
+		public static async Task ClassCleanup()
+		{
+			await DisposeSnifferAndLog();
+		}
+
+		public override int SecurityStrength => 128;
+
+		public override IE2eEndpoint[] GenerateEndpoints(IE2eSymmetricCipher Cipher)
+        {
+            return [new ModuleLattice128Endpoint(Cipher)];
+        }
+
+    }
+}
---
+++ Networking/Waher.Networking.XMPP.Test/E2eTests/ModuleLattice192Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Test.E2eTests
+{
+    [TestClass]
+    public class ModuleLattice192Tests : XmppE2eTests
+	{
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext _)
+		{
+			SetupSnifferAndLog();
+		}
+
+		[ClassCleanup]
+		public static async Task ClassCleanup()
+		{
+			await DisposeSnifferAndLog();
+		}
+
+		public override int SecurityStrength => 192;
+
+		public override IE2eEndpoint[] GenerateEndpoints(IE2eSymmetricCipher Cipher)
+        {
+            return [new ModuleLattice192Endpoint(Cipher)];
+        }
+
+    }
+}

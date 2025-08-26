e1ef3b569808c38bf289988595b3ce390c4d9639
Peter Waher <peterwaher@hotmail.com>
2025-08-26 12:48:02 +0200

    Script source in script runtime exceptions & log entries.

--- Script/Waher.Script/Exceptions/ScriptRuntimeErrorException.cs
+++ Script/Waher.Script/Exceptions/ScriptRuntimeErrorException.cs
@@ -14,7 +14,7 @@ namespace Waher.Script.Exceptions
 		/// <param name="Message">Message text.</param>
 		/// <param name="Node">Script node where syntax error was detected.</param>
 		public ScriptRuntimeErrorException(string Message, ScriptNode Node)
-			: base(Message, Node)
+			: base(Message, Node, true)
 		{
 		}
 
@@ -25,7 +25,7 @@ namespace Waher.Script.Exceptions
 		/// <param name="Node">Script node where syntax error was detected.</param>
 		/// <param name="InnerErrorException">Inner exception.</param>
 		public ScriptRuntimeErrorException(string Message, ScriptNode Node, Exception InnerErrorException)
-			: base(Message, Node, InnerErrorException)
+			: base(Message, Node, InnerErrorException, true)
 		{
 		}
 	}

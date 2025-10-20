49d9a7581771c5cea9e89546ebefdf8e255f2094
Peter Waher <peterwaher@hotmail.com>
2025-10-20 14:29:17 -0300

    Fix: Improved error message

--- Waher.IoTGateway.Resources/Root/Sparql.js
+++ Waher.IoTGateway.Resources/Root/Sparql.js
@@ -72,64 +72,81 @@ function ExecuteQuery()
 		{
 			var Result = document.getElementById("Result");
 
-			if (IsHtml)
-				Result.innerHTML = "<legend>Result</legend>" + xhttp.responseText;
-			else
+			switch (xhttp.status)
 			{
-				Result.innerHTML = "<legend>Result</legend>";
-
-				if (IsBinary)
-				{
-					var Blob = xhttp.response;
-					var Url = window.URL.createObjectURL(Blob);
-
-					var A = document.createElement("A");
-					A.href = Url;
-					A.download = "SPARQL Result Set.xlsx";
-
-					Result.appendChild(A);
-					A.click();
-					Result.removeChild(A);
-
-					var P = document.createElement("P");
-					P.innerText = "The result has been downloaded.";
-					Result.appendChild(P);
-
-					window.URL.revokeObjectURL(Url);
-				}
-				else if (IsSvg)
-				{
-					var Div = document.createElement("DIV");
-					Result.appendChild(Div);
-
-					Div.innerHTML = xhttp.responseText;
-}
-				else if (IsPng)
-				{
-					var Blob = xhttp.response;
-					var Reader = new FileReader();
-
-					Reader.onload = function (e)
+				case 200:
+					if (IsHtml)
+						Result.innerHTML = "<legend>Result</legend>" + xhttp.responseText;
+					else
 					{
-						var Img = document.createElement("IMG");
-						Img.src = e.target.result;
-						Img.alt = "SPARQL Result Image";
+						Result.innerHTML = "<legend>Result</legend>";
 
-						Result.appendChild(Img);
-					};
+						if (IsBinary)
+						{
+							var Blob = xhttp.response;
+							var Url = window.URL.createObjectURL(Blob);
 
-					Reader.readAsDataURL(Blob);
-				}
-				else
-				{
-					var Pre = document.createElement("PRE");
-					Result.appendChild(Pre);
+							var A = document.createElement("A");
+							A.href = Url;
+							A.download = "SPARQL Result Set.xlsx";
 
-					var Code = document.createElement("CODE");
-					Pre.appendChild(Code);
+							Result.appendChild(A);
+							A.click();
+							Result.removeChild(A);
 
-					Code.innerText = xhttp.responseText;
-				}
+							var P = document.createElement("P");
+							P.innerText = "The result has been downloaded.";
+							Result.appendChild(P);
+
+							window.URL.revokeObjectURL(Url);
+						}
+						else if (IsSvg)
+						{
+							var Div = document.createElement("DIV");
+							Result.appendChild(Div);
+
+							Div.innerHTML = xhttp.responseText;
+						}
+						else if (IsPng)
+						{
+							var Blob = xhttp.response;
+							var Reader = new FileReader();
+
+							Reader.onload = function (e)
+							{
+								var Img = document.createElement("IMG");
+								Img.src = e.target.result;
+								Img.alt = "SPARQL Result Image";
+
+								Result.appendChild(Img);
+							};
+
+							Reader.readAsDataURL(Blob);
+						}
+						else
+						{
+							var Pre = document.createElement("PRE");
+							Result.appendChild(Pre);
+
+							var Code = document.createElement("CODE");
+							Pre.appendChild(Code);
+
+							Code.innerText = xhttp.responseText;
+						}
+					}
+					break;
+
+				default:
+					var s = "<legend>Error " + xhttp.status + " - ";
+
+					if (xhttp.statusText)
+						s += xhttp.statusText;
+					else
+						s += GetStatusText(xhttp.status);
+
+					Result.innerHTML += "</legend>" + xhttp.responseText;
+					Result.innerHTML = s;
+					break;
 			}
 
 			Result.setAttribute("style", "display:block");
@@ -209,4 +226,79 @@ function ExecuteQuery()
 	}
 
 	xhttp.send(Form);
+}
+
+function GetStatusText(status)
+{
+	switch (status)
+	{
+		case 100: return "Continue";
+		case 101: return "Switching Protocols";
+		case 102: return "Processing";
+		case 103: return "Early Hints";
+
+		case 200: return "OK";
+		case 201: return "Created";
+		case 202: return "Accepted";
+		case 203: return "Non-Authoritative Information";
+		case 204: return "No Content";
+		case 205: return "Reset Content";
+		case 206: return "Partial Content";
+		case 207: return "Multi-Status";
+		case 208: return "Already Reported";
+		case 226: return "IM Used";
+
+		case 300: return "Multiple Choices";
+		case 301: return "Moved Permanently";
+		case 302: return "Found";
+		case 303: return "See Other";
+		case 304: return "Not Modified";
+		case 305: return "Use Proxy";
+		case 307: return "Temporary Redirect";
+		case 308: return "Permanent Redirect";
+
+		case 400: return "Bad Request";
+		case 401: return "Unauthorized";
+		case 402: return "Payment Required";
+		case 403: return "Forbidden";
+		case 404: return "Not Found";
+		case 405: return "Method Not Allowed";
+		case 406: return "Not Acceptable";
+		case 407: return "Proxy Authentication Required";
+		case 408: return "Request Timeout";
+		case 409: return "Conflict";
+		case 410: return "Gone";
+		case 411: return "Length Required";
+		case 412: return "Precondition Failed";
+		case 413: return "Content Too Large";
+		case 414: return "URI Too Long";
+		case 415: return "Unsupported Media Type";
+		case 416: return "Range Not Satisfiable";
+		case 417: return "Expectation Failed";
+		case 418: return "I'm a Teapot";
+		case 421: return "Misdirected Request";
+		case 422: return "Unprocessable Content";
+		case 423: return "Locked";
+		case 424: return "Failed Dependency";
+		case 425: return "Too Early";
+		case 426: return "Upgrade Required";
+		case 428: return "Precondition Required";
+		case 429: return "Too Many Requests";
+		case 431: return "Request Header Fields Too Large";
+		case 451: return "Unavailable For Legal Reasons";
+
+		case 500: return "Internal Server Error";
+		case 501: return "Not Implemented";
+		case 502: return "Bad Gateway";
+		case 503: return "Service Unavailable";
+		case 504: return "Gateway Timeout";
+		case 505: return "HTTP Version Not Supported";
+		case 506: return "Variant Also Negotiates";
+		case 507: return "Insufficient Storage";
+		case 508: return "Loop Detected";
+		case 510: return "Not Extended";
+		case 511: return "Network Authentication Required";
+
+		default: return "Unknown Status";
+	}
 }

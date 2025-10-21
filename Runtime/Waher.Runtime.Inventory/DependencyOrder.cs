7f1f89fac00fce59e9c6919e5e57247c26b523cd
Peter Waher <peterwaher@hotmail.com>
2025-10-20 21:23:36 -0300

    Fix: Module dependencies & start order.

--- Runtime/Waher.Runtime.Inventory/DependencyOrder.cs
+++ Runtime/Waher.Runtime.Inventory/DependencyOrder.cs
@@ -21,32 +21,40 @@ namespace Waher.Runtime.Inventory
 			Type TypeY = y.GetType();
 			IEnumerable<ModuleDependencyAttribute> AttrsX = TypeX.GetCustomAttributes<ModuleDependencyAttribute>();
 			IEnumerable<ModuleDependencyAttribute> AttrsY = TypeY.GetCustomAttributes<ModuleDependencyAttribute>();
+			bool XHasDependencies = false;
+			bool YHasDependencies = false;
 			bool XDependsOnY = false;
 			bool YDependsOnX = false;
 
-			if (!(AttrsX is null))
+			foreach (ModuleDependencyAttribute AttrX in AttrsX)
 			{
-				foreach (ModuleDependencyAttribute AttrX in AttrsX)
+				XHasDependencies = true;
+
+				if (AttrX.DependsOn(TypeY))
 				{
-					if (AttrX.DependsOn(TypeY))
-					{
-						XDependsOnY = true;
-						break;
-					}
+					XDependsOnY = true;
+					break;
 				}
 			}
 
-			if (!(AttrsY is null))
+			foreach (ModuleDependencyAttribute AttrY in AttrsY)
 			{
-				foreach (ModuleDependencyAttribute AttrY in AttrsY)
+				YHasDependencies = true;
+
+				if (AttrY.DependsOn(TypeX))
 				{
-					if (AttrY.DependsOn(TypeX))
-					{
-						YDependsOnX = true;
-						break;
-					}
+					YDependsOnX = true;
+					break;
 				}
 			}
+
+			if (XHasDependencies)
+			{
+				if (!YHasDependencies)
+					return 1;
+			}
+			else if (YHasDependencies)
+				return -1;
 
 			int i = XDependsOnY.CompareTo(YDependsOnX);
 			if (i != 0)

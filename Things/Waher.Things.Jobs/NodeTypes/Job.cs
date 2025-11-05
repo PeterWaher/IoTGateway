264f6a397138df9b904e010e961a8cf0cbf2c350
Peter Waher <peterwaher@hotmail.com>
2025-11-04 21:53:24 -0300

    Jobs

---
+++ Things/Waher.Things.Jobs/NodeTypes/Job.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Waher.Runtime.Collections;
+using Waher.Runtime.Language;
+
+namespace Waher.Things.Jobs.NodeTypes
+{
+	/// <summary>
+	/// Represents a job.
+	/// </summary>
+	public class Job : JobNode
+	{
+		/// <summary>
+		/// Represents a job.
+		/// </summary>
+		public Job()
+		{
+		}
+
+		/// <summary>
+		/// Gets the type name of the node.
+		/// </summary>
+		/// <param name="Language">Language to use.</param>
+		/// <returns>Localized type node.</returns>
+		public override Task<string> GetTypeNameAsync(Language Language)
+		{
+			return Language.GetStringAsync(typeof(JobSource), 18, "Job Node");
+		}
+
+		/// <summary>
+		/// If the node accepts a presumptive child, i.e. can receive as a child (if that child accepts the node as a parent).
+		/// </summary>
+		/// <param name="Child">Presumptive child node.</param>
+		/// <returns>If the child is acceptable.</returns>
+		public override Task<bool> AcceptsChildAsync(INode Child)
+		{
+			return Task.FromResult(false);
+		}
+
+		/// <summary>
+		/// If the node accepts a presumptive parent, i.e. can be added to that parent (if that parent accepts the node as a child).
+		/// </summary>
+		/// <param name="Parent">Presumptive parent node.</param>
+		/// <returns>If the parent is acceptable.</returns>
+		public override Task<bool> AcceptsParentAsync(INode Parent)
+		{
+			return Task.FromResult(Parent is Root);
+		}
+	}
+}

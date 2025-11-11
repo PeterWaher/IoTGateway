2dc1ad7ce10a425e399bee975c6511a6b6da0d7b
Peter Waher <peterwaher@hotmail.com>
2025-11-11 12:52:30 -0300

    Scheduling jobs

---
+++ Jobs/Waher.Jobs/JobScheduler.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using Waher.Jobs.NodeTypes;
+using Waher.Runtime.Inventory;
+using Waher.Runtime.Timing;
+using Waher.Things;
+
+namespace Waher.Jobs
+{
+	/// <summary>
+	/// Module that manages the scheduling and execution of jobs.
+	/// </summary>
+	public class JobScheduler : IModule
+	{
+		private static Scheduler scheduler = null;
+		private bool disposeScheduler = false;
+
+		/// <summary>
+		/// Module that manages the scheduling and execution of jobs.
+		/// </summary>
+		public JobScheduler()
+		{
+		}
+
+		/// <summary>
+		/// Scheduler used to schedule jobs.
+		/// </summary>
+		public static Scheduler Scheduler => scheduler;
+
+		/// <summary>
+		/// Starts the module.
+		/// </summary>
+		public async Task Start()
+		{
+			if (scheduler is null)
+			{
+				if (Types.TryGetModuleParameter("Scheduler", out scheduler))
+					this.disposeScheduler = false;
+				else
+				{
+					scheduler = new Scheduler();
+					this.disposeScheduler = true;
+				}
+			}
+
+			foreach (INode Node in await JobSource.Root.ChildNodes)
+			{
+				if (Node is Job Job)
+					Job.RescheduleJob();
+			}
+		}
+
+		/// <summary>
+		/// Stops the module.
+		/// </summary>
+		public Task Stop()
+		{
+			if (this.disposeScheduler)
+				scheduler?.Dispose();
+
+			scheduler = null;
+
+			return Task.CompletedTask;
+		}
+	}
+}
--- Jobs/Waher.Jobs/NodeTypes/Job.cs
+++ Jobs/Waher.Jobs/NodeTypes/Job.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Waher.Content;
+using Waher.Events;
 using Waher.Groups;
 using Waher.Jobs.Commands;
+using Waher.Persistence.Attributes;
 using Waher.Runtime.Collections;
 using Waher.Runtime.Language;
 using Waher.Things;
+using Waher.Things.Attributes;
 using Waher.Things.Queries;
 
 namespace Waher.Jobs.NodeTypes
@@ -14,11 +19,90 @@ namespace Waher.Jobs.NodeTypes
 	/// </summary>
 	public class Job : JobNode, IGroup
 	{
+		private DateTime? scheduledExecutionTime = null;
+		private DateTime? executionTime = null;
+		private Duration? period = null;
+		private Duration? burstInterval = null;
+		private int burstCount = 1;
+		private int burstCountLeft = 1;
+
 		/// <summary>
 		/// Represents a job.
 		/// </summary>
 		public Job()
 		{
+		}
+
+		/// <summary>
+		/// Gets or sets the scheduled execution time of the job.
+		/// </summary>
+		[Header(3, "Scheduled execution time:", 10)]
+		[Page(2, "Job", 0)]
+		[ToolTip(4, "When the job is scheduled to execute.")]
+		[DefaultValueNull]
+		public DateTime? ExecutionTime
+		{
+			get => this.executionTime;
+			set
+			{
+				if (this.executionTime != value)
+				{
+					this.executionTime = value;
+					Task.Run(async () => await this.RescheduleJobDelayed(1000));
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the scheduled execution time of the job.
+		/// </summary>
+		[Header(5, "Job Period:", 20)]
+		[Page(2, "Job", 0)]
+		[ToolTip(6, "Time between job executions. Defines a repetitive job.")]
+		[DefaultValueNull]
+		public Duration? Period
+		{
+			get => this.period;
+			set => this.period = value;
+		}
+
+		/// <summary>
+		/// Number of executions each period.
+		/// </summary>
+		[Header(7, "Burst count:", 30)]
+		[Page(2, "Job", 0)]
+		[ToolTip(8, "Number of times the job will be executed each period.")]
+		[DefaultValue(1)]
+		public int BurstCount
+		{
+			get => this.burstCount;
+			set => this.burstCount = value;
+		}
+
+		/// <summary>
+		/// Gets or sets the scheduled execution time of the job.
+		/// </summary>
+		[Header(9, "Burst Interval:", 40)]
+		[Page(2, "Job", 0)]
+		[ToolTip(10, "Time between job executions in a burst.")]
+		[DefaultValueNull]
+		public Duration? BurstInterval
+		{
+			get => this.burstInterval;
+			set => this.burstInterval = value;
+		}
+
+		/// <summary>
+		/// Number of executions each period.
+		/// </summary>
+		[Header(11, "Burst count left:", 50)]
+		[Page(2, "Job", 0)]
+		[ToolTip(12, "Number of times left the job will be executed within the current period.")]
+		[DefaultValue(1)]
+		public int BurstCountLeft
+		{
+			get => this.burstCountLeft;
+			set => this.burstCountLeft = value;
 		}
 
 		/// <summary>
@@ -134,5 +218,118 @@ namespace Waher.Jobs.NodeTypes
 				await Task.ExecuteTask(State);
 		}
 
+		/// <summary>
+		/// Reschedules the job, with a small delay.
+		/// </summary>
+		/// <param name="DelayMs">Milliseconds to delay the rescheduling of the job.</param>
+		public async Task RescheduleJobDelayed(int DelayMs)
+		{
+			await Task.Delay(DelayMs);
+			await this.RescheduleJob();
+		}
+
+		/// <summary>
+		/// Reschedules a job.
+		/// </summary>
+		public async Task RescheduleJob()
+		{
+			try
+			{
+				if (this.scheduledExecutionTime.HasValue)
+				{
+					JobScheduler.Scheduler?.Remove(this.scheduledExecutionTime.Value);
+					this.scheduledExecutionTime = null;
+				}
+
+				if (!this.executionTime.HasValue)
+					return;
+
+				DateTime Now = DateTime.UtcNow;
+				bool Changed = false;
+
+				while (this.executionTime.Value.ToUniversalTime() < Now &&
+					await this.AdvanceToNextTime(false))
+				{
+					Changed = true;
+				}
+
+				if (Changed)
+					await this.UpdateAsync();
+
+				if (this.executionTime.HasValue)
+				{
+					this.scheduledExecutionTime = JobScheduler.Scheduler?.Add(
+						this.executionTime.Value, this.ExecuteScheduledJob, null);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Exception(ex);
+			}
+		}
+
+		private async Task ExecuteScheduledJob(object _)
+		{
+			try
+			{
+				await this.ExecuteJob(await Translator.GetDefaultLanguageAsync());
+				await this.RemoveErrorAsync("ExecutionError");
+			}
+			catch (Exception ex)
+			{
+				await this.LogErrorAsync("ExecutionError", ex.Message);
+			}
+			finally
+			{
+				await this.AdvanceToNextTime(true);
+			}
+		}
+
+		private async Task<bool> AdvanceToNextTime(bool PersistChange)
+		{
+			if (!this.executionTime.HasValue)
+				return false;
+
+			DateTime TP = this.executionTime.Value;
+
+			if (this.burstInterval.HasValue &&
+				this.burstInterval.Value > Duration.Zero)
+			{
+				if (this.burstCountLeft > 0)
+				{
+					this.executionTime = TP + this.burstInterval.Value;
+					this.burstCountLeft--;
+
+					if (PersistChange)
+						await this.NodeUpdated();
+
+					return true;
+				}
+				else if (this.period.HasValue && this.period.Value > Duration.Zero)
+				{
+					this.burstCountLeft = this.burstCount;
+					this.executionTime = TP - this.burstCount * this.burstInterval.Value;
+				}
+				else
+					return false;
+			}
+
+			if (this.period.HasValue && this.period.Value > Duration.Zero)
+			{
+				this.executionTime += this.period.Value;
+
+				if (PersistChange)
+					await this.NodeUpdated();
+
+				return true;
+			}
+
+			this.executionTime = null;
+
+			if (PersistChange)
+				await this.NodeUpdated();
+
+			return false;
+		}
 	}
 }

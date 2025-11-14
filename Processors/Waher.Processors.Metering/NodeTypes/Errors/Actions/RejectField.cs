25147da10edfe2c3e164c152b3062a50ce64be7e
Peter Waher <peterwaher@hotmail.com>
2025-11-14 11:37:56 -0300

    Script error processing

---
+++ Processors/Waher.Processors.Metering/NodeTypes/Errors/Actions/RejectField.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Waher.Runtime.Language;
+using Waher.Things;
+
+namespace Waher.Processors.Metering.NodeTypes.Errors.Actions
+{
+	/// <summary>
+	/// Rejects a field.
+	/// </summary>
+	public class RejectField : DecisionTreeLeafStatement
+	{
+		/// <summary>
+		/// Rejects a field.
+		/// </summary>
+		public RejectField()
+			: base()
+		{
+		}
+
+		/// <summary>
+		/// Gets the type name of the node.
+		/// </summary>
+		/// <param name="Language">Language to use.</param>
+		/// <returns>Localized type node.</returns>
+		public override Task<string> GetTypeNameAsync(Language Language)
+		{
+			return Language.GetStringAsync(typeof(RejectField), 19, "Reject Field");
+		}
+
+		/// <summary>
+		/// Processes a single thing error.
+		/// </summary>
+		/// <param name="Device">Thing reporting the errors.</param>
+		/// <param name="Error">Error to process.</param>
+		/// <returns>Processed set of fields. Can be null if field does not pass processing.</returns>
+		public override Task<ThingError[]> ProcessError(INode Device, ThingError Error)
+		{
+			return Task.FromResult<ThingError[]>(null);
+		}
+	}
+}
--- Processors/Waher.Processors.Metering/NodeTypes/Errors/Actions/ScriptProcessing.cs
+++ Processors/Waher.Processors.Metering/NodeTypes/Errors/Actions/ScriptProcessing.cs
@@ -6,9 +6,8 @@ using Waher.Runtime.Language;
 using Waher.Script;
 using Waher.Things;
 using Waher.Things.Attributes;
-using Waher.Things.SensorData;
 
-namespace Waher.Processors.Metering.NodeTypes.Fields.Actions
+namespace Waher.Processors.Metering.NodeTypes.Errors.Actions
 {
 	/// <summary>
 	/// Processes a field using script.
@@ -32,7 +31,7 @@ namespace Waher.Processors.Metering.NodeTypes.Fields.Actions
 		[Header(2, "Script:", 0)]
 		[Page(1, "Script", 10)]
 		[ToolTip(3, "Script used to process fields.")]
-		[Text(TextPosition.AfterField, 5, "When executing the script, the variables Sensor and Field will contain references to the corresponding sensor and field objects being processed. Remember to return the field or array of fields you want to continue to process. Returning null or something that is not a field rejects the field from future processing.")]
+		[Text(TextPosition.AfterField, 5, "When executing the script, the variables Device and Error will contain references to the corresponding device and error objects being processed. Remember to return the error or array of errors you want to continue to process. Returning null or something that is not an error mutes the error from future processing.")]
 		[ContentType("application/x-webscript")]
 		public string[] Script
 		{
@@ -51,16 +50,16 @@ namespace Waher.Processors.Metering.NodeTypes.Fields.Actions
 		/// <returns>Localized type node.</returns>
 		public override Task<string> GetTypeNameAsync(Language Language)
 		{
-			return Language.GetStringAsync(typeof(ScriptProcessing), 4, "Process field using script");
+			return Language.GetStringAsync(typeof(ScriptProcessing), 4, "Process error using script");
 		}
 
 		/// <summary>
-		/// Processes a single sensor data field.
+		/// Processes a single thing error.
 		/// </summary>
-		/// <param name="Sensor">Sensor reporting the field.</param>
-		/// <param name="Field">Field to process.</param>
+		/// <param name="Device">Thing reporting the errors.</param>
+		/// <param name="Error">Error to process.</param>
 		/// <returns>Processed set of fields. Can be null if field does not pass processing.</returns>
-		public override async Task<Field[]> ProcessField(ISensor Sensor, Field Field)
+		public override async Task<ThingError[]> ProcessError(INode Device, ThingError Error)
 		{
 			if (this.parsedScript is null)
 			{
@@ -74,30 +73,30 @@ namespace Waher.Processors.Metering.NodeTypes.Fields.Actions
 
 			Variables Variables = new Variables()
 			{
-				{ "Sensor", Sensor },
-				{ "Field", Field }
+				{ "Device", Device },
+				{ "Error", Error }
 			};
 
 			object Result = await this.parsedScript.EvaluateAsync(Variables);
 
-			if (Result is Field Field2)
-				return new Field[] { Field2 };
-			else if (Result is Field[] Fields)
-				return Fields;
+			if (Result is ThingError Error2)
+				return new ThingError[] { Error2 };
+			else if (Result is ThingError[] Errors)
+				return Errors;
 			else if (Result is Array A)
 			{
-				ChunkedList<Field> Fields2 = null;
+				ChunkedList<ThingError> Errors2 = null;
 
 				foreach (object Item in A)
 				{
-					if (Item is Field Field3)
+					if (Item is ThingError Error3)
 					{
-						Fields2 ??= new ChunkedList<Field>();
-						Fields2.Add(Field3);
+						Errors2 ??= new ChunkedList<ThingError>();
+						Errors2.Add(Error3);
 					}
 				}
 
-				return Fields2?.ToArray();
+				return Errors2?.ToArray();
 			}
 			else
 				return null;

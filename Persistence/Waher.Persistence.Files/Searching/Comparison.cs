11d67cc94cf3563373c05b717b1b5a4fe5cae7a7
Peter Waher <peterwaher@hotmail.com>
2025-11-21 15:39:50 -0300

    Minor edits

--- Persistence/Waher.Persistence.Files/Searching/Comparison.cs
+++ Persistence/Waher.Persistence.Files/Searching/Comparison.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Waher.Persistence.Serialization;
 
 namespace Waher.Persistence.Files.Searching
--- Persistence/Waher.Persistence.Files/Searching/RangeInfo.cs
+++ Persistence/Waher.Persistence.Files/Searching/RangeInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Waher.Persistence.Files.Searching
+﻿namespace Waher.Persistence.Files.Searching
 {
 	/// <summary>
 	/// Contains information about a range in a search operation.

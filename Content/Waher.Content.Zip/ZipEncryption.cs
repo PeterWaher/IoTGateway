39cc7eaf2e4ec2918874ce0ecbe41c8b89d6d83f
Peter Waher <peterwaher@hotmail.com>
2025-12-29 17:59:07 -0300

    Fix comments

--- Content/Waher.Content.Zip/ZipEncryption.cs
+++ Content/Waher.Content.Zip/ZipEncryption.cs
@@ -14,49 +14,39 @@ namespace Waher.Content.Zip
 
 		/// <summary>
 		/// Traditional PKWARE.
-		/// ZipCrypto is considered insecure.
 		/// </summary>
+		/// <remarks>
+		/// ZipCrypto is considered insecure.
+		/// </remarks>
 		ZipCrypto,
 
 		/// <summary>
 		/// PKWARE AES (AE-1), 128-bit.
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes128Ae1,
 
 		/// <summary>
 		/// PKWARE AES (AE-1), 192-bit
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes192Ae1,
 
 		/// <summary>
 		/// PKWARE AES (AE-1), 256-bit
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes256Ae1,
 
 		/// <summary>
 		/// PKWARE AES (AE-2), 128-bit (with auth tag)
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes128Ae2,
 
 		/// <summary>
 		/// PKWARE AES (AE-2), 192-bit (with auth tag)
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes192Ae2,
 
 		/// <summary>
 		/// PKWARE AES (AE-2), 256-bit (with auth tag)
-		/// May require additional license from PKWARE, see §10.0:
-		/// https://pkwaredownloads.blob.core.windows.net/pem/APPNOTE.txt
 		/// </summary>
 		Aes256Ae2
 	}

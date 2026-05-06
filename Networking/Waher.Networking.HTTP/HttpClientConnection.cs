1bd51f947249d53869491a67f30c2aca27adabdc
Peter Waher <peterwaher@hotmail.com>
2026-05-06 10:45:49 -0400

    Max HTTP Entity Size upgraded to 10GB from 2GB to allow for huge Docker file uploads.

--- Networking/Waher.Networking.HTTP/HttpClientConnection.cs
+++ Networking/Waher.Networking.HTTP/HttpClientConnection.cs
@@ -37,7 +37,7 @@ namespace Waher.Networking.HTTP
 		internal const byte LF = 10;
 		internal const int MaxHeaderSize = 65536;
 		internal const int MaxInmemoryMessageSize = 1024 * 1024;        // 1 MB
-		internal const long MaxEntitySize = 2L * 1024 * 1024 * 1024;    // 2 GB
+		internal const long MaxEntitySize = 10L * 1024 * 1024 * 1024;   // 10 GB
 
 		private Guid id = Guid.NewGuid();
 		private MemoryStream headerStream = null;

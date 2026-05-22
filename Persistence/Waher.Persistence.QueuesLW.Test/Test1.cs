1668a80fcce01921a868e59c01427a50727737d9
Peter Waher <peterwaher@hotmail.com>
2026-05-22 11:25:17 -0400

    Adding libraries for persisted queues

---
+++ Persistence/Waher.Persistence.QueuesLW.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Waher.Persistence.QueuesLW.Test
+{
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ Persistence/Waher.Persistence.QueuesLW/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Waher.Persistence.Queues
+{
+	public class Class1
+	{
+
+	}
+}

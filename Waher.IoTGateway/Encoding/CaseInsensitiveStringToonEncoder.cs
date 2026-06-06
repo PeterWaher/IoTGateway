da881738c3f211b721981b6fdbac5d236d587294
Peter Waher <peterwaher@hotmail.com>
2026-06-06 18:31:03 -0400

    Fix: TOON encoding of case-insensitive strings

--- Waher.IoTGateway/Encoding/CaseInsensitiveStringToonEncoder.cs
+++ Waher.IoTGateway/Encoding/CaseInsensitiveStringToonEncoder.cs
@@ -1,42 +1,42 @@
 ﻿using System;
-using System.Text;
 using Waher.Content.Toon;
+using Waher.Content.Toon.Model;
 using Waher.Persistence;
 using Waher.Runtime.Inventory;
 
 namespace Waher.IoTGateway.Encoding
 {
 	/// <summary>
-	/// Encodes <see cref="string"/> values.
+	/// Encodes <see cref="CaseInsensitiveString"/> values.
 	/// </summary>
-	public class CaseInsensitiveStringToonEncoder : IToonEncoder
+	public class CaseInsensitiveStringToonEncoder : SimpleToonEncoder
 	{
 		/// <summary>
-		/// Encodes <see cref="string"/> values.
+		/// Encodes <see cref="CaseInsensitiveString"/> values.
 		/// </summary>
 		public CaseInsensitiveStringToonEncoder()
 		{
 		}
 
 		/// <summary>
-		/// Encodes the <paramref name="Object"/> to JSON.
+		/// Encodes the <paramref name="Object"/> to TOON.
 		/// </summary>
 		/// <param name="Object">Object to encode.</param>
 		/// <param name="Indent">Any indentation to apply.</param>
-		/// <param name="Toon">JSON output.</param>
-		public void Encode(object Object, int? Indent, StringBuilder Toon)
+		/// <param name="Toon">TOON output.</param>
+		public override void Encode(object Object, int? Indent, ToonOutput Toon)
 		{
 			CaseInsensitiveString s = (CaseInsensitiveString)Object;
-			
-			Toon.Append(TOON.Encode(s.Value));
+
+			Toon.AppendEncoded(s.Value, !Toon.Empty);
 		}
 
 		/// <summary>
-		/// How well the JSON encoder encodes objects of type <paramref name="ObjectType"/>.
+		/// How well the TOON encoder encodes objects of type <paramref name="ObjectType"/>.
 		/// </summary>
 		/// <param name="ObjectType">Type of object to encode.</param>
 		/// <returns>How well objects of the given type are encoded.</returns>
-		public Grade Supports(Type ObjectType)
+		public override Grade Supports(Type ObjectType)
 		{
 			return ObjectType == typeof(CaseInsensitiveString) ? Grade.Excellent : Grade.NotAtAll;
 		}

2941b6de76571b6e1a6f66baa39157c839a70a02
Peter Waher <peterwaher@hotmail.com>
2026-07-02 13:24:21 -0400

    Adding OAUTH unit tests

---
+++ Networking/Waher.Networking.HTTP.Test/OAuthTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Waher.Events;
+using Waher.Events.Console;
+using Waher.Networking.HTTP.OAuth;
+using Waher.Networking.Sniffers;
+using Waher.Security;
+using Waher.Security.JWT;
+
+namespace Waher.Networking.HTTP.Test
+{
+	[TestClass]
+	public class OAuthTests : IUserSource
+	{
+		private HttpServer server;
+		private ConsoleEventSink sink = null;
+		private XmlFileSniffer xmlSniffer = null;
+		private JwtFactory jwtFactory = null;
+
+		/// <summary>
+		/// Test context
+		/// </summary>
+		public TestContext TestContext { get; set; }
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			string SnifferFileName = this.TestContext.TestName;
+			if (string.IsNullOrEmpty(SnifferFileName))
+				SnifferFileName = "OAuth";
+
+			SnifferFileName = "Sniffers" + Path.DirectorySeparatorChar + SnifferFileName + ".xml";
+
+			this.sink = new ConsoleEventSink();
+			Log.Register(this.sink);
+
+			File.Delete(SnifferFileName);
+			this.xmlSniffer = new XmlFileSniffer(SnifferFileName,
+				@"..\..\..\..\..\Waher.IoTGateway.Resources\Transforms\SnifferXmlToHtml.xslt",
+				int.MaxValue, BinaryPresentationMethod.ByteCount);
+
+			this.jwtFactory = JwtFactory.CreateHmacSha256();
+			this.server = new HttpServer(8081);
+
+			OAuthTokenResource TokenResource;
+			OAuthAuthorizeResource AuthorizeResource;
+			JwtAuthentication JwtAuthentication = new("Test", this, this.jwtFactory,
+				new Uri("http://localhost:8081" + ProtectedResourceMetaData.WellKnowResourcePath));
+
+			this.server.Register(new ProtectedResourceMetaData());
+			this.server.Register(TokenResource = new OAuthTokenResource(this.jwtFactory));
+			this.server.Register(AuthorizeResource = new OAuthAuthorizeResource(TokenResource, this.jwtFactory));
+			this.server.Register(new AuthorizationServerMetaData(AuthorizeResource));
+
+			this.server.Register("/Hello", this.Hello, 
+				new JwtAuthentication("Test", this, this.jwtFactory));
+		}
+
+		private async Task Hello(HttpRequest Request, HttpResponse Response)
+		{
+			if (Request.User is null)
+			{
+				await Response.SendResponse(new ForbiddenException());
+				return;
+			}
+
+			StringBuilder sb = new();
+
+			sb.Append("Hello ");
+			sb.Append(Request.User.UserName);
+			sb.AppendLine(".");
+
+			await Response.Return(sb.ToString());
+		}
+
+		[TestCleanup]
+		public async Task TestCleanup()
+		{
+			if (this.server is not null)
+			{
+				await this.server.DisposeAsync();
+				this.server = null;
+			}
+
+			if (this.xmlSniffer is not null)
+			{
+				await this.xmlSniffer.DisposeAsync();
+				this.xmlSniffer = null;
+			}
+
+			if (this.sink is not null)
+			{
+				Log.Unregister(this.sink);
+				await this.sink.DisposeAsync();
+				this.sink = null;
+			}
+		}
+
+		public Task<IUser> TryGetUser(string UserName)
+		{
+			if (UserName == "User")
+				return Task.FromResult<IUser>(new User());
+			else
+				return Task.FromResult<IUser>(null);
+		}
+
+		[TestMethod]
+		public async Task Test_01_()
+		{
+		}
+
+	}
+}

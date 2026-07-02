c8004e4f26a51ebf56aabd7ecd0d21f588a12c23
Peter Waher <peterwaher@hotmail.com>
2026-07-01 20:00:43 -0400

    Transfer

--- Networking/Waher.Networking.HTTP.UWP/HttpFolderResource.cs
+++ Networking/Waher.Networking.HTTP.UWP/HttpFolderResource.cs
@@ -221,6 +221,9 @@ namespace Waher.Networking.HTTP
 		/// Any authentication schemes used to authenticate users before access is granted to the corresponding resource.
 		/// </summary>
 		/// <param name="Request">Current request</param>
+		/// <returns>Array of authentication schemes (possibly empty) available for
+		/// authenticating the user making the request. If no default authentication
+		/// is to be performed, null can be returned.</returns>
 		public override HttpAuthenticationScheme[] GetAuthenticationSchemes(HttpRequest Request)
 		{
 			string s;

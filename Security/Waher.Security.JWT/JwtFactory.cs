c6eb6be0f5cfedff4a874c114846542ce0342fb5
Peter Waher <peterwaher@hotmail.com>
2026-07-04 14:57:38 -0400

    Minor edit

--- Security/Waher.Security.JWT/JwtFactory.cs
+++ Security/Waher.Security.JWT/JwtFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using Waher.Runtime.Collections;
 using Waher.Security.JWS;

e3779856a281e280b7b8891123f94916befc2679
Peter Waher <peterwaher@hotmail.com>
2026-07-15 12:45:42 -0400

    Adding Brainpool ECC E2EE tests

---
+++ Networking/Waher.Networking.XMPP.Test/E2eTests/BrainpoolP512Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Test.E2eTests
+{
+    [TestClass]
+    public class BrainpoolP512Tests : XmppE2eTests
+	{
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext _)
+		{
+			SetupSnifferAndLog();
+		}
+
+		[ClassCleanup]
+		public static async Task ClassCleanup()
+		{
+			await DisposeSnifferAndLog();
+		}
+
+		public override int SecurityStrength => 256;
+
+		public override IE2eEndpoint[] GenerateEndpoints(IE2eSymmetricCipher Cipher)
+        {
+            return [new BrainpoolP512Endpoint(Cipher)];
+        }
+
+    }
+}
---
+++ Networking/Waher.Networking.XMPP.Test/E2eTests/HttpxTests/BrainpoolP320Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Waher.Networking.XMPP.P2P.E2E;
+
+namespace Waher.Networking.XMPP.Test.E2eTests.HttpxTests
+{
+    [TestClass]
+    public class BrainpoolP320Tests : XmppHttpxTests
+	{
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext _)
+		{
+			SetupSnifferAndLog();
+		}
+
+		[ClassCleanup]
+		public static async Task ClassCleanup()
+		{
+			await DisposeSnifferAndLog();
+		}
+
+		public override int SecurityStrength => 160;
+
+		public override IE2eEndpoint[] GenerateEndpoints(IE2eSymmetricCipher Cipher)
+        {
+            return [new BrainpoolP320Endpoint(Cipher)];
+        }
+
+    }
+}

1d37ca230672863df582d05342b8962f5fff3849
Peter Waher <peterwaher@hotmail.com>
2026-08-12 17:29:36 -0400

    Minor edits: Removing unused namespaces

--- Runtime/Waher.Runtime.Settings/SettingObjects/BooleanSetting.cs
+++ Runtime/Waher.Runtime.Settings/SettingObjects/BooleanSetting.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Waher.Persistence;
-using Waher.Persistence.Attributes;
-
-namespace Waher.Runtime.Settings.SettingObjects
+﻿namespace Waher.Runtime.Settings.SettingObjects
 {
 	/// <summary>
 	/// Boolean setting object.
